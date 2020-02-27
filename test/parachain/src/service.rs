5832f71850f8954a039d83bb0c27e7d39bdebe27
Cecile Tonglet <cecile.tonglet@cecton.com>
2020-02-27 16:55:28 +0100

    Revert "Update substrate branch to cecton-start-collator"
    This reverts commit 373e3f139540b6024d6e5925513e8e8bc0d1516f.

--- test/parachain/src/service.rs
+++ test/parachain/src/service.rs
@@ -21,12 +21,19 @@ use parachain_runtime::{self, opaque::Block, GenesisConfig};
 use sc_executor::native_executor_instance;
 use sc_network::construct_simple_protocol;
 use sc_service::{AbstractService, Configuration};
+use sp_consensus::{BlockImport, Environment, Proposer};
+use sp_inherents::InherentDataProviders;
+use sp_core::crypto::Pair;
 
+use polkadot_collator::build_collator_service;
 use polkadot_primitives::parachain::CollatorPair;
+use polkadot_service::IsKusama;
 
 use cumulus_collator::CollatorBuilder;
 
-use futures::FutureExt;
+use futures::{future, task::Spawn, FutureExt, TryFutureExt, select, pin_mut};
+
+use log::error;
 
 pub use sc_executor::NativeExecutor;
 
@@ -102,20 +109,18 @@ pub fn run_collator<E: sc_service::ChainSpecExtension>(
 
 		let block_import = service.client();
 		let client = service.client();
-		let builder = CollatorBuilder::new(
-			proposer_factory,
-			inherent_data_providers,
-			block_import,
-			crate::PARA_ID,
-			client,
-		);
+		let builder = move || {
+			CollatorBuilder::new(
+				proposer_factory,
+				inherent_data_providers,
+				block_import,
+				crate::PARA_ID,
+				client,
+			)
+		};
 
-		let polkadot_future = polkadot_collator::start_collator(
-			builder,
-			crate::PARA_ID,
-			key,
-			polkadot_config,
-		).map(|_| ());
+		let polkadot_future = polkadot_collator::build_collator(polkadot_config, crate::PARA_ID, key, Box::new(builder));
+		//let polkadot_future = async { () };
 		service.spawn_essential_task("polkadot", polkadot_future);
 
 		Ok(service)
