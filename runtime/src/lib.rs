d4b2045573796955de4e5bf8f74b6c48b44c3bee
Bastian Köcher <git@kchr.de>
2020-04-20 12:42:28 +0200

    Fix attribute

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -14,9 +14,9 @@
 // You should have received a copy of the GNU General Public License
 // along with Cumulus.  If not, see <http://www.gnu.org/licenses/>.
 
-///! The Cumulus runtime to make a runtime a parachain.
-
 #![cfg_attr(not(feature = "std"), no_std)]
+
+///! The Cumulus runtime to make a runtime a parachain.
 
 use codec::{Decode, Encode};
 use sp_runtime::traits::Block as BlockT;
