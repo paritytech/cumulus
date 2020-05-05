20b79776eff1c001f6d68b74ad71d92ad17c1328
Bastian Köcher <git@kchr.de>
2020-05-05 20:57:23 +0200

    Add the upward-message crate

---
+++ upward-message/src/lib.rs
@@ -0,0 +1,38 @@
+// Copyright 2020 Parity Technologies (UK) Ltd.
+// This file is part of Cumulus.
+
+// Substrate is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Substrate is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Cumulus. If not, see <http://www.gnu.org/licenses/>.
+
+//! Upward messages types and traits for Polkadot, Kusama and Westend.
+//!
+//! As Cumulus needs to suits multiple Polkadot-like runtimes the upward message
+//! type is different for each of them. To support all of them, Cumulus provides
+//! traits to write upward message generic code.
+
+use polkadot_core_primitives::{Balance, AccountId};
+
+mod polkadot;
+mod kusama;
+mod westend;
+
+pub use polkadot::UpwardMessage as PolkadotUpwardMessage;
+pub use kusama::UpwardMessage as KusamaUpwardMessage;
+pub use westend::UpwardMessage as WestendUpwardMessage;
+
+/// A `Balances` related upward message.
+pub trait BalancesMessage: Sized {
+	/// Transfer the given `amount` from the parachain account to the given
+	/// `dest` account.
+	fn transfer(dest: AccountId, amount: Balance) -> Self;
+}
