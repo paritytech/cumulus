baa4357e570142f9699677f75a5c261194ebdc88
Cecile Tonglet <cecile.tonglet@cecton.com>
2020-05-13 10:47:42 +0200

    clean-up

--- test/parachain/tests/integration_test.rs
+++ test/parachain/tests/integration_test.rs
@@ -14,37 +14,32 @@
 // You should have received a copy of the GNU General Public License
 // along with Substrate.  If not, see <http://www.gnu.org/licenses/>.
 
-// TODO
 // This test needs --release to work
-//#![cfg(not(debug_assertions))]
-#![allow(unused_imports)]
+#![cfg(not(debug_assertions))]
+// TODO: this is necessary for the jsonrpsee macro used
+#![allow(unused_variables, dead_code)]
 
 use assert_cmd::cargo::cargo_bin;
-use std::{
-	convert::TryInto, process::{Child, Command, Stdio}, thread, time::Duration, fs, path::PathBuf,
-	env, net, io,
-};
-use tempfile::tempdir;
-use jsonrpc_client_transports::transports::ws;
-use jsonrpc_client_transports::RpcChannel;
-use url::Url;
+use codec::Encode;
 use polkadot_primitives::parachain::{Info, Scheduling};
 use polkadot_primitives::Hash as PHash;
-use polkadot_runtime_common::{registrar, parachains, BlockHashCount};
-use polkadot_runtime::{Runtime, OnlyStakingAndClaims, SignedExtra, SignedPayload};
-use polkadot_service::chain_spec::{polkadot_local_testnet_config, PolkadotChainSpec};
-use polkadot_service::ChainSpec;
-use codec::Encode;
-use substrate_test_runtime_client::AccountKeyring::Alice;
-use sp_runtime::generic;
+use polkadot_runtime::{Header, OnlyStakingAndClaims, Runtime, SignedExtra, SignedPayload};
+use polkadot_runtime_common::{parachains, registrar, BlockHashCount};
+use regex::Regex;
 use sp_arithmetic::traits::SaturatedConversion;
-use sp_api::runtime_decl_for_Core::Core;
-use sp_state_machine::BasicExternalities;
-use sp_runtime::traits::Verify;
-use sp_runtime::traits::SignedExtension;
+use sp_runtime::generic;
 use sp_version::RuntimeVersion;
 use std::io::Read;
-use regex::{Captures, Regex};
+use std::{
+	convert::TryInto,
+	env, fs, io, net,
+	path::PathBuf,
+	process::{Child, Command, Stdio},
+	thread,
+	time::Duration,
+};
+use substrate_test_runtime_client::AccountKeyring::Alice;
+use tempfile::tempdir;
 
 static POLKADOT_ARGS: &[&str] = &["polkadot", "--chain=res/polkadot_chainspec.json"];
 
@@ -59,7 +54,7 @@ jsonrpsee::rpc_api! {
 		fn current_block_hash() -> PHash;
 
 		#[rpc(method = "chain_getHeader", positional_params)]
-		fn header(hash: PHash) -> Option<polkadot_runtime::Header>;
+		fn header(hash: PHash) -> Option<Header>;
 
 		#[rpc(method = "chain_getBlockHash", positional_params)]
 		fn block_hash(hash: Option<u64>) -> Option<PHash>;
@@ -98,8 +93,16 @@ impl<'a> Drop for ProcessCleanUp<'a> {
 		let name = self.name.clone();
 
 		self.terminate();
-		eprintln!("process '{}' stdout:\n{}\n", name, self.read_stdout_to_end());
-		eprintln!("process '{}' stderr:\n{}\n", name, self.read_stderr_to_end());
+		eprintln!(
+			"process '{}' stdout:\n{}\n",
+			name,
+			self.read_stdout_to_end().unwrap_or_default()
+		);
+		eprintln!(
+			"process '{}' stderr:\n{}\n",
+			name,
+			self.read_stderr_to_end().unwrap_or_default()
+		);
 	}
 }
 
@@ -113,43 +116,57 @@ impl<'a> ProcessCleanUp<'a> {
 		}
 	}
 
-	fn read_stdout_to_end(&mut self) -> &str {
+	fn read_stdout_to_end(&mut self) -> io::Result<&str> {
 		let mut output = String::new();
 
-		self.child.stdout.as_mut().unwrap().read_to_string(&mut output);
+		self.child
+			.stdout
+			.as_mut()
+			.ok_or_else(|| io::Error::new(io::ErrorKind::Other, "stdout not captured"))?
+			.read_to_string(&mut output)?;
 		self.stdout.push_str(output.as_str());
 
-		&self.stdout
+		Ok(&self.stdout)
 	}
 
-	fn read_stderr_to_end(&mut self) -> &str {
+	fn read_stderr_to_end(&mut self) -> io::Result<&str> {
 		let mut output = String::new();
 
-		self.child.stderr.as_mut().unwrap().read_to_string(&mut output);
+		self.child
+			.stderr
+			.as_mut()
+			.ok_or_else(|| io::Error::new(io::ErrorKind::Other, "stderr not captured"))?
+			.read_to_string(&mut output)?;
 		self.stderr.push_str(output.as_str());
 
-		&self.stderr
+		Ok(&self.stderr)
 	}
 
-	fn read_stderr(&mut self, size: usize) -> &str {
+	fn read_stderr(&mut self, size: usize) -> io::Result<&str> {
 		let mut buffer = vec![0; size];
-		let size = self.child.stderr.as_mut().unwrap().read(&mut buffer).unwrap();
+		let size = self
+			.child
+			.stderr
+			.as_mut()
+			.ok_or_else(|| io::Error::new(io::ErrorKind::Other, "stderr not captured"))?
+			.read(&mut buffer)?;
 
-		self.stderr.push_str(&String::from_utf8_lossy(&buffer[..size]));
+		self.stderr
+			.push_str(&String::from_utf8_lossy(&buffer[..size]));
 
-		&self.stderr
+		Ok(&self.stderr)
 	}
 
 	fn terminate(&mut self) {
 		match self.child.try_wait() {
 			Ok(Some(_)) => return,
-			Ok(None) => {},
+			Ok(None) => {}
 			Err(err) => {
 				eprintln!("could not wait for child process to finish: {}", err);
 				let _ = self.child.kill();
 				let _ = self.child.wait();
 				return;
-			},
+			}
 		}
 
 		#[cfg(unix)]
@@ -171,7 +188,7 @@ impl<'a> ProcessCleanUp<'a> {
 					Err(err) => {
 						eprintln!("could not wait for child process to finish: {}", err);
 						break false;
-					},
+					}
 				}
 			};
 
@@ -184,6 +201,13 @@ impl<'a> ProcessCleanUp<'a> {
 		let _ = self.child.kill();
 
 		let _ = self.child.wait();
+	}
+}
+
+fn tcp_port_is_open<A: net::ToSocketAddrs>(address: A) -> bool {
+	match net::TcpStream::connect(&address) {
+		Ok(_) => true,
+		Err(_) => false,
 	}
 }
 
@@ -203,7 +227,12 @@ fn wait_for_tcp<A: net::ToSocketAddrs>(address: A) -> io::Result<()> {
 
 #[test]
 fn integration_test() {
-	// starts Alice
+	assert!(
+		!tcp_port_is_open("127.0.0.1:9933"),
+		"tcp port is already open 127.0.0.1:9933, this test cannot be ran",
+	);
+
+	// start alice
 	let polkadot_alice_dir = tempdir().unwrap();
 	let mut polkadot_alice = Command::new(cargo_bin("cumulus-test-parachain-collator"))
 		.stdout(Stdio::piped())
@@ -213,14 +242,13 @@ fn integration_test() {
 		.arg(polkadot_alice_dir.path())
 		.arg("--alice")
 		.arg("--unsafe-rpc-expose")
-		//.args(&["--log", "debug"])
 		.spawn()
 		.unwrap();
 	let mut polkadot_alice_clean_up = ProcessCleanUp::new("alice", &mut polkadot_alice);
 	wait_for_tcp("127.0.0.1:9933").unwrap();
 	let polkadot_alice_id = find_local_node_identity(&mut polkadot_alice_clean_up);
 
-	// starts Bob
+	// start bob
 	let polkadot_bob_dir = tempdir().unwrap();
 	let mut polkadot_bob = Command::new(cargo_bin("cumulus-test-parachain-collator"))
 		.stdout(Stdio::piped())
@@ -234,50 +262,61 @@ fn integration_test() {
 	let mut polkadot_bob_clean_up = ProcessCleanUp::new("bob", &mut polkadot_bob);
 	let polkadot_bob_id = find_local_node_identity(&mut polkadot_bob_clean_up);
 
+	// wait a bit for some relay chains blocks to be generated
+	thread::sleep(Duration::from_secs(10));
+
 	// export genesis state
 	let cmd = Command::new(cargo_bin("cumulus-test-parachain-collator"))
 		.arg("export-genesis-state")
 		.output()
 		.unwrap();
 	assert!(cmd.status.success());
-	let output = &cmd.stdout[2..];
+	let output = &cmd.stdout;
 	let genesis_state = hex::decode(&output[2..output.len() - 1]).unwrap();
-	//thread::sleep_ms(10000);
 
 	// connect RPC client
 	let transport_client =
 		jsonrpsee::transport::http::HttpTransportClient::new("http://127.0.0.1:9933");
 	let mut client = jsonrpsee::raw::RawClient::new(transport_client);
 
-	let runtime_version = async_std::task::block_on(async {
-		State::runtime_version(&mut client).await.unwrap()
-	});
+	// retrieve runtime version
+	let runtime_version =
+		async_std::task::block_on(async { State::runtime_version(&mut client).await.unwrap() });
 
 	// get the current block
-	let current_block_hash = async_std::task::block_on(async {
-		Chain::block_hash(&mut client, None).await.unwrap()
-	}).unwrap();
+	let current_block_hash =
+		async_std::task::block_on(async { Chain::block_hash(&mut client, None).await.unwrap() })
+			.unwrap();
 	let current_block = async_std::task::block_on(async {
-		Chain::header(&mut client, current_block_hash).await.unwrap()
-	}).unwrap().number.saturated_into::<u64>(); //.saturating_sub(1);
+		Chain::header(&mut client, current_block_hash)
+			.await
+			.unwrap()
+	})
+	.unwrap()
+	.number
+	.saturated_into::<u64>();
 
-	let genesis_block = async_std::task::block_on(async {
-		Chain::block_hash(&mut client, 0).await.unwrap()
-	}).unwrap();
+	let genesis_block =
+		async_std::task::block_on(async { Chain::block_hash(&mut client, 0).await.unwrap() })
+			.unwrap();
 
 	// create and sign transaction
-	let wasm = fs::read(target_dir()
-		.join("wbuild/cumulus-test-parachain-runtime/cumulus_test_parachain_runtime.compact.wasm")).unwrap();
-	let call = pallet_sudo::Call::sudo(
-		Box::new(registrar::Call::<Runtime>::register_para(
+	let wasm =
+		fs::read(target_dir().join(
+			"wbuild/cumulus-test-parachain-runtime/cumulus_test_parachain_runtime.compact.wasm",
+		))
+		.unwrap();
+	let call = pallet_sudo::Call::sudo(Box::new(
+		registrar::Call::<Runtime>::register_para(
 			100.into(),
 			Info {
 				scheduling: Scheduling::Always,
 			},
 			wasm.into(),
 			genesis_state.into(),
-		).into()),
-	);
+		)
+		.into(),
+	));
 	let nonce = 0;
 	let period = BlockHashCount::get()
 		.checked_next_power_of_two()
@@ -295,17 +334,21 @@ fn integration_test() {
 		registrar::LimitParathreadCommits::<Runtime>::new(),
 		parachains::ValidateDoubleVoteReports::<Runtime>::new(),
 	);
-	let raw_payload = SignedPayload::from_raw(call.clone().into(), extra.clone(), (
-		(),
-		runtime_version.spec_version,
-		genesis_block,
-		current_block_hash,
-		(),
-		(),
-		(),
-		(),
-		(),
-	));
+	let raw_payload = SignedPayload::from_raw(
+		call.clone().into(),
+		extra.clone(),
+		(
+			(),
+			runtime_version.spec_version,
+			genesis_block,
+			current_block_hash,
+			(),
+			(),
+			(),
+			(),
+			(),
+		),
+	);
 	let signature = raw_payload.using_encoded(|e| Alice.sign(e));
 
 	// register parachain
@@ -315,32 +358,10 @@ fn integration_test() {
 		sp_runtime::MultiSignature::Sr25519(signature),
 		extra,
 	);
-	/*
-	async_std::task::block_on(async {
-		let uri = "http://localhost:9933";
-
-		http::connect(uri)
-			.and_then(|client: AuthorClient<Hash, Hash>| {
-				remove_all_extrinsics(client)
-			})
-			.map_err(|e| {
-				println!("Error: {:?}", e);
-			})
-			.await
-	});
-	*/
-	//let client = ws::connect(&Url::parse("ws://127.0.0.1:9944").unwrap()).await;
-	async_std::task::block_on(async {
+	let _register_block_hash = async_std::task::block_on(async {
 		Author::submit_extrinsic(&mut client, format!("0x{}", hex::encode(ex.encode()))).await
-	});
-	/*
-	assert!(Command::new("/tmp/b/node_modules/.bin/polkadot-js-api")
-		.args(&["--ws", "ws://127.0.0.1:9944", "--sudo", "--seed", "//Alice", "tx.registrar.registerPara", "100", "{\"scheduling\":\"Always\"}", "@/home/cecile/repos/cumulus/target/release/wbuild/cumulus-test-parachain-runtime/cumulus_test_parachain_runtime.compact.wasm"])
-		.arg(&String::from_utf8(genesis_state).unwrap())
-		.status()
-		.unwrap()
-		.success());
-	*/
+	})
+	.unwrap();
 
 	// run cumulus
 	let cumulus_dir = tempdir().unwrap();
@@ -350,19 +371,28 @@ fn integration_test() {
 		.arg("--base-path")
 		.arg(cumulus_dir.path())
 		.arg("--")
-		.arg(format!("--bootnodes=/ip4/127.0.0.1/tcp/30333/p2p/{}", polkadot_alice_id))
-		.arg(format!("--bootnodes=/ip4/127.0.0.1/tcp/50666/p2p/{}", polkadot_bob_id))
+		.arg(format!(
+			"--bootnodes=/ip4/127.0.0.1/tcp/30333/p2p/{}",
+			polkadot_alice_id
+		))
+		.arg(format!(
+			"--bootnodes=/ip4/127.0.0.1/tcp/50666/p2p/{}",
+			polkadot_bob_id
+		))
 		.spawn()
 		.unwrap();
 	let mut cumulus_clean_up = ProcessCleanUp::new("cumulus", &mut cumulus);
 
 	// wait for blocks to be generated
-	thread::sleep(Duration::from_secs(30));
+	thread::sleep(Duration::from_secs(60));
 
 	// check output
 	cumulus_clean_up.terminate();
 	assert!(
-		cumulus_clean_up.read_stderr_to_end().contains("best: #3"),
+		cumulus_clean_up
+			.read_stderr_to_end()
+			.unwrap()
+			.contains("best: #2"),
 		"no parachain blocks seems to have been produced",
 	);
 }
@@ -371,7 +401,7 @@ fn find_local_node_identity(instance: &mut ProcessCleanUp) -> String {
 	let regex = Regex::new(r"Local node identity is: (.+)\n").unwrap();
 
 	loop {
-		let s = instance.read_stderr(200);
+		let s = instance.read_stderr(200).unwrap();
 
 		if let Some(captures) = regex.captures(s) {
 			break captures.get(1).unwrap().as_str().to_string();
