87489c56bfde3a54d5fa6d91bba02f0f20b11dba
Cecile Tonglet <cecile.tonglet@cecton.com>
2020-05-14 11:49:02 +0200

    Implement max time allowed for the test

--- test/parachain/tests/integration_test.rs
+++ test/parachain/tests/integration_test.rs
@@ -18,7 +18,9 @@
 #![allow(unused_variables, dead_code)]
 
 use assert_cmd::cargo::cargo_bin;
+use async_std::task::sleep;
 use codec::Encode;
+use futures::{future::FutureExt, pin_mut, select};
 use polkadot_primitives::parachain::{Info, Scheduling};
 use polkadot_primitives::Hash as PHash;
 use polkadot_runtime::{Header, OnlyStakingAndClaims, Runtime, SignedExtra, SignedPayload};
@@ -29,8 +31,9 @@ use sp_runtime::generic;
 use sp_version::RuntimeVersion;
 use std::{
 	convert::TryInto,
-	env, fs, io, net,
+	env, fs, io,
 	io::Read,
+	net,
 	path::PathBuf,
 	process::{Child, Command, Stdio},
 	thread,
@@ -196,14 +199,14 @@ fn tcp_port_is_open<A: net::ToSocketAddrs>(address: A) -> bool {
 	net::TcpStream::connect(&address).is_ok()
 }
 
-fn wait_for_tcp<A: net::ToSocketAddrs + std::fmt::Display>(address: A) {
+async fn wait_for_tcp<A: net::ToSocketAddrs + std::fmt::Display>(address: A) {
 	loop {
 		match net::TcpStream::connect(&address) {
 			Ok(_) => break,
 			Err(err) => {
 				eprintln!("Waiting for {} to be up ({})...", address, err);
-				thread::sleep(Duration::from_secs(1));
-			},
+				sleep(Duration::from_secs(1)).await;
+			}
 		}
 	}
 }
@@ -220,177 +223,189 @@ async fn integration_test() {
 		"tcp port is already open 127.0.0.1:9934, this test cannot be run",
 	);
 
-	// start alice
-	let polkadot_alice_dir = tempdir().unwrap();
-	let mut polkadot_alice = Command::new(cargo_bin("cumulus-test-parachain-collator"))
-		.stdout(Stdio::piped())
-		.stderr(Stdio::piped())
-		.args(POLKADOT_ARGS)
-		.arg("--base-path")
-		.arg(polkadot_alice_dir.path())
-		.arg("--alice")
-		.arg("--unsafe-rpc-expose")
-		.spawn()
-		.unwrap();
-	let polkadot_alice_helper = ChildHelper::new("alice", &mut polkadot_alice);
-	wait_for_tcp("127.0.0.1:9933");
+	let t1 = sleep(Duration::from_secs(60 * 10)).fuse();
+	let t2 = async {
+		// start alice
+		let polkadot_alice_dir = tempdir().unwrap();
+		let mut polkadot_alice = Command::new(cargo_bin("cumulus-test-parachain-collator"))
+			.stdout(Stdio::piped())
+			.stderr(Stdio::piped())
+			.args(POLKADOT_ARGS)
+			.arg("--base-path")
+			.arg(polkadot_alice_dir.path())
+			.arg("--alice")
+			.arg("--unsafe-rpc-expose")
+			.spawn()
+			.unwrap();
+		let polkadot_alice_helper = ChildHelper::new("alice", &mut polkadot_alice);
+		wait_for_tcp("127.0.0.1:9933").await;
 
-	// start bob
-	let polkadot_bob_dir = tempdir().unwrap();
-	let mut polkadot_bob = Command::new(cargo_bin("cumulus-test-parachain-collator"))
-		.stdout(Stdio::piped())
-		.stderr(Stdio::piped())
-		.args(POLKADOT_ARGS)
-		.arg("--base-path")
-		.arg(polkadot_bob_dir.path())
-		.arg("--bob")
-		.arg("--unsafe-rpc-expose")
-		.arg("--rpc-port=9934")
-		.spawn()
-		.unwrap();
-	let polkadot_bob_helper = ChildHelper::new("bob", &mut polkadot_bob);
-	wait_for_tcp("127.0.0.1:9934");
+		// start bob
+		let polkadot_bob_dir = tempdir().unwrap();
+		let mut polkadot_bob = Command::new(cargo_bin("cumulus-test-parachain-collator"))
+			.stdout(Stdio::piped())
+			.stderr(Stdio::piped())
+			.args(POLKADOT_ARGS)
+			.arg("--base-path")
+			.arg(polkadot_bob_dir.path())
+			.arg("--bob")
+			.arg("--unsafe-rpc-expose")
+			.arg("--rpc-port=9934")
+			.spawn()
+			.unwrap();
+		let polkadot_bob_helper = ChildHelper::new("bob", &mut polkadot_bob);
+		wait_for_tcp("127.0.0.1:9934").await;
 
-	// export genesis state
-	let cmd = Command::new(cargo_bin("cumulus-test-parachain-collator"))
-		.arg("export-genesis-state")
-		.output()
-		.unwrap();
-	assert!(cmd.status.success());
-	let output = &cmd.stdout;
-	let genesis_state = hex::decode(&output[2..output.len() - 1]).unwrap();
+		// export genesis state
+		let cmd = Command::new(cargo_bin("cumulus-test-parachain-collator"))
+			.arg("export-genesis-state")
+			.output()
+			.unwrap();
+		assert!(cmd.status.success());
+		let output = &cmd.stdout;
+		let genesis_state = hex::decode(&output[2..output.len() - 1]).unwrap();
 
-	// connect RPC clients
-	let transport_client_alice =
-		jsonrpsee::transport::http::HttpTransportClient::new("http://127.0.0.1:9933");
-	let mut client_alice = jsonrpsee::raw::RawClient::new(transport_client_alice);
-	let transport_client_bob =
-		jsonrpsee::transport::http::HttpTransportClient::new("http://127.0.0.1:9934");
-	let mut client_bob = jsonrpsee::raw::RawClient::new(transport_client_bob);
+		// connect RPC clients
+		let transport_client_alice =
+			jsonrpsee::transport::http::HttpTransportClient::new("http://127.0.0.1:9933");
+		let mut client_alice = jsonrpsee::raw::RawClient::new(transport_client_alice);
+		let transport_client_bob =
+			jsonrpsee::transport::http::HttpTransportClient::new("http://127.0.0.1:9934");
+		let mut client_bob = jsonrpsee::raw::RawClient::new(transport_client_bob);
 
-	// retrieve nodes network id
-	let polkadot_alice_id = System::network_state(&mut client_alice).await.unwrap()["peerId"]
-		.as_str()
-		.unwrap()
-		.to_string();
-	let polkadot_bob_id = System::network_state(&mut client_bob).await.unwrap()["peerId"]
-		.as_str()
-		.unwrap()
-		.to_string();
+		// retrieve nodes network id
+		let polkadot_alice_id = System::network_state(&mut client_alice).await.unwrap()["peerId"]
+			.as_str()
+			.unwrap()
+			.to_string();
+		let polkadot_bob_id = System::network_state(&mut client_bob).await.unwrap()["peerId"]
+			.as_str()
+			.unwrap()
+			.to_string();
 
-	// retrieve runtime version
-	let runtime_version = State::runtime_version(&mut client_alice).await.unwrap();
+		// retrieve runtime version
+		let runtime_version = State::runtime_version(&mut client_alice).await.unwrap();
 
-	// get the current block
-	let current_block_hash = Chain::block_hash(&mut client_alice, None)
-		.await
-		.unwrap()
-		.unwrap();
-	let current_block = Chain::header(&mut client_alice, current_block_hash)
-		.await
-		.unwrap()
-		.unwrap()
-		.number
-		.saturated_into::<u64>();
+		// get the current block
+		let current_block_hash = Chain::block_hash(&mut client_alice, None)
+			.await
+			.unwrap()
+			.unwrap();
+		let current_block = Chain::header(&mut client_alice, current_block_hash)
+			.await
+			.unwrap()
+			.unwrap()
+			.number
+			.saturated_into::<u64>();
 
-	let genesis_block = Chain::block_hash(&mut client_alice, 0)
-		.await
-		.unwrap()
-		.unwrap();
+		let genesis_block = Chain::block_hash(&mut client_alice, 0)
+			.await
+			.unwrap()
+			.unwrap();
 
-	// create and sign transaction
-	let wasm =
-		fs::read(target_dir().join(
+		// create and sign transaction
+		let wasm = fs::read(target_dir().join(
 			"wbuild/cumulus-test-parachain-runtime/cumulus_test_parachain_runtime.compact.wasm",
 		))
 		.unwrap();
-	let call = pallet_sudo::Call::sudo(Box::new(
-		registrar::Call::<Runtime>::register_para(
-			100.into(),
-			Info {
-				scheduling: Scheduling::Always,
-			},
-			wasm.into(),
-			genesis_state.into(),
-		)
-		.into(),
-	));
-	let nonce = 0;
-	let period = BlockHashCount::get()
-		.checked_next_power_of_two()
-		.map(|c| c / 2)
-		.unwrap_or(2) as u64;
-	let tip = 0;
-	let extra: SignedExtra = (
-		OnlyStakingAndClaims,
-		frame_system::CheckVersion::<Runtime>::new(),
-		frame_system::CheckGenesis::<Runtime>::new(),
-		frame_system::CheckEra::<Runtime>::from(generic::Era::mortal(period, current_block)),
-		frame_system::CheckNonce::<Runtime>::from(nonce),
-		frame_system::CheckWeight::<Runtime>::new(),
-		pallet_transaction_payment::ChargeTransactionPayment::<Runtime>::from(tip),
-		registrar::LimitParathreadCommits::<Runtime>::new(),
-		parachains::ValidateDoubleVoteReports::<Runtime>::new(),
-	);
-	let raw_payload = SignedPayload::from_raw(
-		call.clone().into(),
-		extra.clone(),
-		(
-			(),
-			runtime_version.spec_version,
-			genesis_block,
-			current_block_hash,
-			(),
-			(),
-			(),
-			(),
-			(),
-		),
-	);
-	let signature = raw_payload.using_encoded(|e| Alice.sign(e));
+		let call = pallet_sudo::Call::sudo(Box::new(
+			registrar::Call::<Runtime>::register_para(
+				100.into(),
+				Info {
+					scheduling: Scheduling::Always,
+				},
+				wasm.into(),
+				genesis_state.into(),
+			)
+			.into(),
+		));
+		let nonce = 0;
+		let period = BlockHashCount::get()
+			.checked_next_power_of_two()
+			.map(|c| c / 2)
+			.unwrap_or(2) as u64;
+		let tip = 0;
+		let extra: SignedExtra = (
+			OnlyStakingAndClaims,
+			frame_system::CheckVersion::<Runtime>::new(),
+			frame_system::CheckGenesis::<Runtime>::new(),
+			frame_system::CheckEra::<Runtime>::from(generic::Era::mortal(period, current_block)),
+			frame_system::CheckNonce::<Runtime>::from(nonce),
+			frame_system::CheckWeight::<Runtime>::new(),
+			pallet_transaction_payment::ChargeTransactionPayment::<Runtime>::from(tip),
+			registrar::LimitParathreadCommits::<Runtime>::new(),
+			parachains::ValidateDoubleVoteReports::<Runtime>::new(),
+		);
+		let raw_payload = SignedPayload::from_raw(
+			call.clone().into(),
+			extra.clone(),
+			(
+				(),
+				runtime_version.spec_version,
+				genesis_block,
+				current_block_hash,
+				(),
+				(),
+				(),
+				(),
+				(),
+			),
+		);
+		let signature = raw_payload.using_encoded(|e| Alice.sign(e));
 
-	// register parachain
-	let ex = polkadot_runtime::UncheckedExtrinsic::new_signed(
-		call.into(),
-		Alice.into(),
-		sp_runtime::MultiSignature::Sr25519(signature),
-		extra,
-	);
-	let _register_block_hash =
-		Author::submit_extrinsic(&mut client_alice, format!("0x{}", hex::encode(ex.encode())))
-			.await
+		// register parachain
+		let ex = polkadot_runtime::UncheckedExtrinsic::new_signed(
+			call.into(),
+			Alice.into(),
+			sp_runtime::MultiSignature::Sr25519(signature),
+			extra,
+		);
+		let _register_block_hash =
+			Author::submit_extrinsic(&mut client_alice, format!("0x{}", hex::encode(ex.encode())))
+				.await
+				.unwrap();
+
+		// run cumulus
+		let cumulus_dir = tempdir().unwrap();
+		let mut cumulus = Command::new(cargo_bin("cumulus-test-parachain-collator"))
+			.stdout(Stdio::piped())
+			.stderr(Stdio::piped())
+			.arg("--base-path")
+			.arg(cumulus_dir.path())
+			.arg("--")
+			.arg(format!(
+				"--bootnodes=/ip4/127.0.0.1/tcp/30333/p2p/{}",
+				polkadot_alice_id
+			))
+			.arg(format!(
+				"--bootnodes=/ip4/127.0.0.1/tcp/50666/p2p/{}",
+				polkadot_bob_id
+			))
+			.spawn()
 			.unwrap();
+		let mut cumulus_helper = ChildHelper::new("cumulus", &mut cumulus);
 
-	// run cumulus
-	let cumulus_dir = tempdir().unwrap();
-	let mut cumulus = Command::new(cargo_bin("cumulus-test-parachain-collator"))
-		.stdout(Stdio::piped())
-		.stderr(Stdio::piped())
-		.arg("--base-path")
-		.arg(cumulus_dir.path())
-		.arg("--")
-		.arg(format!(
-			"--bootnodes=/ip4/127.0.0.1/tcp/30333/p2p/{}",
-			polkadot_alice_id
-		))
-		.arg(format!(
-			"--bootnodes=/ip4/127.0.0.1/tcp/50666/p2p/{}",
-			polkadot_bob_id
-		))
-		.spawn()
-		.unwrap();
-	let mut cumulus_helper = ChildHelper::new("cumulus", &mut cumulus);
+		// wait for blocks to be generated
+		sleep(Duration::from_secs(60)).await;
 
-	// wait for blocks to be generated
-	thread::sleep(Duration::from_secs(60));
+		// check output
+		cumulus_helper.terminate();
+		assert!(
+			cumulus_helper
+				.read_stderr_to_end()
+				.unwrap()
+				.contains("best: #2"),
+			"no parachain blocks seems to have been produced",
+		);
+	}
+	.fuse();
 
-	// check output
-	cumulus_helper.terminate();
-	assert!(
-		cumulus_helper
-			.read_stderr_to_end()
-			.unwrap()
-			.contains("best: #2"),
-		"no parachain blocks seems to have been produced",
-	);
+	pin_mut!(t1, t2);
+
+	select! {
+		_ = t1 => {
+			panic!("the test took too long");
+		},
+		_ = t2 => {},
+	}
 }
