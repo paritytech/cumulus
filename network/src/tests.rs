38a96c39aecbb29b63135f8e56a7e3645aa55faa
Cecile Tonglet <cecile.tonglet@cecton.com>
2020-05-26 12:24:23 +0200

    WIP
    Forked at: 032595ddd054153221f65d04dbe7cfabe925f19d
    Parent branch: origin/master

--- network/src/tests.rs
+++ network/src/tests.rs
@@ -28,9 +28,9 @@ use polkadot_primitives::{
 	parachain::{
 		ParachainHost, ValidatorId, Id as ParaId, Chain, CollatorId, Retriable, DutyRoster,
 		ValidationCode, GlobalValidationSchedule, LocalValidationData, AbridgedCandidateReceipt,
-		SigningContext,
+		SigningContext, ValidatorPair,
 	},
-	Block as PBlock, Hash as PHash,
+	Block as PBlock, Hash as PHash, Header as PHeader,
 };
 use sp_api::{ApiRef, ProvideRuntimeApi};
 use sp_blockchain::{Error as ClientError, HeaderBackend};
@@ -38,22 +38,104 @@ use sp_runtime::traits::{NumberFor, Block as BlockT, Zero};
 use polkadot_service::PolkadotChainSpec;
 use sp_state_machine::BasicExternalities;
 use sc_chain_spec::ChainSpec;
+use polkadot_validation::{Statement, sign_table_statement};
+use substrate_test_runtime_client::AccountKeyring::Alice;
+use sp_core::H256;
+use sp_keyring::Sr25519Keyring;
+
+fn make_validator(authorities: Vec<ValidatorId>) -> JustifiedBlockAnnounceValidator<Block, TestApi> {
+	let (validator, _client) = make_validator_and_client(authorities);
+
+	validator
+}
+
+fn make_validator_and_client(authorities: Vec<ValidatorId>) -> (JustifiedBlockAnnounceValidator<Block, TestApi>, Arc<TestApi>) {
+	let builder = TestClientBuilder::new();
+	let client = Arc::new(TestApi::new(Arc::new(builder.build())));
+
+	(JustifiedBlockAnnounceValidator::new(authorities, client.clone()), client)
+}
 
 #[test]
 fn valid_if_no_data() {
-	let builder = TestClientBuilder::new();
-	let client = Arc::new(TestApi::new(Arc::new(builder.build())));
-	let authorities = Vec::new();
-	let mut validator = JustifiedBlockAnnounceValidator::<Block, _>::new(authorities, client);
+	let mut validator = make_validator(Vec::new());
+	let header = Header {
+		number: Default::default(),
+		digest: Default::default(),
+		extrinsics_root: Default::default(),
+		parent_hash: Default::default(),
+		state_root: Default::default(),
+	};
 
-	let genesis_config = PolkadotChainSpec::from_json_bytes(&include_bytes!("../../test/parachain/res/polkadot_chainspec.json")[..]).unwrap();
-	let storage = genesis_config.as_storage_builder().build_storage().unwrap();
-	let mut basic_ext = BasicExternalities::new(storage);
-	let raw_payload = basic_ext.execute_with(|| {
-		let header = Runtime::finalize_block();
-		validator.validate(&header, &[]);
-	});
+	assert!(
+		matches!(validator.validate(&header, &[]), Ok(Validation::Success)),
+		"validating without data is always a success",
+	);
 }
+
+#[test]
+fn check_gossip_message_is_valid() {
+	let mut validator = make_validator(Vec::new());
+	let header = Header {
+		number: Default::default(),
+		digest: Default::default(),
+		extrinsics_root: Default::default(),
+		parent_hash: Default::default(),
+		state_root: Default::default(),
+	};
+
+	let res = validator.validate(&header, &[0x42]).err();
+	assert!(res.is_some(), "only data that are gossip message are allowed");
+	assert!(matches!(*res.unwrap().downcast::<ClientError>().unwrap(), ClientError::BadJustification(_)));
+}
+
+#[test]
+fn check_relay_parent_is_head() {
+	let (mut validator, client) = make_validator_and_client(Vec::new());
+	let header = Header {
+		number: Default::default(),
+		digest: Default::default(),
+		extrinsics_root: Default::default(),
+		parent_hash: Default::default(),
+		state_root: Default::default(),
+	};
+
+	let key = Sr25519Keyring::Alice.pair().into();
+	let relay_chain_leaf = H256::from_low_u64_be(0);
+	let signing_context = client
+		.runtime_api()
+		.signing_context(&BlockId::Hash(relay_chain_leaf))
+		.unwrap();
+	let candidate_receipt = AbridgedCandidateReceipt::default();
+	let statement = Statement::Candidate(candidate_receipt);
+	let signature = sign_table_statement(&statement, &key, &signing_context);
+	let sender = 0;
+	let gossip_statement = GossipStatement {
+		relay_chain_leaf: Default::default(),
+		signed_statement: SignedStatement {
+			statement,
+			signature,
+			sender,
+		},
+	};
+	let gossip_message = GossipMessage::Statement(gossip_statement);
+	let data = gossip_message.encode();
+	let res = validator.validate(&header, data.as_slice());
+	assert_eq!(
+		res.unwrap(),
+		Validation::Failure,
+		"validation fails if the relay chain parent is not the relay chain head",
+	);
+}
+
+	//let genesis_config = PolkadotChainSpec::from_json_bytes(&include_bytes!("../../test/parachain/res/polkadot_chainspec.json")[..]).unwrap();
+	//let storage = genesis_config.as_storage_builder().build_storage().unwrap();
+	//let mut basic_ext = BasicExternalities::new(storage);
+
+	/*
+		//parent_hash: H256::from_low_u64_be(3),
+	*/
+
 #[derive(Default)]
 struct ApiData {
 	validators: Vec<ValidatorId>,
@@ -133,18 +215,20 @@ sp_api::mock_impl_runtime_apis! {
 }
 
 /// Blockchain database header backend. Does not perform any validation.
-impl<Block: BlockT> HeaderBackend<Block> for TestApi {
+impl HeaderBackend<PBlock> for TestApi {
 	fn header(
 		&self,
-		_id: BlockId<Block>,
-	) -> std::result::Result<Option<Block::Header>, sp_blockchain::Error> {
+		_id: BlockId<PBlock>,
+	) -> std::result::Result<Option<PHeader>, sp_blockchain::Error> {
 		Ok(None)
 	}
 
-	fn info(&self) -> sc_client_api::blockchain::Info<Block> {
+	fn info(&self) -> sc_client_api::blockchain::Info<PBlock> {
+		let best_hash = H256::from_low_u64_be(1);
+
 		sc_client_api::blockchain::Info {
-			best_hash: Default::default(),
-			best_number: Zero::zero(),
+			best_hash,
+			best_number: 1,
 			finalized_hash: Default::default(),
 			finalized_number: Zero::zero(),
 			genesis_hash: Default::default(),
@@ -154,22 +238,26 @@ impl<Block: BlockT> HeaderBackend<Block> for TestApi {
 
 	fn status(
 		&self,
-		_id: BlockId<Block>,
+		_id: BlockId<PBlock>,
 	) -> std::result::Result<sc_client_api::blockchain::BlockStatus, sp_blockchain::Error> {
 		Ok(sc_client_api::blockchain::BlockStatus::Unknown)
 	}
 
 	fn number(
 		&self,
-		_hash: Block::Hash,
-	) -> std::result::Result<Option<NumberFor<Block>>, sp_blockchain::Error> {
-		Ok(None)
+		hash: PHash,
+	) -> std::result::Result<Option<NumberFor<PBlock>>, sp_blockchain::Error> {
+		Ok(match hash.as_fixed_bytes() {
+			&[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] => Some(0),
+			&[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1] => Some(1),
+			_ => None,
+		})
 	}
 
 	fn hash(
 		&self,
-		_number: NumberFor<Block>,
-	) -> std::result::Result<Option<Block::Hash>, sp_blockchain::Error> {
+		_number: NumberFor<PBlock>,
+	) -> std::result::Result<Option<PHash>, sp_blockchain::Error> {
 		Ok(None)
 	}
 }
