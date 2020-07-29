99f6af94297540358e5533d5de4cb6e8fd0ba0b0
Cecile Tonglet <cecile.tonglet@cecton.com>
2020-07-29 16:01:22 +0200

    WIP
    Forked at: bcc627799a1924cf79c7de41080781e14d667471
    Parent branch: origin/master

--- test/parachain/src/service.rs
+++ test/parachain/src/service.rs
@@ -205,7 +205,8 @@ pub fn run_collator(
 			.spawn("polkadot", polkadot_future);
 	} else {
 		let is_light = matches!(polkadot_config.role, Role::Light);
-		let builder = polkadot_service::NodeBuilder::new(polkadot_config);
+		let builder = polkadot_service::NodeBuilder::new(polkadot_config)
+			.with_block_announce_validator(block_announce_validator);
 		let mut polkadot_task_manager = if is_light {
 			return Err("Light client not supported.".into());
 		} else {
