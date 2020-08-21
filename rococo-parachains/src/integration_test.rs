6e1e4ac98c776aede5f254f1b75c0cdcc17c89e2
Cecile Tonglet <cecile@parity.io>
2020-08-21 15:41:24 +0200

    CLEANUP
    Forked at: 9e28ea67b6fca10390cd9941313914fb15997771
    Parent branch: origin/master

--- rococo-parachains/src/integration_test.rs
+++ rococo-parachains/src/integration_test.rs
@@ -48,124 +48,23 @@ async fn integration_test() {
 		spawn(fut).map(|_| ())
 	}).into();
 
-	//let polkadot_spec = polkadot_service::chain_spec::rococo_staging_testnet_config().unwrap();
-	//let polkadot_spec = polkadot_service::chain_spec::rococo_local_testnet_config().unwrap();
-
 	// start alice
 	let mut alice =
 		polkadot_test_service::run_test_node(task_executor.clone(), Alice, || {
+			// TODO
 			//polkadot_test_runtime::ExpectedBlockTime::set(&10000);
 		}, vec![]);
-	/*
-	let mut alice_config = polkadot_test_service::node_config(
-		|| {},
-		task_executor.clone(),
-		Alice,
-		vec![],
-	);
-	alice_config.chain_spec = Box::new(polkadot_spec.clone());
-	alice_config.pruning = PruningMode::ArchiveAll;
-	alice_config.offchain_worker = OffchainWorkerConfig { enabled: true, indexing_enabled: false };
-
-	alice_config.rpc_http = Some("127.0.0.1:9933".parse().unwrap());
-	alice_config.rpc_ws = Some("127.0.0.1:9944".parse().unwrap());
-	alice_config.rpc_cors = Some(vec!["http://localhost:*".to_string(),
-		"http://127.0.0.1:*".to_string(),
-		"https://localhost:*".to_string(),
-		"https://127.0.0.1:*".to_string(),
-		"https://polkadot.js.org".to_string()]);
-	alice_config.network = NetworkConfiguration {
-		net_config_path: Some(alice_config.base_path.as_ref().unwrap().path().join("network")),
-		listen_addresses: vec!["/ip6/::/tcp/27015".parse().unwrap(), "/ip4/0.0.0.0/tcp/27015".parse().unwrap()],
-		transport: TransportConfig::Normal {
-			enable_mdns: true,
-			allow_private_ipv4: true,
-			wasm_external_transport: None,
-			use_yamux_flow_control: true,
-		},
-		..alice_config.network
-	};
-
-	println!("{:?}", alice_config);
-	let multiaddr = alice_config.network.listen_addresses[0].clone();
-	let authority_discovery_disabled = false;
-	let grandpa_pause = None;
-	let (task_manager, client, handles, network, rpc_handlers) = polkadot_service::new_full::<rococo_runtime::RuntimeApi, polkadot_service::RococoExecutor>(
-		alice_config,
-		None,
-		None,
-		authority_discovery_disabled,
-		6000,
-		grandpa_pause,
-		true,
-	).unwrap();
-	let peer_id = network.local_peer_id().clone();
-	let addr = MultiaddrWithPeerId { multiaddr, peer_id };
-	let mut alice = polkadot_test_service::PolkadotTestNode {
-		task_manager,
-		client,
-		handles,
-		addr,
-		rpc_handlers,
-	};
-	*/
 
 	// start bob
 	let mut bob = polkadot_test_service::run_test_node(
 		task_executor.clone(),
 		Bob,
 		|| {
+			// TODO
 			//polkadot_test_runtime::ExpectedBlockTime::set(&10000);
 		},
 		vec![alice.addr.clone()],
 	);
-	/*
-	let mut bob_config = polkadot_test_service::node_config(
-		|| {},
-		task_executor.clone(),
-		Bob,
-		vec![alice.addr.clone()],
-	);
-	bob_config.chain_spec = Box::new(polkadot_spec.clone());
-	bob_config.pruning = PruningMode::ArchiveAll;
-	bob_config.offchain_worker = OffchainWorkerConfig { enabled: true, indexing_enabled: false };
-
-	bob_config.network = NetworkConfiguration {
-		net_config_path: Some(bob_config.base_path.as_ref().unwrap().path().join("network")),
-		listen_addresses: vec!["/ip6/::/tcp/27016".parse().unwrap(), "/ip4/0.0.0.0/tcp/27016".parse().unwrap()],
-		transport: TransportConfig::Normal {
-			enable_mdns: true,
-			allow_private_ipv4: true,
-			wasm_external_transport: None,
-			use_yamux_flow_control: true,
-		},
-		..bob_config.network
-	};
-
-	let multiaddr = bob_config.network.listen_addresses[0].clone();
-	let authority_discovery_disabled = false;
-	let grandpa_pause = None;
-	let (task_manager, client, handles, network, rpc_handlers) = polkadot_service::new_full::<rococo_runtime::RuntimeApi, polkadot_service::RococoExecutor>(
-		bob_config,
-		None,
-		None,
-		authority_discovery_disabled,
-		6000,
-		grandpa_pause,
-		true,
-	).unwrap();
-	let peer_id = network.local_peer_id().clone();
-	let addr = MultiaddrWithPeerId { multiaddr, peer_id };
-	let mut bob = polkadot_test_service::PolkadotTestNode {
-		task_manager,
-		client,
-		handles,
-		addr,
-		rpc_handlers,
-	};
-	*/
-
-	//sleep(Duration::from_secs(60)).await;
 
 	let t1 = sleep(Duration::from_secs(
 		INTEGRATION_TEST_ALLOWED_TIME
@@ -181,15 +80,9 @@ async fn integration_test() {
 
 		// export genesis state
 		let spec = Box::new(crate::chain_spec::get_chain_spec(para_id));
-		/*
-		let spec = Box::new(crate::chain_spec::ChainSpec::from_json_bytes(
-			&include_bytes!("./integration_test.json")[..],
-		).unwrap());
-		*/
 		use sp_api::BlockT;
 		let block = crate::command::generate_genesis_state(&(spec.clone() as Box<_>))
 			.unwrap();
-			//.encode();
 		let genesis_state = block.header().encode();
 
 		// create and sign transaction
@@ -206,89 +99,6 @@ async fn integration_test() {
 				genesis_state.into(),
 			)),
 		)));
-		/*
-		macro_rules! call_function {
-			($function:expr) => {{
-				use sp_blockchain::HeaderBackend;
-				use sp_runtime::SaturatedConversion;
-				use sp_runtime::generic;
-				use polkadot_runtime_common::BlockHashCount;
-				let caller = Alice;
-				let current_block_hash = alice.client.info().best_hash;
-				let current_block = alice.client.info().best_number.saturated_into();
-				let genesis_block = alice.client.hash(0).unwrap().unwrap();
-				let nonce = 0;
-				let period = BlockHashCount::get()
-					.checked_next_power_of_two()
-					.map(|c| c / 2)
-					.unwrap_or(2) as u64;
-				let tip = 0;
-				let extra: rococo_runtime::SignedExtra = (
-					//rococo_runtime::RestrictFunctionality,
-					frame_system::CheckSpecVersion::<rococo_runtime::Runtime>::new(),
-					frame_system::CheckTxVersion::<rococo_runtime::Runtime>::new(),
-					frame_system::CheckGenesis::<rococo_runtime::Runtime>::new(),
-					frame_system::CheckEra::<rococo_runtime::Runtime>::from(generic::Era::mortal(period, current_block)),
-					frame_system::CheckNonce::<rococo_runtime::Runtime>::from(nonce),
-					frame_system::CheckWeight::<rococo_runtime::Runtime>::new(),
-					pallet_transaction_payment::ChargeTransactionPayment::<rococo_runtime::Runtime>::from(tip),
-					registrar::LimitParathreadCommits::<rococo_runtime::Runtime>::new(),
-					polkadot_runtime_common::parachains::ValidateDoubleVoteReports::<rococo_runtime::Runtime>::new(),
-				);
-				let raw_payload = rococo_runtime::SignedPayload::from_raw(
-					$function.clone(),
-					extra.clone(),
-					(
-						rococo_runtime::VERSION.spec_version,
-						rococo_runtime::VERSION.transaction_version,
-						genesis_block,
-						current_block_hash,
-						(),
-						(),
-						(),
-						(),
-						(),
-					),
-				);
-				let signature = raw_payload.using_encoded(|e| caller.sign(e));
-				let extrinsic = rococo_runtime::UncheckedExtrinsic::new_signed(
-					$function.clone(),
-					caller.public().into(),
-					polkadot_primitives::v0::Signature::Sr25519(signature.clone()),
-					extra.clone(),
-				);
-
-				// register parachain
-				use substrate_test_client::RpcHandlersExt;
-				let res = alice.rpc_handlers.send_transaction(extrinsic.into()).await.unwrap();
-				println!("###### {:?}", res);
-			}};
-		}
-
-		let function = rococo_runtime::Call::Sudo(pallet_sudo::Call::sudo(Box::new(
-			rococo_runtime::Call::Registrar(registrar::Call::register_para(
-				para_id,
-				Info {
-					scheduling: Scheduling::Always,
-				},
-				parachain_runtime::WASM_BINARY
-					.expect("You need to build the WASM binary to run this test!")
-					.to_vec()
-					.into(),
-				genesis_state.into(),
-			)),
-		)));
-		call_function!(function);
-		*/
-
-		/*
-		let function = rococo_runtime::Call::Sudo(pallet_sudo::Call::sudo(Box::new(
-			rococo_runtime::Call::Registrar(registrar::Call::para_info(
-				para_id,
-			)),
-		)));
-		call_function!(function);
-		*/
 
 		// register parachain
 		let _ = alice.call_function(function, Alice).await.unwrap();
@@ -296,19 +106,21 @@ async fn integration_test() {
 		// run cumulus charlie
 		let key = Arc::new(sp_core::Pair::generate().0);
 		let mut polkadot_config = polkadot_test_service::node_config(
-			|| {},
+			|| {
+				// TODO
+				//polkadot_test_runtime::ExpectedBlockTime::set(&10000);
+			},
 			task_executor.clone(),
 			Charlie,
 			vec![alice.addr.clone(), bob.addr.clone()],
 		);
-		//polkadot_config.chain_spec = Box::new(polkadot_spec.clone());
+		/*
 		polkadot_config.role = Role::Full;
 		polkadot_config.execution_strategies.importing = sc_client_api::ExecutionStrategy::NativeElseWasm;
 		polkadot_config.dev_key_seed = None;
-		println!("{:?}", polkadot_config);
+		*/
 		let parachain_config =
 			parachain_config(task_executor.clone(), Charlie, vec![], spec).unwrap();
-		println!("{:?}", parachain_config);
 		let (_service, charlie_client) =
 			crate::service::start_node(parachain_config, key, polkadot_config, para_id, true, true)
 				.unwrap();
@@ -391,7 +203,6 @@ pub fn parachain_config(
 			offchain_worker: sc_client_api::ExecutionStrategy::NativeWhenPossible,
 			other: sc_client_api::ExecutionStrategy::NativeWhenPossible,
 		},
-		//execution_strategies: Default::default(),
 		rpc_http: None,
 		rpc_ws: None,
 		rpc_ipc: None,
@@ -405,8 +216,7 @@ pub fn parachain_config(
 		offchain_worker: OffchainWorkerConfig { enabled: true, indexing_enabled: false },
 		force_authoring: false,
 		disable_grandpa: false,
-		//dev_key_seed: Some(key_seed),
-		dev_key_seed: None,
+		dev_key_seed: Some(key_seed),
 		tracing_targets: None,
 		tracing_receiver: Default::default(),
 		max_runtime_instances: 8,
