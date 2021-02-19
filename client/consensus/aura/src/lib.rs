d56c303e0e3ac77bedfb6161dfb58fc77479b5bc
Bastian Köcher <info@kchr.de>
2021-02-19 19:17:57 +0100

    Bring back the builder

--- client/consensus/aura/src/lib.rs
+++ client/consensus/aura/src/lib.rs
@@ -44,7 +44,7 @@ pub use import_queue::import_queue;
 use parking_lot::Mutex;
 use polkadot_service::ClientHandle;
 use sc_client_api::{backend::AuxStore, Backend, BlockOf};
-use sc_consensus_slots::BackoffAuthoringBlocksStrategy;
+use sc_consensus_slots::{BackoffAuthoringBlocksStrategy, SlotInfo};
 use sp_api::ProvideRuntimeApi;
 use sp_application_crypto::AppPublic;
 use sp_blockchain::{HeaderBackend, ProvideCache};
@@ -56,9 +56,8 @@ use sp_consensus_aura::AuraApi;
 use sp_core::crypto::Pair;
 use sp_inherents::{CreateInherentDataProviders, InherentData, InherentDataProvider};
 use sp_keystore::SyncCryptoStorePtr;
-use sp_runtime::traits::{Block as BlockT, Header as HeaderT, Member, NumberFor};
+use sp_runtime::traits::{Block as BlockT, HashFor, Header as HeaderT, Member, NumberFor};
 use std::{convert::TryFrom, hash::Hash, marker::PhantomData, sync::Arc};
-use sc_consensus_slots::SlotInfo;
 
 mod import_queue;
 
@@ -93,7 +92,7 @@ where
 {
 	/// Create a new instance of AURA consensus.
 	pub fn new<P, Client, BI, SO, PF, BS, Error>(
-		client: Arc<Client>,
+		para_client: Arc<Client>,
 		block_import: BI,
 		sync_oracle: SO,
 		proposer_factory: PF,
@@ -125,7 +124,7 @@ where
 		P::Signature: TryFrom<Vec<u8>> + Hash + Member + Encode + Decode,
 	{
 		let worker = sc_consensus_aura::build_aura_worker::<P, _, _, _, _, _, _, _>(
-			client,
+			para_client,
 			block_import,
 			proposer_factory,
 			sync_oracle,
@@ -190,12 +189,16 @@ where
 		relay_parent: PHash,
 		validation_data: &PersistedValidationData,
 	) -> Option<ParachainCandidate<B>> {
-		let timestamp = std::time::SystemTime::now().duration_since(std::time::SystemTime::UNIX_EPOCH).unwrap();
+		let timestamp = std::time::SystemTime::now()
+			.duration_since(std::time::SystemTime::UNIX_EPOCH)
+			.unwrap();
 
 		let info = SlotInfo {
 			slot: (validation_data.relay_parent_number as u64).into(),
 			duration: 12000,
-			inherent_data: self.inherent_data(parent.hash(), validation_data, relay_parent).await?,
+			inherent_data: self
+				.inherent_data(parent.hash(), validation_data, relay_parent)
+				.await?,
 			chain_head: parent.clone(),
 			timestamp,
 			ends_at: std::time::Instant::now() + std::time::Duration::from_millis(500),
@@ -211,95 +214,163 @@ where
 	}
 }
 
-/*
-/// Paramaters of [`build_relay_chain_consensus`].
-pub struct BuildRelayChainConsensusParams<PF, BI, RBackend, CIDP> {
-	pub para_id: ParaId,
+/// Paramaters of [`build_aura_consensus`].
+pub struct BuildAuraConsensusParams<PF, BI, RBackend, CIDP, Client, BS, SO> {
 	pub proposer_factory: PF,
 	pub inherent_data_providers: CIDP,
 	pub block_import: BI,
 	pub relay_chain_client: polkadot_service::Client,
 	pub relay_chain_backend: Arc<RBackend>,
+	pub para_client: Arc<Client>,
+	pub backoff_authoring_blocks: Option<BS>,
+	pub sync_oracle: SO,
+	pub keystore: SyncCryptoStorePtr,
+	pub force_authoring: bool,
 }
 
-/// Build the [`RelayChainConsensus`].
+/// Build the [`AuraConsensus`].
 ///
 /// Returns a boxed [`ParachainConsensus`].
-pub fn build_relay_chain_consensus<Block, PF, BI, RBackend, CIDP>(
-	BuildRelayChainConsensusParams {
-		para_id,
+pub fn build_relay_chain_consensus<P, Block, PF, BI, RBackend, CIDP, Client, SO, BS, Error>(
+	BuildAuraConsensusParams {
 		proposer_factory,
 		inherent_data_providers,
 		block_import,
 		relay_chain_client,
 		relay_chain_backend,
-	}: BuildRelayChainConsensusParams<PF, BI, RBackend, CIDP>,
+		para_client,
+		backoff_authoring_blocks,
+		sync_oracle,
+		keystore,
+		force_authoring,
+	}: BuildAuraConsensusParams<PF, BI, RBackend, CIDP, Client, BS, SO>,
 ) -> Box<dyn ParachainConsensus<Block>>
 where
 	Block: BlockT,
-	PF: Environment<Block> + Send + Sync + 'static,
-	PF::Proposer: Proposer<Block, Transaction = BI::Transaction>,
-	BI: BlockImport<Block> + Send + Sync + 'static,
-	RBackend: Backend<PBlock> + 'static,
 	// Rust bug: https://github.com/rust-lang/rust/issues/24159
 	sc_client_api::StateBackendFor<RBackend, PBlock>: sc_client_api::StateBackend<HashFor<PBlock>>,
-	CIDP: CreateInherentDataProviders<Block, (PHash, PersistedValidationData)> + Send + Sync + 'static,
+	RBackend: Backend<PBlock> + 'static,
+	CIDP: CreateInherentDataProviders<Block, (PHash, PersistedValidationData)>
+		+ Send
+		+ Sync
+		+ 'static,
+	Client: ProvideRuntimeApi<Block>
+		+ BlockOf
+		+ ProvideCache<Block>
+		+ AuxStore
+		+ HeaderBackend<Block>
+		+ Send
+		+ Sync
+		+ 'static,
+	Client::Api: AuraApi<Block, P::Public>,
+	BI: BlockImport<Block, Transaction = sp_api::TransactionFor<Client, Block>>
+		+ Send
+		+ Sync
+		+ 'static,
+	SO: SyncOracle + Send + Sync + Clone + 'static,
+	BS: BackoffAuthoringBlocksStrategy<NumberFor<Block>> + Send + 'static,
+	PF: Environment<Block, Error = Error> + Send + Sync + 'static,
+	PF::Proposer:
+		Proposer<Block, Error = Error, Transaction = sp_api::TransactionFor<Client, Block>>,
+	Error: std::error::Error + Send + From<sp_consensus::Error> + 'static,
+	P: Pair + Send + Sync,
+	P::Public: AppPublic + Hash + Member + Encode + Decode,
+	P::Signature: TryFrom<Vec<u8>> + Hash + Member + Encode + Decode,
 {
-	RelayChainConsensusBuilder::new(
-		para_id,
+	AuraConsensusBuilder::<P, _, _, _, _, _, _, _, _, _>::new(
 		proposer_factory,
 		block_import,
 		inherent_data_providers,
 		relay_chain_client,
 		relay_chain_backend,
+		para_client,
+		backoff_authoring_blocks,
+		sync_oracle,
+		force_authoring,
+		keystore,
 	)
 	.build()
 }
 
-/// Relay chain consensus builder.
+/// Aura consensus builder.
 ///
-/// Builds a [`RelayChainConsensus`] for a parachain. As this requires
+/// Builds a [`AuraConsensus`] for a parachain. As this requires
 /// a concrete relay chain client instance, the builder takes a [`polkadot_service::Client`]
 /// that wraps this concrete instanace. By using [`polkadot_service::ExecuteWithClient`]
 /// the builder gets access to this concrete instance.
-struct RelayChainConsensusBuilder<Block, PF, BI, RBackend, CIDP> {
-	para_id: ParaId,
-	_phantom: PhantomData<Block>,
+struct AuraConsensusBuilder<P, Block, PF, BI, RBackend, CIDP, Client, SO, BS, Error> {
+	_phantom: PhantomData<(Block, Error, P)>,
 	proposer_factory: PF,
 	inherent_data_providers: CIDP,
 	block_import: BI,
 	relay_chain_backend: Arc<RBackend>,
 	relay_chain_client: polkadot_service::Client,
+	para_client: Arc<Client>,
+	backoff_authoring_blocks: Option<BS>,
+	sync_oracle: SO,
+	force_authoring: bool,
+	keystore: SyncCryptoStorePtr,
 }
 
-impl<Block, PF, BI, RBackend, CIDP> RelayChainConsensusBuilder<Block, PF, BI, RBackend, CIDP>
+impl<Block, PF, BI, RBackend, CIDP, Client, SO, BS, P, Error>
+	AuraConsensusBuilder<P, Block, PF, BI, RBackend, CIDP, Client, SO, BS, Error>
 where
 	Block: BlockT,
 	// Rust bug: https://github.com/rust-lang/rust/issues/24159
 	sc_client_api::StateBackendFor<RBackend, PBlock>: sc_client_api::StateBackend<HashFor<PBlock>>,
-	PF: Environment<Block> + Send + Sync + 'static,
-	PF::Proposer: Proposer<Block, Transaction = BI::Transaction>,
-	BI: BlockImport<Block> + Send + Sync + 'static,
 	RBackend: Backend<PBlock> + 'static,
-	CIDP: CreateInherentDataProviders<Block, (PHash, PersistedValidationData)> + Send + Sync + 'static,
+	CIDP: CreateInherentDataProviders<Block, (PHash, PersistedValidationData)>
+		+ Send
+		+ Sync
+		+ 'static,
+	Client: ProvideRuntimeApi<Block>
+		+ BlockOf
+		+ ProvideCache<Block>
+		+ AuxStore
+		+ HeaderBackend<Block>
+		+ Send
+		+ Sync
+		+ 'static,
+	Client::Api: AuraApi<Block, P::Public>,
+	BI: BlockImport<Block, Transaction = sp_api::TransactionFor<Client, Block>>
+		+ Send
+		+ Sync
+		+ 'static,
+	SO: SyncOracle + Send + Sync + Clone + 'static,
+	BS: BackoffAuthoringBlocksStrategy<NumberFor<Block>> + Send + 'static,
+	PF: Environment<Block, Error = Error> + Send + Sync + 'static,
+	PF::Proposer:
+		Proposer<Block, Error = Error, Transaction = sp_api::TransactionFor<Client, Block>>,
+	Error: std::error::Error + Send + From<sp_consensus::Error> + 'static,
+	P: Pair + Send + Sync,
+	P::Public: AppPublic + Hash + Member + Encode + Decode,
+	P::Signature: TryFrom<Vec<u8>> + Hash + Member + Encode + Decode,
 {
 	/// Create a new instance of the builder.
 	fn new(
-		para_id: ParaId,
 		proposer_factory: PF,
 		block_import: BI,
 		inherent_data_providers: CIDP,
 		relay_chain_client: polkadot_service::Client,
 		relay_chain_backend: Arc<RBackend>,
+		para_client: Arc<Client>,
+		backoff_authoring_blocks: Option<BS>,
+		sync_oracle: SO,
+		force_authoring: bool,
+		keystore: SyncCryptoStorePtr,
 	) -> Self {
 		Self {
-			para_id,
 			_phantom: PhantomData,
 			proposer_factory,
 			block_import,
 			inherent_data_providers,
 			relay_chain_backend,
 			relay_chain_client,
+			para_client,
+			backoff_authoring_blocks,
+			sync_oracle,
+			force_authoring,
+			keystore,
 		}
 	}
 
@@ -309,17 +380,39 @@ where
 	}
 }
 
-impl<Block, PF, BI, RBackend, CIDP> polkadot_service::ExecuteWithClient
-	for RelayChainConsensusBuilder<Block, PF, BI, RBackend, CIDP>
+impl<Block, PF, BI, RBackend, CIDP, Client, SO, BS, P, Error> polkadot_service::ExecuteWithClient
+	for AuraConsensusBuilder<P, Block, PF, BI, RBackend, CIDP, Client, SO, BS, Error>
 where
 	Block: BlockT,
 	// Rust bug: https://github.com/rust-lang/rust/issues/24159
 	sc_client_api::StateBackendFor<RBackend, PBlock>: sc_client_api::StateBackend<HashFor<PBlock>>,
-	PF: Environment<Block> + Send + Sync + 'static,
-	PF::Proposer: Proposer<Block, Transaction = BI::Transaction>,
-	BI: BlockImport<Block> + Send + Sync + 'static,
 	RBackend: Backend<PBlock> + 'static,
-	CIDP: CreateInherentDataProviders<Block, (PHash, PersistedValidationData)> + Send + Sync + 'static,
+	CIDP: CreateInherentDataProviders<Block, (PHash, PersistedValidationData)>
+		+ Send
+		+ Sync
+		+ 'static,
+	Client: ProvideRuntimeApi<Block>
+		+ BlockOf
+		+ ProvideCache<Block>
+		+ AuxStore
+		+ HeaderBackend<Block>
+		+ Send
+		+ Sync
+		+ 'static,
+	Client::Api: AuraApi<Block, P::Public>,
+	BI: BlockImport<Block, Transaction = sp_api::TransactionFor<Client, Block>>
+		+ Send
+		+ Sync
+		+ 'static,
+	SO: SyncOracle + Send + Sync + Clone + 'static,
+	BS: BackoffAuthoringBlocksStrategy<NumberFor<Block>> + Send + 'static,
+	PF: Environment<Block, Error = Error> + Send + Sync + 'static,
+	PF::Proposer:
+		Proposer<Block, Error = Error, Transaction = sp_api::TransactionFor<Client, Block>>,
+	Error: std::error::Error + Send + From<sp_consensus::Error> + 'static,
+	P: Pair + Send + Sync,
+	P::Public: AppPublic + Hash + Member + Encode + Decode,
+	P::Signature: TryFrom<Vec<u8>> + Hash + Member + Encode + Decode,
 {
 	type Output = Box<dyn ParachainConsensus<Block>>;
 
@@ -331,14 +424,17 @@ where
 		Api: polkadot_service::RuntimeApiCollection<StateBackend = PBackend::State>,
 		PClient: polkadot_service::AbstractClient<PBlock, PBackend, Api = Api> + 'static,
 	{
-		Box::new(AuraConsensus::new(
-			self.para_id,
-			self.proposer_factory,
-			self.inherent_data_providers,
+		Box::new(AuraConsensus::new::<P, _, _, _, _, _, _>(
+			self.para_client,
 			self.block_import,
+			self.sync_oracle,
+			self.proposer_factory,
+			self.force_authoring,
+			self.backoff_authoring_blocks,
+			self.keystore,
+			self.inherent_data_providers,
 			client.clone(),
 			self.relay_chain_backend,
 		))
 	}
 }
-*/
