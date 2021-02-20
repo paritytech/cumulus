8e0ee6192c4150edf2fb145f22aacdf80fffbf05
Alain Brenzikofer <alain.brenzikofer@scs.ch>
2021-02-20 15:41:04 +0100

    bump version to 0.3.0. spec v6

--- polkadot-parachains/shell-runtime/src/lib.rs
+++ polkadot-parachains/shell-runtime/src/lib.rs
@@ -79,8 +79,8 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("encointer-parachain"),
 	impl_name: create_runtime_str!("encointer-parachain"),
 	authoring_version: 1,
-	spec_version: 16,
-	impl_version: 0,
+	spec_version: 6,
+	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
 };
