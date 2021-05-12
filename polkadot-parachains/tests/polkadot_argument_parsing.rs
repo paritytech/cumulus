5d231e9be736c717fa0b242446f6a5da6d0fad5c
Bastian Köcher <info@kchr.de>
2021-05-12 16:06:50 +0200

    Test fixes and some further search&replace

--- polkadot-parachains/tests/polkadot_argument_parsing.rs
+++ polkadot-parachains/tests/polkadot_argument_parsing.rs
@@ -32,7 +32,7 @@ fn polkadot_argument_parsing() {
 
 	fn run_command_and_kill(signal: Signal) {
 		let _ = fs::remove_dir_all("polkadot_argument_parsing");
-		let mut cmd = Command::new(cargo_bin("rococo-collator"))
+		let mut cmd = Command::new(cargo_bin("polkadot-collator"))
 			.args(&[
 				"-d",
 				"polkadot_argument_parsing",
