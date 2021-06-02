7fb7722c9df1fe44d7cca7b5575d57435c0d3539
Shawn Tabrizi <shawntabrizi@gmail.com>
2021-06-01 23:48:23 -0400

    add_benchmark

--- polkadot-parachains/statemine-runtime/src/lib.rs
+++ polkadot-parachains/statemine-runtime/src/lib.rs
@@ -868,6 +868,7 @@ impl_runtime_apis! {
 			add_benchmark!(params, batches, pallet_balances, Balances);
 			add_benchmark!(params, batches, pallet_multisig, Multisig);
 			add_benchmark!(params, batches, pallet_proxy, Proxy);
+			add_benchmark!(params, batches, pallet_uniques, Uniques);
 			add_benchmark!(params, batches, pallet_utility, Utility);
 			add_benchmark!(params, batches, pallet_timestamp, Timestamp);
 			add_benchmark!(params, batches, pallet_collator_selection, CollatorSelection);
