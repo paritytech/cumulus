3edffc7cb70c1e215a72ea7e1dfd9d9b9ebf8f28
willesxm <willeslau@gmail.com>
2021-06-09 00:06:39 +0800

    added docker support

--- setup/config_genesis.sh
+++ setup/config_genesis.sh
@@ -1,7 +1,7 @@
 source target_env
 
-../target/release/polkadot-collator export-genesis-state --parachain-id $PARA_ID > genesis-state-$PARA_ID
-../target/release/polkadot-collator export-genesis-wasm > genesis-wasm-$PARA_ID
+../target/release/polkadot-collator export-genesis-state --parachain-id $PARA_ID > ${HEAD_PATH}
+../target/release/polkadot-collator export-genesis-wasm > ${WASM_PATH}
 
 ../target/release/polkadot build-spec --chain $SPEC_PATH --disable-default-bootnode > $TMP_PATH
 
@@ -9,5 +9,5 @@ node addParachainToGenesis.js
 
 ../target/release/polkadot build-spec --chain $TMP_PATH --disable-default-bootnode --raw > $OUT_PATH
 rm ./$TMP_PATH
-rm genesis-state-$PARA_ID
-rm genesis-wasm-$PARA_ID
+rm ${HEAD_PATH}
+rm ${WASM_PATH}
