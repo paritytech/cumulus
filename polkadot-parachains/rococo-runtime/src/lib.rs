838f6acbf0ac08e052fdd492813e4d12ed282ec7
Christian Langenbacher <clangenb+gh@protonmail.ch>
2021-06-12 07:33:39 +0200

    bump collator and runtime

--- polkadot-parachains/rococo-runtime/src/lib.rs
+++ polkadot-parachains/rococo-runtime/src/lib.rs
@@ -84,7 +84,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("integritee-parachain"),
 	impl_name: create_runtime_str!("integritee-parachain"),
 	authoring_version: 1,
-	spec_version: 5,
+	spec_version: 9,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
