462551ed266ed181c247789ca20b71d00ea583f4
Alain Brenzikofer <news@brenzi.ch>
2021-06-15 22:32:12 +0200

    revert spec version change

--- polkadot-parachains/integritee-runtime/src/lib.rs
+++ polkadot-parachains/integritee-runtime/src/lib.rs
@@ -84,7 +84,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("integritee-parachain"),
 	impl_name: create_runtime_str!("integritee-full"),
 	authoring_version: 1,
-	spec_version: 1,
+	spec_version: 9,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
