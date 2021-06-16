399f5443efb197dd2a376e20cc10a6fa22119b0c
Bastian Köcher <info@kchr.de>
2021-06-16 10:17:23 +0200

    Fix warnings

--- polkadot-parachains/westmint-runtime/src/lib.rs
+++ polkadot-parachains/westmint-runtime/src/lib.rs
@@ -510,7 +510,7 @@ parameter_types! {
 }
 
 /// No local origins on this chain are allowed to dispatch XCM sends/executions.
-pub type LocalOriginToLocation = (SignedToAccountId32<Origin, AccountId, RelayNetwork>,);
+pub type LocalOriginToLocation = SignedToAccountId32<Origin, AccountId, RelayNetwork>;
 
 /// The means for routing XCM messages which are not for local execution into the right message
 /// queues.
