b721c7c0ec793def71bad011b4f081695c923215
Alain Brenzikofer <news@brenzi.ch>
2021-06-20 16:29:09 +0200

    more verbose dump script

--- scripts/dump_wasm_and_state_for_all_chains.sh
+++ scripts/dump_wasm_and_state_for_all_chains.sh
@@ -8,10 +8,13 @@ chainspecs=("integritee-rococo-local" \
       "shell-kusama" \
       )
 
+COLLATOR=./target/release/integritee-collator
+
+$COLLATOR --version
 # Print array values in  lines
 for spec in ${chainspecs[*]}; do
   echo "dumping spec for $spec"
-  ./target/release/integritee-collator export-genesis-state --chain $spec --parachain-id 2015 > ${spec}.state
-  ./target/release/integritee-collator export-genesis-wasm --chain $spec > ${spec}.wasm
-  ./target/release/integritee-collator build-spec --chain $spec > ${spec}.json
+  $COLLATOR export-genesis-state --chain $spec --parachain-id 2015 > ${spec}.state
+  $COLLATOR export-genesis-wasm --chain $spec > ${spec}.wasm
+  $COLLATOR build-spec --chain $spec > ${spec}.json
 done
