c4365f9d24516b71ca3b17d0a7384eb20c106299
zjb0807 <zjb0807@qq.com>
2021-06-22 19:53:06 +0800

    Fix CandidateRemoved event

--- pallets/collator-selection/src/lib.rs
+++ pallets/collator-selection/src/lib.rs
@@ -343,9 +343,9 @@ pub mod pallet {
 				candidates.remove(index);
 				<LastAuthoredBlock<T>>::remove(who.clone());
 				Ok(candidates.len())
-			});
+			})?;
 			Self::deposit_event(Event::CandidateRemoved(who.clone()));
-			current_count
+			Ok(current_count)
 		}
 
 		/// Assemble the current set of candidates and invulnerables into the next collator set.
