5c30e051dbfd3d6f5dc27e1d5b8ca58958c7cbc3
Alexander Popiak <alexander.popiak@parity.io>
2021-07-08 17:39:10 +0200

    increment spec_version and transaction_version (because we change transaction signing)

--- polkadot-parachains/statemint/src/lib.rs
+++ polkadot-parachains/statemint/src/lib.rs
@@ -103,10 +103,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("statemint"),
 	impl_name: create_runtime_str!("statemint"),
 	authoring_version: 1,
-	spec_version: 1,
+	spec_version: 2,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
-	transaction_version: 1,
+	transaction_version: 2,
 };
 
 /// The version information used to identify this runtime when compiled natively.
