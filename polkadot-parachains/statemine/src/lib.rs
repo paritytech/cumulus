a12c4edc1bb7108ac55b2fe7ab42fb2eb9821b67
André Silva <andrerfosilva@gmail.com>
2021-07-15 17:44:25 +0100

    bump spec version of statemine and westmint

--- polkadot-parachains/statemine/src/lib.rs
+++ polkadot-parachains/statemine/src/lib.rs
@@ -103,8 +103,8 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("statemine"),
 	impl_name: create_runtime_str!("statemine"),
 	authoring_version: 1,
-	spec_version: 1,
-	impl_version: 1,
+	spec_version: 2,
+	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
 };
