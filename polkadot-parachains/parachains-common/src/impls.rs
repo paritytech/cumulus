ef75ea710bb52ef50cf5a20024aea6b67729cdb4
Alexander Popiak <alexander.popiak@parity.io>
2021-09-28 16:01:23 +0200

    add test and docs for AssetsFrom

--- polkadot-parachains/parachains-common/src/impls.rs
+++ polkadot-parachains/parachains-common/src/impls.rs
@@ -19,8 +19,8 @@
 use frame_support::traits::{fungibles, Contains, Currency, Get, Imbalance, OnUnbalanced};
 use sp_runtime::traits::Zero;
 use sp_std::marker::PhantomData;
-use xcm_executor::traits::FilterAssetLocation;
 use xcm::latest::{AssetId, Fungibility::Fungible, MultiAsset, MultiLocation};
+use xcm_executor::traits::FilterAssetLocation;
 
 pub type NegativeImbalance<T> = <pallet_balances::Pallet<T> as Currency<
 	<T as frame_system::Config>::AccountId,
@@ -46,6 +46,7 @@ where
 	}
 }
 
+/// Merge the fees into one item and pass them on to the staking pot.
 pub struct DealWithFees<R>(sp_std::marker::PhantomData<R>);
 impl<R> OnUnbalanced<NegativeImbalance<R>> for DealWithFees<R>
 where
@@ -76,11 +77,13 @@ where
 	}
 }
 
+/// Asset filter that allows all assets from a certain location.
 pub struct AssetsFrom<T>(PhantomData<T>);
 impl<T: Get<MultiLocation>> FilterAssetLocation for AssetsFrom<T> {
 	fn filter_asset_location(asset: &MultiAsset, origin: &MultiLocation) -> bool {
 		let loc = T::get();
-		&loc == origin && matches!(asset, MultiAsset { id: AssetId::Concrete(asset_loc), fun: Fungible(_a) }
+		&loc == origin
+			&& matches!(asset, MultiAsset { id: AssetId::Concrete(asset_loc), fun: Fungible(_a) }
 			if asset_loc.match_and_split(&loc).is_some())
 	}
 }
@@ -102,6 +105,7 @@ mod tests {
 		traits::{BlakeTwo256, IdentityLookup},
 		Perbill,
 	};
+	use xcm::prelude::*;
 
 	type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
 	type Block = frame_system::mocking::MockBlock<Test>;
@@ -233,5 +237,28 @@ mod tests {
 			// Author gets 100% of tip and 100% of fee = 30
 			assert_eq!(Balances::free_balance(CollatorSelection::account_id()), 30);
 		});
+	}
+
+	#[test]
+	fn assets_from_filters_correctly() {
+		parameter_types! {
+			pub SomeSiblingParachain: MultiLocation = MultiLocation::new(1, X1(Parachain(1234)));
+		}
+
+		let asset_location = SomeSiblingParachain::get()
+			.clone()
+			.pushed_with_interior(GeneralIndex(42))
+			.expect("multilocation will only have 2 junctions; qed");
+		let asset = MultiAsset {
+			id: Concrete(asset_location),
+			fun: 1_000_000.into(),
+		};
+		assert!(
+			AssetsFrom::<SomeSiblingParachain>::filter_asset_location(
+				&asset,
+				&SomeSiblingParachain::get()
+			),
+			"AssetsFrom should allow assets from any of its interior locations"
+		);
 	}
 }
