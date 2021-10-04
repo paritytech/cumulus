90113d0a086453c6d26d71a608ae31f7af978bec
clangenb <37865735+clangenb@users.noreply.github.com>
2021-10-04 06:55:53 +0200

    chain dump scripts make binary independent chain-specs (#36)
    * [scripts] fix binary-independent chain-specs
    
    * [scripts] extract `dump_wasm_state_and_spec.sh`
    
    * [scripts] make collator configurable
    
    * [scripts] replace paraid in generated spec with arg
    
    * [scripts] add some documentation
    
    * [scripts] make dump-dir configurable
    
    * [scripts] improve docs and fix typo
    
    * [scripts] make dump-dir configurable
    
    * [scripts] fix example in doc

--- scripts/dump_wasm_and_state_for_all_chains.sh
+++ scripts/dump_wasm_and_state_for_all_chains.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 # Remark: wasm and state will be identical for different relay-chains
+
+# Helper script to generate the wasm, state and chain-spec/ -raw.json for a all chain-specs.
+#
+# Usage: ./scripts/dump_wasm_and_state_for_all_chains.sh <para-id> <collator-binary> <dump-dir>
+#
+# Example: ./scripts/dump_wasm_state_and_spec.sh 2000 ./collator ./dump_dir
+#
+# All arguments are optional.
+
+PARA_ID=${1:-2015}
+COLLATOR=${2:-./target/release/integritee-collator}
+DUMP_DIR=${3:-./chain_dumps}
+
+mkdir -p ${DUMP_DIR}
+
 chainspecs=("integritee-rococo-local" \
       "integritee-rococo-local-dev" \
       "integritee-rococo" \
@@ -21,17 +36,8 @@ chainspecs=("integritee-rococo-local" \
 
       )
 
-COLLATOR=./target/release/integritee-collator
-DUMP_DIR=./chain_dumps
-
-mkdir -p $DUMP_DIR
-
 $COLLATOR --version
 # Print array values in  lines
 for spec in ${chainspecs[*]}; do
-  echo "dumping spec for $spec"
-  $COLLATOR export-genesis-state --chain $spec --parachain-id 2015 > $DUMP_DIR/${spec}.state
-  $COLLATOR export-genesis-wasm --chain $spec >  $DUMP_DIR/${spec}.wasm
-  $COLLATOR build-spec --chain $spec >  $DUMP_DIR/${spec}.json
-  $COLLATOR build-spec --chain $spec --raw > $DUMP_DIR/${spec}-raw.json
+  ./scripts/dump_wasm_state_and_spec.sh ${spec} ${PARA_ID} ${COLLATOR} ${DUMP_DIR}
 done
---
+++ scripts/dump_wasm_state_and_spec.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Helper script to generate the wasm, state and chain-spec/ -raw.json for a given chain-spec.
+#
+# Usage: ./scripts/dump_wasm_state_and_spec.sh <chain-spec> <para-id> <collator-binary> <dump-dir>
+#
+# Example: ./scripts/dump_wasm_state_and_spec.sh shell-kusama-local-dev 2000 collator ./dump_dir
+#
+# chain-spec is mandatory, the rest is optional.
+
+
+CHAIN_SPEC=$1
+PARA_ID=${2:-2015}
+COLLATOR=${3:-./target/release/integritee-collator}
+DUMP_DIR=${4:-./chain_dumps}
+
+mkdir -p ${DUMP_DIR}
+
+echo "dumping spec for: $CHAIN_SPEC"
+echo "para_id:          ${PARA_ID}"
+echo "collator:         ${COLLATOR}"
+echo "dump_dir:         ${DUMP_DIR}"
+echo ""
+
+$COLLATOR build-spec --chain ${CHAIN_SPEC} >$DUMP_DIR/${CHAIN_SPEC}.json
+$COLLATOR build-spec --chain ${CHAIN_SPEC} --raw >$DUMP_DIR/${CHAIN_SPEC}-raw.json
+
+# Overwrite the rust-default value with the $PARA_ID
+sed -i "/\"para_id\": 2015/s/2015/${PARA_ID}/" $DUMP_DIR/${CHAIN_SPEC}.json
+sed -i "/\"para_id\": 2015/s/2015/${PARA_ID}/" $DUMP_DIR/${CHAIN_SPEC}-raw.json
+
+$COLLATOR export-genesis-state --chain $DUMP_DIR/${CHAIN_SPEC}-raw.json --parachain-id ${PARA_ID} >$DUMP_DIR/${CHAIN_SPEC}.state
+$COLLATOR export-genesis-wasm --chain $DUMP_DIR/${CHAIN_SPEC}-raw.json >$DUMP_DIR/${CHAIN_SPEC}.wasm
