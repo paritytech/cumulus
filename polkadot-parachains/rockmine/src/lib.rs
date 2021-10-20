6744b9ee652e6430f02e740622d40b889987327c
Ignacio Palacios <ignacio.palacios.santos@gmail.com>
2021-10-20 14:59:44 +0200

    Update polkadot-parachains/rockmine/src/lib.rs
    Co-authored-by: Alexander Popiak <alexander.popiak@parity.io>

--- polkadot-parachains/rockmine/src/lib.rs
+++ polkadot-parachains/rockmine/src/lib.rs
@@ -557,7 +557,7 @@ pub type LocalOriginToLocation = SignedToAccountId32<Origin, AccountId, RelayNet
 /// queues.
 pub type XcmRouter = (
 	// Two routers - use UMP to communicate with the relay chain:
-	cumulus_primitives_utility::ParentAsUmp<ParachainSystem, ()>,
+	cumulus_primitives_utility::ParentAsUmp<ParachainSystem, PolkadotXcm>,
 	// ..and XCMP to communicate with the sibling chains.
 	XcmpQueue,
 );
