c6d601160cac750acf24791a630bf91530f390fe
mosonyi <mosonyi@esix.hu>
2021-11-04 08:45:56 +0100

    Added new CI (#58) (#60)
    * Added new CI (#58)
    
    * update runtime crate names
    
    * [GA] don't use clippy because of upstream merge hassle
    
    Co-authored-by: Christian Langenbacher <clangenb+gh@protonmail.ch>
    Co-authored-by: clangenb <37865735+clangenb@users.noreply.github.com>

--- polkadot-parachains/launch-runtime/src/weights/mod.rs
+++ polkadot-parachains/launch-runtime/src/weights/mod.rs
@@ -16,6 +16,9 @@
 
 //! The weights used in the encointer-launch-runtime
 
+// the generated files do not pass clippy
+#![allow(clippy::all)]
+
 pub mod frame_system;
 pub mod pallet_balances;
 pub mod pallet_timestamp;
