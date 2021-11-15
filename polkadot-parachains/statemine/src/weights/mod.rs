7cf85b8c7a44448c059a7d83831c965b16c9a074
NachoPal <ignacio.palacios.santos@gmail.com>
2021-11-15 18:43:34 +0100

    cargo toml fix + fmt

--- polkadot-parachains/statemine/src/weights/mod.rs
+++ polkadot-parachains/statemine/src/weights/mod.rs
@@ -1,3 +1,4 @@
+pub mod frame_system;
 pub mod pallet_assets;
 pub mod pallet_balances;
 pub mod pallet_collator_selection;
@@ -7,4 +8,3 @@ pub mod pallet_session;
 pub mod pallet_timestamp;
 pub mod pallet_uniques;
 pub mod pallet_utility;
-pub mod frame_system;
