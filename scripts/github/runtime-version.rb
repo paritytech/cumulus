68fecf08f1d4a463529b4a91ceb6de67c2a479f5
Wilfried Kopp <wilfried@parity.io>
2021-11-18 15:26:13 +0100

    Fix runtime_dir for cumulus

--- scripts/github/runtime-version.rb
+++ scripts/github/runtime-version.rb
@@ -3,8 +3,8 @@
 # Gets the runtime version for a given runtime from the filesystem.
 # Optionally accepts a path that is the root of the project which defaults to
 # the current working directory
-def get_runtime(runtime, path = '.')
-  File.open(path + "/runtime/#{runtime}/src/lib.rs") do |f|
+def get_runtime(runtime, path: '.', runtime_dir: 'runtime')
+  File.open(path + "/#{runtime_dir}/#{runtime}/src/lib.rs") do |f|
     f.find { |l| l =~ /spec_version/ }.match(/[0-9]+/)[0]
   end
 end
