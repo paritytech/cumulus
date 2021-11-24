5741bc2e17b063fdc3c1a4c9c8b628f61e78ee06
Wilfried Kopp <wilfried@parity.io>
2021-11-24 09:57:52 +0100

    Add checks, debug and doc

--- scripts/scale_encode_genesis/index.js
+++ scripts/scale_encode_genesis/index.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const { exit } = require("process");
-const {WsProvider, ApiPromise} = require("@polkadot/api");
+const { WsProvider, ApiPromise } = require("@polkadot/api");
 const util = require("@polkadot/util");
 
 // Utility script constructing a SCALE-encoded setStorage call from a key-value json array of
@@ -9,7 +9,7 @@ const util = require("@polkadot/util");
 
 // connect to a local substrate chain and return the api object
 async function connect(port, types = {}) {
-	const provider = new WsProvider("ws://127.0.0.1:" + port);
+	const provider = new WsProvider("ws://localhost:" + port);
 	const api = await ApiPromise.create({
 		provider,
 		types,
@@ -19,28 +19,35 @@ async function connect(port, types = {}) {
 }
 
 if (!process.argv[2] || !process.argv[3]) {
-  console.log("usage: node generate_keys <input json> <scale output file>");
-  exit();
+	console.log("usage: node generate_keys <input json> <scale output file>");
+	exit();
 }
 
 const input = process.argv[2];
 const output = process.argv[3];
+
+console.log("Processing", input, output);
 fs.readFile(input, "utf8", (err, data) => {
-  if (err) {
-    console.log(`Error reading file from disk: ${err}`);
-    exit(1);
-  }
+	if (err) {
+		console.log(`Error reading file from disk: ${err}`);
+		exit(1);
+	}
 
-  const genesis = JSON.parse(data);
+	const genesis = JSON.parse(data);
 
-  connect(9944).then(api => {
-	const setStorage = api.tx.system.setStorage(genesis);
-	const raw = setStorage.method.toU8a();
-	const hex = util.u8aToHex(raw);
-	fs.writeFileSync(output, hex);
-	exit(0)
-  }).catch(e => {
-	  console.error(e);
-	  exit(1)
-  });
+	console.log("loaded genesis, length =  ", genesis.length);
+	console.log('Connecting via WebSocket to :9944');
+	connect(9944)
+		.then((api) => {
+			console.log('Connected');
+			const setStorage = api.tx.system.setStorage(genesis);
+			const raw = setStorage.method.toU8a();
+			const hex = util.u8aToHex(raw);
+			fs.writeFileSync(output, hex);
+			exit(0);
+		})
+		.catch((e) => {
+			console.error(e);
+			exit(1);
+		});
 });
