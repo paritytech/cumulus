e52ca3eb231e74a9392a0a78973ba84676e81246
gilescope <gilescope@gmail.com>
2022-01-19 16:15:33 +0000

    removed non-existent set_changes_trie_config

--- polkadot-parachains/statemine/src/weights/frame_system.rs
+++ polkadot-parachains/statemine/src/weights/frame_system.rs
@@ -62,13 +62,6 @@ impl<T: frame_system::Config> frame_system::WeightInfo for WeightInfo<T> {
 			.saturating_add(T::DbWeight::get().reads(1 as Weight))
 			.saturating_add(T::DbWeight::get().writes(2 as Weight))
 	}
-	// Storage: System Digest (r:1 w:1)
-	// Storage: unknown [0x3a6368616e6765735f74726965] (r:0 w:1)
-	fn set_changes_trie_config() -> Weight {
-		(6_710_000 as Weight)
-			.saturating_add(T::DbWeight::get().reads(1 as Weight))
-			.saturating_add(T::DbWeight::get().writes(2 as Weight))
-	}
 	// Storage: Skipped Metadata (r:0 w:0)
 	fn set_storage(i: u32, ) -> Weight {
 		(0 as Weight)
