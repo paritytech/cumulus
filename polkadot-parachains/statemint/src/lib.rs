b7ee436a1d298c0bfac5a960d80684be2e4f0deb
joepetrowski <joe@parity.io>
2022-03-03 14:31:37 +0100

    no Contains

--- polkadot-parachains/statemint/src/lib.rs
+++ polkadot-parachains/statemint/src/lib.rs
@@ -44,7 +44,7 @@ use codec::{Decode, Encode, MaxEncodedLen};
 use constants::{currency::*, fee::WeightToFee};
 use frame_support::{
 	construct_runtime, parameter_types,
-	traits::{Contains, EnsureOneOf, InstanceFilter},
+	traits::{EnsureOneOf, InstanceFilter},
 	weights::{
 		constants::{BlockExecutionWeight, ExtrinsicBaseWeight},
 		DispatchClass, Weight,
