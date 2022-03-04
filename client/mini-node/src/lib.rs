ba681d27a5007f282714d795e01571e7431c8c2c
Sebastian Kunert <skunert49@gmail.com>
2022-03-04 12:04:25 +0100

    Fix dependencies

--- client/mini-node/src/lib.rs
+++ client/mini-node/src/lib.rs
@@ -15,33 +15,40 @@
 // along with Polkadot.  If not, see <http://www.gnu.org/licenses/>.
 
 use lru::LruCache;
-use polkadot_core_primitives::{Block, Hash};
 use polkadot_node_network_protocol::request_response::{
 	v1::{AvailableDataFetchingRequest, CollationFetchingRequest},
-	IncomingRequestReceiver,
+	IncomingRequest, IncomingRequestReceiver,
 };
-use polkadot_node_subsystem_util::metrics::prometheus::Registry;
+use polkadot_node_subsystem_util::metrics::{prometheus::Registry, Metrics};
 use polkadot_overseer::{
 	BlockInfo, DummySubsystem, MetricsTrait, Overseer, OverseerHandle, OverseerMetrics, SpawnNamed,
 	KNOWN_LEAVES_CACHE_SIZE,
 };
-use polkadot_primitives::v2::ParachainHost;
+use polkadot_primitives::v1::CollatorPair;
 use polkadot_service::{
 	overseer::{
 		AvailabilityRecoverySubsystem, CollationGenerationSubsystem, CollatorProtocolSubsystem,
 		NetworkBridgeSubsystem, ProtocolSide, RuntimeApiSubsystem,
 	},
-	AuthorityDiscoveryApi, Error, IsCollator, OverseerConnector,
+	AuthorityDiscoveryApi, Error, OverseerConnector,
 };
 use sc_authority_discovery::Service as AuthorityDiscoveryService;
-use sc_client_api::AuxStore;
 use sc_keystore::LocalKeystore;
-use sp_api::ProvideRuntimeApi;
 use sp_blockchain::HeaderBackend;
-use sp_consensus_babe::BabeApi;
+pub struct CollatorOverseerGen;
 use std::sync::Arc;
 
-pub struct CollatorOverseerGen;
+use cumulus_primitives_core::relay_chain::{v2::ParachainHost, Block, Hash as PHash};
+
+use polkadot_client::FullBackend;
+use polkadot_service::{
+	AuxStore, BabeApi, Configuration, ConstructRuntimeApi, FullClient, Handle, IdentifyVariant,
+	NativeExecutionDispatch, OverseerGen, RuntimeApiCollection, SelectRelayChain, TaskManager,
+};
+
+use sc_telemetry::TelemetryWorkerHandle;
+use sp_api::ProvideRuntimeApi;
+use sp_runtime::traits::Block as BlockT;
 
 impl CollatorOverseerGen {
 	pub fn generate<'a, Spawner, RuntimeClient>(
@@ -57,7 +64,7 @@ impl CollatorOverseerGen {
 			available_data_req_receiver,
 			registry,
 			spawner,
-			is_collator,
+			collator_pair,
 		}: CollatorOverseerGenArgs<'a, Spawner, RuntimeClient>,
 	) -> Result<(Overseer<Spawner, Arc<RuntimeClient>>, OverseerHandle), Error>
 	where
@@ -65,8 +72,6 @@ impl CollatorOverseerGen {
 		RuntimeClient::Api: ParachainHost<Block> + BabeApi<Block> + AuthorityDiscoveryApi<Block>,
 		Spawner: 'static + SpawnNamed + Clone + Unpin,
 	{
-		use polkadot_node_subsystem_util::metrics::Metrics;
-
 		let metrics = <OverseerMetrics as MetricsTrait>::register(registry)?;
 
 		let builder = Overseer::builder()
@@ -84,19 +89,12 @@ impl CollatorOverseerGen {
 			.chain_api(DummySubsystem)
 			.collation_generation(CollationGenerationSubsystem::new(Metrics::register(registry)?))
 			.collator_protocol({
-				let side = match is_collator {
-					IsCollator::Yes(collator_pair) => ProtocolSide::Collator(
-						network_service.local_peer_id().clone(),
-						collator_pair,
-						collation_req_receiver,
-						Metrics::register(registry)?,
-					),
-					IsCollator::No => ProtocolSide::Validator {
-						keystore: keystore.clone(),
-						eviction_policy: Default::default(),
-						metrics: Metrics::register(registry)?,
-					},
-				};
+				let side = ProtocolSide::Collator(
+					network_service.local_peer_id().clone(),
+					collator_pair,
+					collation_req_receiver,
+					Metrics::register(registry)?,
+				);
 				CollatorProtocolSubsystem::new(side)
 			})
 			.network_bridge(NetworkBridgeSubsystem::new(
@@ -149,7 +147,7 @@ where
 	/// Runtime client generic, providing the `ProvieRuntimeApi` trait besides others.
 	pub runtime_client: Arc<RuntimeClient>,
 	/// Underlying network service implementation.
-	pub network_service: Arc<sc_network::NetworkService<Block, Hash>>,
+	pub network_service: Arc<sc_network::NetworkService<Block, PHash>>,
 	/// Underlying authority discovery service.
 	pub authority_discovery_service: AuthorityDiscoveryService,
 	pub collation_req_receiver: IncomingRequestReceiver<CollationFetchingRequest>,
@@ -159,5 +157,203 @@ where
 	/// Task spawner to be used throughout the overseer and the APIs it provides.
 	pub spawner: Spawner,
 	/// Determines the behavior of the collator.
-	pub is_collator: IsCollator,
+	pub collator_pair: CollatorPair,
+}
+
+pub struct NewCollator {
+	pub task_manager: TaskManager,
+	pub overseer_handle: Option<Handle>,
+	pub network: Arc<sc_network::NetworkService<Block, <Block as BlockT>::Hash>>,
+}
+
+pub fn new_mini<RuntimeApi, ExecutorDispatch, OverseerGenerator>(
+	mut config: Configuration,
+	collator_pair: CollatorPair,
+	jaeger_agent: Option<std::net::SocketAddr>,
+	telemetry_worker_handle: Option<TelemetryWorkerHandle>,
+	overseer_enable_anyways: bool,
+) -> Result<NewCollator, Error>
+where
+	RuntimeApi: ConstructRuntimeApi<Block, FullClient<RuntimeApi, ExecutorDispatch>>
+		+ Send
+		+ Sync
+		+ 'static,
+	RuntimeApi::RuntimeApi:
+		RuntimeApiCollection<StateBackend = sc_client_api::StateBackendFor<FullBackend, Block>>,
+	ExecutorDispatch: NativeExecutionDispatch + 'static,
+	OverseerGenerator: OverseerGen,
+{
+	// use sc_network::config::IncomingRequest;
+
+	let role = config.role.clone();
+
+	// let basics = polkadot_service::new_partial_basics::<RuntimeApi, ExecutorDispatch>(
+	// 	&mut config,
+	// 	jaeger_agent,
+	// 	telemetry_worker_handle,
+	// )?;
+
+	let task_manager = {
+		let registry = config.prometheus_config.as_ref().map(|cfg| &cfg.registry);
+		TaskManager::new(config.tokio_handle.clone(), registry)?
+	};
+
+	let prometheus_registry = config.prometheus_registry().cloned();
+
+	let overseer_connector = OverseerConnector::default();
+	let overseer_handle = Handle::new(overseer_connector.handle());
+
+	let chain_spec = config.chain_spec.cloned_box();
+
+	let local_keystore = None;
+	let requires_overseer_for_chain_sel = local_keystore.is_some();
+
+	let disputes_enabled = chain_spec.is_rococo() ||
+		chain_spec.is_kusama() ||
+		chain_spec.is_westend() ||
+		chain_spec.is_versi() ||
+		chain_spec.is_wococo();
+
+	let select_chain = if requires_overseer_for_chain_sel {
+		let metrics = Metrics::register(prometheus_registry.as_ref())?;
+
+		SelectRelayChain::new_disputes_aware(
+			basics.backend.clone(),
+			overseer_handle.clone(),
+			metrics,
+			disputes_enabled,
+		)
+	} else {
+		SelectRelayChain::new_longest_chain(basics.backend.clone())
+	};
+
+	let auth_disc_publish_non_global_ips = config.network.allow_non_globals_in_dht;
+	{
+		use polkadot_network_bridge::{peer_sets_info, IsAuthority};
+		let is_authority = if role.is_authority() { IsAuthority::Yes } else { IsAuthority::No };
+		config.network.extra_sets.extend(peer_sets_info(is_authority));
+	}
+
+	let (collation_req_receiver, cfg) = IncomingRequest::get_config_receiver();
+	config.network.request_response_protocols.push(cfg);
+	let (available_data_req_receiver, cfg) = IncomingRequest::get_config_receiver();
+	config.network.request_response_protocols.push(cfg);
+
+	let (network, _, network_starter) =
+		sc_service::build_network(sc_service::BuildNetworkParams {
+			config: &config,
+			client: client.clone(),
+			transaction_pool: transaction_pool.clone(),
+			spawn_handle: task_manager.spawn_handle(),
+			import_queue,
+			block_announce_validator_builder: None,
+			warp_sync: None,
+		})?;
+
+	let overseer_client = client.clone();
+	let spawner = task_manager.spawn_handle();
+	// Cannot use the `RelayChainSelection`, since that'd require a setup _and running_ overseer
+	// which we are about to setup.
+	let active_leaves =
+		futures::executor::block_on(active_leaves(select_chain.as_longest_chain(), &*client))?;
+
+	let authority_discovery_service = {
+		use futures::StreamExt;
+		use sc_network::Event;
+
+		let authority_discovery_role = sc_authority_discovery::Role::Discover;
+		let dht_event_stream =
+			network.event_stream("authority-discovery").filter_map(|e| async move {
+				match e {
+					Event::Dht(e) => Some(e),
+					_ => None,
+				}
+			});
+		let (worker, service) = sc_authority_discovery::new_worker_and_service_with_config(
+			sc_authority_discovery::WorkerConfig {
+				publish_non_global_ips: auth_disc_publish_non_global_ips,
+				..Default::default()
+			},
+			client.clone(),
+			network.clone(),
+			Box::pin(dht_event_stream),
+			authority_discovery_role,
+			prometheus_registry.clone(),
+		);
+
+		task_manager.spawn_handle().spawn(
+			"authority-discovery-worker",
+			Some("authority-discovery"),
+			Box::pin(worker.run()),
+		);
+		Some(service)
+	};
+
+	if local_keystore.is_none() {
+		tracing::info!("Cannot run as validator without local keystore.");
+	}
+
+	let maybe_params =
+		local_keystore.and_then(move |k| authority_discovery_service.map(|a| (a, k)));
+
+	let overseer_handle = if let Some((authority_discovery_service, keystore)) = maybe_params {
+		let overseer_gen = CollatorOverseerGen;
+		let (overseer, overseer_handle) = overseer_gen
+			.generate::<sc_service::SpawnTaskHandle, FullClient<RuntimeApi, ExecutorDispatch>>(
+				overseer_connector,
+				CollatorOverseerGenArgs {
+					leaves: active_leaves,
+					keystore,
+					runtime_client: overseer_client.clone(),
+					network_service: network.clone(),
+					authority_discovery_service,
+					collation_req_receiver,
+					available_data_req_receiver,
+					registry: prometheus_registry.as_ref(),
+					spawner,
+					collator_pair,
+				},
+			)
+			.map_err(|e| {
+				tracing::error!("Failed to init overseer: {}", e);
+				e
+			})?;
+		let handle = Handle::new(overseer_handle.clone());
+
+		{
+			let handle = handle.clone();
+			task_manager.spawn_essential_handle().spawn_blocking(
+				"overseer",
+				None,
+				Box::pin(async move {
+					use futures::{pin_mut, select, FutureExt};
+
+					let forward = polkadot_overseer::forward_events(overseer_client, handle);
+
+					let forward = forward.fuse();
+					let overseer_fut = overseer.run().fuse();
+
+					pin_mut!(overseer_fut);
+					pin_mut!(forward);
+
+					select! {
+						_ = forward => (),
+						_ = overseer_fut => (),
+						complete => (),
+					}
+				}),
+			);
+		}
+		Some(handle)
+	} else {
+		assert!(
+			!requires_overseer_for_chain_sel,
+			"Precondition congruence (false) is guaranteed by manual checking. qed"
+		);
+		None
+	};
+
+	network_starter.start_network();
+
+	Ok(NewCollator { task_manager, overseer_handle, network })
 }
