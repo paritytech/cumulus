29797f0deb0436b8235bb0e20b9970874cf625d6
Gav Wood <gavin@parity.io>
2022-03-05 12:59:04 +0100

    Fixes

--- polkadot-parachains/statemint/src/weights/pallet_uniques.rs
+++ polkadot-parachains/statemint/src/weights/pallet_uniques.rs
@@ -224,7 +224,7 @@ impl<T: frame_system::Config> pallet_uniques::WeightInfo for WeightInfo<T> {
 	// Storage: Uniques Asset (r:1 w:1)
 	fn set_accept_ownership() -> Weight {
 		(19_417_000 as Weight)
-			.saturating_add(RocksDbWeight::get().reads(1 as Weight))
-			.saturating_add(RocksDbWeight::get().writes(1 as Weight))
+			.saturating_add(T::DbWeight::get().reads(1 as Weight))
+			.saturating_add(T::DbWeight::get().writes(1 as Weight))
 	}
 }
