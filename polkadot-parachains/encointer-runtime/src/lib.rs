a24d17661ed40536d50cbc279d1ae125a8c0af9b
Alain Brenzikofer <news@brenzi.ch>
2022-04-04 14:39:41 +0200

    bump v0.6.11. spec v5

--- polkadot-parachains/encointer-runtime/src/lib.rs
+++ polkadot-parachains/encointer-runtime/src/lib.rs
@@ -120,7 +120,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("encointer-parachain"),
 	impl_name: create_runtime_str!("encointer-parachain"),
 	authoring_version: 1,
-	spec_version: 4,
+	spec_version: 5,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
