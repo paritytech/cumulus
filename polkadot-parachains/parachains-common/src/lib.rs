53c7c412d87c232dcabe897cdbfdb72cfe4575fa
Giles Cope <gilescope@gmail.com>
2022-04-13 16:11:32 +0100

    remove dupe

--- polkadot-parachains/parachains-common/src/lib.rs
+++ polkadot-parachains/parachains-common/src/lib.rs
@@ -18,15 +18,9 @@
 pub mod impls;
 pub mod xcm_config;
 pub use constants::*;
-use core::marker::PhantomData;
-use frame_support::weights::Weight;
 pub use opaque::*;
 pub use types::*;
-use xcm::{
-	opaque::v2::prelude::{DepositReserveAsset, TransferReserveAsset},
-	v2::{Junctions, MultiLocation, Xcm},
-};
-use xcm_executor::traits::ShouldExecute;
+
 /// Common types of parachains.
 mod types {
 	use sp_runtime::traits::{IdentifyAccount, Verify};
@@ -116,58 +110,4 @@ pub mod opaque {
 	pub type Block = generic::Block<Header, UncheckedExtrinsic>;
 	/// Opaque block identifier type.
 	pub type BlockId = generic::BlockId<Block>;
-}
-
-//TODO: move DenyThenTry to polkadot's xcm module.
-/// Deny executing the xcm message if it matches any of the Deny filter regardless of anything else.
-/// If it passes the Deny, and matches one of the Allow cases then it is let through.
-pub struct DenyThenTry<Deny, Allow>(PhantomData<Deny>, PhantomData<Allow>)
-where
-	Deny: ShouldExecute,
-	Allow: ShouldExecute;
-
-impl<Deny, Allow> ShouldExecute for DenyThenTry<Deny, Allow>
-where
-	Deny: ShouldExecute,
-	Allow: ShouldExecute,
-{
-	fn should_execute<Call>(
-		origin: &MultiLocation,
-		message: &mut Xcm<Call>,
-		max_weight: Weight,
-		weight_credit: &mut Weight,
-	) -> Result<(), ()> {
-		if Deny::should_execute(origin, message, max_weight, weight_credit).is_ok() {
-			return Err(())
-		}
-		Allow::should_execute(origin, message, max_weight, weight_credit)
-	}
-}
-
-// See issue #5233
-pub struct DenyReserveTransferToRelayChain;
-impl ShouldExecute for DenyReserveTransferToRelayChain {
-	fn should_execute<Call>(
-		_origin: &MultiLocation,
-		message: &mut Xcm<Call>,
-		_max_weight: Weight,
-		_weight_credit: &mut Weight,
-	) -> Result<(), ()> {
-		if message.0.iter().any(|inst| {
-			matches!(
-				inst,
-				DepositReserveAsset {
-					dest: MultiLocation { parents: 1, interior: Junctions::Here },
-					..
-				} | TransferReserveAsset {
-					dest: MultiLocation { parents: 1, interior: Junctions::Here },
-					..
-				}
-			)
-		}) {
-			Ok(())
-		} else {
-			Err(())
-		}
-	}
 }
