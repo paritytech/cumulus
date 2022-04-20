2317ef00dfa29a4d23fd6aa808d791a5dab82a02
Bastian Köcher <info@kchr.de>
2022-04-20 15:56:24 +0200

    Prepare the branch

--- client/relay-chain-inprocess-interface/src/lib.rs
+++ client/relay-chain-inprocess-interface/src/lib.rs
@@ -19,8 +19,10 @@ use std::{pin::Pin, sync::Arc, time::Duration};
 use async_trait::async_trait;
 use cumulus_primitives_core::{
 	relay_chain::{
-		runtime_api::ParachainHost,
-		v2::{CommittedCandidateReceipt, OccupiedCoreAssumption, SessionIndex, ValidatorId},
+		v2::{
+			CommittedCandidateReceipt, OccupiedCoreAssumption, ParachainHost, SessionIndex,
+			ValidatorId,
+		},
 		Block as PBlock, BlockId, Hash as PHash, Header as PHeader, InboundHrmpMessage,
 	},
 	InboundDownwardMessage, ParaId, PersistedValidationData,
