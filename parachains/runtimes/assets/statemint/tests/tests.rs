7efb86bbf9c7fa157536b7ead8c6bdc1ef05eb21
gorka <gorka.irazoki@gmail.com>
2022-05-26 13:46:53 +0200

    check total supply in tests

---
+++ parachains/runtimes/assets/statemint/tests/tests.rs
@@ -0,0 +1,303 @@
+use frame_support::{assert_ok, traits::PalletInfo, weights::WeightToFeePolynomial};
+use parachains_common::{AccountId, Balance, StatemintAuraId as AuraId};
+use sp_consensus_aura::AURA_ENGINE_ID;
+pub use statemint_runtime::{
+	constants::fee::WeightToFee, xcm_config::XcmConfig, Assets, Balances, ExistentialDeposit,
+	Runtime, SessionKeys, System,
+};
+use xcm::latest::prelude::*;
+use xcm_executor::traits::WeightTrader;
+
+pub struct ExtBuilder {
+	// endowed accounts with balances
+	balances: Vec<(AccountId, Balance)>,
+	// collators to test block prod
+	collators: Vec<AccountId>,
+}
+
+impl Default for ExtBuilder {
+	fn default() -> ExtBuilder {
+		ExtBuilder { balances: vec![], collators: vec![] }
+	}
+}
+use frame_support::traits::GenesisBuild;
+impl ExtBuilder {
+	pub fn with_balances(mut self, balances: Vec<(AccountId, Balance)>) -> Self {
+		self.balances = balances;
+		self
+	}
+	pub fn with_collators(mut self, collators: Vec<AccountId>) -> Self {
+		self.collators = collators;
+		self
+	}
+
+	pub fn build(self) -> sp_io::TestExternalities {
+		let mut t = frame_system::GenesisConfig::default().build_storage::<Runtime>().unwrap();
+
+		pallet_balances::GenesisConfig::<Runtime> { balances: self.balances }
+			.assimilate_storage(&mut t)
+			.unwrap();
+
+		pallet_collator_selection::GenesisConfig::<Runtime> {
+			invulnerables: self.collators.clone(),
+			candidacy_bond: Default::default(),
+			desired_candidates: Default::default(),
+		}
+		.assimilate_storage(&mut t)
+		.unwrap();
+
+		let keys: Vec<(AccountId, AccountId, SessionKeys)> = self
+			.collators
+			.iter()
+			.map(|account| {
+				let bytearray: &[u8; 32] = account.as_ref();
+				(
+					account.clone(),
+					account.clone(),
+					SessionKeys {
+						aura: AuraId::from(sp_core::ed25519::Public::from_raw(*bytearray)),
+					},
+				)
+			})
+			.collect();
+		pallet_session::GenesisConfig::<Runtime> { keys }
+			.assimilate_storage(&mut t)
+			.unwrap();
+
+		let mut ext = sp_io::TestExternalities::new(t);
+
+		ext.execute_with(|| {
+			System::set_block_number(1);
+		});
+
+		ext
+	}
+}
+
+use codec::Encode;
+use sp_runtime::{Digest, DigestItem};
+/// Utility function that advances the chain to the desired block number.
+/// If an author is provided, that author information is injected to all the blocks in the meantime.
+pub fn run_to_block(n: u32, author: Option<AccountId>) {
+	while System::block_number() < n {
+		// Set the new block number and author
+		match author {
+			Some(ref author) => {
+				let pre_digest =
+					Digest { logs: vec![DigestItem::PreRuntime(AURA_ENGINE_ID, author.encode())] };
+				System::reset_events();
+				System::initialize(
+					&(System::block_number() + 1),
+					&System::parent_hash(),
+					&pre_digest,
+				);
+			},
+			None => {
+				System::set_block_number(System::block_number() + 1);
+			},
+		}
+	}
+}
+
+pub const ALICE: [u8; 32] = [1u8; 32];
+
+pub fn root_origin() -> <Runtime as frame_system::Config>::Origin {
+	<Runtime as frame_system::Config>::Origin::root()
+}
+
+pub fn origin_of(account_id: AccountId) -> <Runtime as frame_system::Config>::Origin {
+	<Runtime as frame_system::Config>::Origin::signed(account_id)
+}
+
+#[test]
+fn test_asset_xcm_trader() {
+	ExtBuilder::default()
+		.with_collators(vec![AccountId::from(ALICE)])
+		.build()
+		.execute_with(|| {
+			// We need root origin to create a sufficient asset
+			// We set existential deposit to be identical to the one for Balances first
+			assert_ok!(Assets::force_create(
+				root_origin(),
+				1,
+				AccountId::from(ALICE).into(),
+				true,
+				ExistentialDeposit::get()
+			));
+
+			// We first mint enough asset for the account to exist for assets
+			assert_ok!(Assets::mint(
+				origin_of(AccountId::from(ALICE)),
+				1,
+				AccountId::from(ALICE).into(),
+				ExistentialDeposit::get()
+			));
+
+			let mut trader = <XcmConfig as xcm_executor::Config>::Trader::new();
+
+			// Set Alice as block author, who will receive fees
+			run_to_block(2, Some(AccountId::from(ALICE)));
+
+			// We are going to buy 4e9 weight
+			let bought = 4_000_000_000u64;
+			// lets calculate amount needed
+			let amount_needed = WeightToFee::calc(&bought);
+
+			let asset_multilocation = MultiLocation::new(
+				0,
+				X2(
+					PalletInstance(
+						<Runtime as frame_system::Config>::PalletInfo::index::<Assets>().unwrap()
+							as u8,
+					),
+					GeneralIndex(1),
+				),
+			);
+
+			let asset: MultiAsset = (asset_multilocation, amount_needed).into();
+			assert_ok!(trader.buy_weight(bought, asset.into()));
+
+			drop(trader);
+			assert_eq!(
+				Assets::balance(1, AccountId::from(ALICE)),
+				ExistentialDeposit::get() + amount_needed
+			);
+
+			// We also need to ensure the total supply increased
+			assert_eq!(
+				Assets::total_supply(1),
+				ExistentialDeposit::get() + amount_needed
+			);
+		});
+}
+
+#[test]
+fn test_asset_xcm_trader_with_refund() {
+	ExtBuilder::default()
+		.with_collators(vec![AccountId::from(ALICE)])
+		.build()
+		.execute_with(|| {
+			// We need root origin to create a sufficient asset
+			// We set existential deposit to be identical to the one for Balances first
+			assert_ok!(Assets::force_create(
+				root_origin(),
+				1,
+				AccountId::from(ALICE).into(),
+				true,
+				ExistentialDeposit::get()
+			));
+
+			// We first mint enough asset for the account to exist for assets
+			assert_ok!(Assets::mint(
+				origin_of(AccountId::from(ALICE)),
+				1,
+				AccountId::from(ALICE).into(),
+				ExistentialDeposit::get()
+			));
+
+			let mut trader = <XcmConfig as xcm_executor::Config>::Trader::new();
+
+			// Set Alice as block author, who will receive fees
+			run_to_block(2, Some(AccountId::from(ALICE)));
+
+			// We are going to buy 4e9 weight
+			let bought = 4_000_000_000u64;
+
+			let asset_multilocation = MultiLocation::new(
+				0,
+				X2(
+					PalletInstance(
+						<Runtime as frame_system::Config>::PalletInfo::index::<Assets>().unwrap()
+							as u8,
+					),
+					GeneralIndex(1),
+				),
+			);
+
+			let amount_bought = WeightToFee::calc(&bought);
+
+			let asset: MultiAsset = (asset_multilocation.clone(), amount_bought).into();
+			assert_ok!(trader.buy_weight(bought, asset.into()));
+
+			let weight_used = bought / 2;
+			let amount_refunded = WeightToFee::calc(&(bought - weight_used));
+
+			assert_eq!(
+				trader.refund_weight(bought - weight_used),
+				Some((asset_multilocation, amount_refunded).into())
+			);
+			drop(trader);
+
+			let fees_paid = WeightToFee::calc(&weight_used);
+
+			assert_eq!(
+				Assets::balance(1, AccountId::from(ALICE)),
+				ExistentialDeposit::get() + fees_paid
+			);
+
+			// We also need to ensure the total supply increased
+			assert_eq!(
+				Assets::total_supply(1),
+				ExistentialDeposit::get() + fees_paid
+			);
+
+		});
+}
+
+#[test]
+fn test_asset_xcm_trader_refund_not_possible_since_amount_less_than_ed() {
+	ExtBuilder::default()
+		.with_collators(vec![AccountId::from(ALICE)])
+		.build()
+		.execute_with(|| {
+			// We need root origin to create a sufficient asset
+			// We set existential deposit to be identical to the one for Balances first
+			assert_ok!(Assets::force_create(
+				root_origin(),
+				1,
+				AccountId::from(ALICE).into(),
+				true,
+				ExistentialDeposit::get()
+			));
+
+			let mut trader = <XcmConfig as xcm_executor::Config>::Trader::new();
+
+			// Set Alice as block author, who will receive fees
+			run_to_block(2, Some(AccountId::from(ALICE)));
+
+			// We are going to buy 4e9 weight
+			let bought = 1_000_000_000u64;
+
+			let asset_multilocation = MultiLocation::new(
+				0,
+				X2(
+					PalletInstance(
+						<Runtime as frame_system::Config>::PalletInfo::index::<Assets>().unwrap()
+							as u8,
+					),
+					GeneralIndex(1),
+				),
+			);
+
+			let amount_bought = WeightToFee::calc(&bought);
+
+			assert!(
+				amount_bought < ExistentialDeposit::get(),
+				"we are testing what happens when the amount does not exceed ED"
+			);
+
+			let asset: MultiAsset = (asset_multilocation.clone(), amount_bought).into();
+			assert_ok!(trader.buy_weight(bought, asset.into()));
+
+			drop(trader);
+
+			// not credited since the ED is higher than this value
+			assert_eq!(Assets::balance(1, AccountId::from(ALICE)), 0);
+
+			// We also need to ensure the total supply increased
+			assert_eq!(
+				Assets::total_supply(1),
+				0
+			);
+		});
+}
