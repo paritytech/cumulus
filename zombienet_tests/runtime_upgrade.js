4714ed16ceef795beec974d7ce04180667fcaf36
Javier Viola <javier@parity.io>
2022-07-02 11:07:47 -0300

    fix upgrade test

--- zombienet_tests/runtime_upgrade.js
+++ zombienet_tests/runtime_upgrade.js
@@ -1,16 +1,8 @@
 const assert = require("assert");
-const polkadotApi = require("@polkadot/api");
-
-async function connect(apiUrl, types) {
-    const provider = new polkadotApi.WsProvider(apiUrl);
-    const api = new polkadotApi.ApiPromise({ provider, types });
-    await api.isReady;
-    return api;
-}
 
 async function run(nodeName, networkInfo, args) {
     const {wsUri, userDefinedTypes} = networkInfo.nodesByName[nodeName];
-    const api = await connect(wsUri, userDefinedTypes);
+    const api = await zombie.connect(wsUri, userDefinedTypes);
 
     // get blockhash/runtimeVersion at block 1
     const hashAtBlock1 = await api.rpc.chain.getBlockHash(1);
@@ -21,10 +13,10 @@ async function run(nodeName, networkInfo, args) {
     const hashAtCurrent = await api.rpc.chain.getBlockHash(currentHeader.number.toHuman());
     const versionAtCurrent = await api.rpc.state.getRuntimeVersion(hashAtCurrent.toHuman());
 
-
+    const oldVersionIncremented = parseInt(versionAtBlock1.specVersion.toHuman(),10) + 1;
     console.log("current", versionAtCurrent.specVersion.toHuman());
     console.log("oldVersionIncremented", oldVersionIncremented);
-    const oldVersionIncremented = versionAtBlock1.specVersion.toHuman() + 1;
+
     // 2 == 2
     assert.equal( oldVersionIncremented, versionAtCurrent.specVersion.toHuman(), "Running version should be the incremented version");
 }
