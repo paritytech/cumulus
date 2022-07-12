9fa698215a10d13812406541e4979f31b8920713
Giles Cope <gilescope@gmail.com>
2022-07-12 09:51:50 +0100

    add missing copyright notices

--- parachains/runtimes/testing/penpal/src/lib.rs
+++ parachains/runtimes/testing/penpal/src/lib.rs
@@ -1,3 +1,19 @@
+// Copyright 2019-2022 Parity Technologies (UK) Ltd.
+// This file is part of Cumulus.
+
+// Cumulus is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Cumulus is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Cumulus.  If not, see <http://www.gnu.org/licenses/>.
+
 //! The Penpal runtime is designed as a test runtime that can be created using an arbitrary parachain id.
 //! (and as such multiple parachains can be on the same relay node - though make sure you have enough relay
 //! nodes running to support this or you will get the not scheduled on a core error message.)
--- parachains/runtimes/testing/penpal/src/xcm_config.rs
+++ parachains/runtimes/testing/penpal/src/xcm_config.rs
@@ -1,3 +1,19 @@
+// Copyright 2019-2022 Parity Technologies (UK) Ltd.
+// This file is part of Cumulus.
+
+// Cumulus is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Cumulus is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Cumulus.  If not, see <http://www.gnu.org/licenses/>.
+
 //! Holds the XCM specific configuration that would otherwise be in lib.rs
 //!
 //! This configuration dictates how the Penpal chain will communicate with other chains.
