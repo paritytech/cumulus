b4f45fa41762d0754ee315ca5caf1321611ff8e9
gorka <gorka.irazoki@gmail.com>
2022-07-18 16:23:56 +0200

    Add doc and struct for weight refund

--- primitives/utility/src/lib.rs
+++ primitives/utility/src/lib.rs
@@ -62,6 +62,26 @@ impl<T: UpwardMessageSender, W: WrapVersion> SendXcm for ParentAsUmp<T, W> {
 	}
 }
 
+/// Contains information to handle refund/payment for xcm-execution
+#[derive(Clone, Eq, PartialEq, Debug)]
+struct AssetTraderRefunder<Balance, AssetId> {
+	// The amount of weight bought minus the weigh already refunded
+	weight: Weight,
+	// The balance paid for the weight bought minus the balance already refunded
+	balance: Balance,
+	// The multilocation representing the fungible asset in which fees were paid
+	location: MultiLocation,
+	// The local assetId the aforementioned location corresponds to
+	asset: AssetId,
+}
+
+/// Charges for exercution in the first asset of those selected for fee payment
+/// First tries to convert the this MultiAsset into a local assetId
+/// Then charges for this assetId as described by FeeCharger
+/// Weight, paid balance, local asset Id and the multilocation is stored for
+/// later refund purposes
+/// Important: Errors if the Trader is being called twice by 2 BuyExecution instructions
+/// Alternatively we could just return payment in the aforementioned case
 pub struct TakeFirstAssetTrader<
 	AccountId,
 	FeeCharger: ChargeWeightInFungibles<AccountId, Assets>,
@@ -69,9 +89,7 @@ pub struct TakeFirstAssetTrader<
 	Assets: fungibles::Mutate<AccountId> + fungibles::Transfer<AccountId> + fungibles::Balanced<AccountId>,
 	HandleRefund: TakeRevenue,
 >(
-	Weight,
-	Assets::Balance,
-	Option<(MultiLocation, Assets::AssetId)>,
+	Option<AssetTraderRefunder<Assets::Balance, Assets::AssetId>>,
 	PhantomData<(AccountId, FeeCharger, Matcher, Assets, HandleRefund)>,
 );
 impl<
@@ -85,10 +103,9 @@ impl<
 	> WeightTrader for TakeFirstAssetTrader<AccountId, FeeCharger, Matcher, Assets, HandleRefund>
 {
 	fn new() -> Self {
-		Self(0, Zero::zero(), None, PhantomData)
+		Self(None, PhantomData)
 	}
 	// We take first asset
-	// TODO: do we want to be iterating over all potential assets in payments?
 	// Check whether we can convert fee to asset_fee (is_sufficient, min_deposit)
 	// If everything goes well, we charge.
 	fn buy_weight(
@@ -99,18 +116,21 @@ impl<
 		log::trace!(target: "xcm::weight", "TakeFirstAssetTrader::buy_weight weight: {:?}, payment: {:?}", weight, payment);
 
 		// Make sure we dont enter twice
-		if self.2.is_some() {
+		if self.0.is_some() {
 			return Err(XcmError::NotWithdrawable)
 		}
 
 		// We take the very first multiasset from payment
-		// TODO: revisit this clone
 		let multiassets: MultiAssets = payment.clone().into();
+
+		// Take the first asset from the selected MultiAssets
 		let first = multiassets.get(0).ok_or(XcmError::TooExpensive)?;
-		// We get the local asset id in which we can pay for fees
+
+		// Get the local asset id in which we can pay for fees
 		let (local_asset_id, _) =
 			Matcher::matches_fungibles(&first).map_err(|_| XcmError::AssetNotFound)?;
-		// we calculate how much we should charge in the asset_id for such amount of weight
+
+		// Calculate how much we should charge in the asset_id for such amount of weight
 		let asset_balance = FeeCharger::charge_weight_in_fungibles(local_asset_id, weight)?;
 
 		match first {
@@ -119,18 +139,20 @@ impl<
 				id: xcm::latest::AssetId::Concrete(location),
 				fun: Fungibility::Fungible(_),
 			} => {
-				// convert asset_balance to u128
+				// Convert asset_balance to u128
 				let u128_amount: u128 = asset_balance.try_into().map_err(|_| XcmError::Overflow)?;
 				// Construct required payment
 				let required = (Concrete(location.clone()), u128_amount).into();
 				// Substract payment
 				let unused = payment.checked_sub(required).map_err(|_| XcmError::TooExpensive)?;
-				// record weight
-				self.0 = self.0.saturating_add(weight);
-				// record amount
-				self.1 = self.1.saturating_add(asset_balance);
-				// record multilocation and local_asset_id
-				self.2 = Some((location.clone(), local_asset_id));
+
+				// record weight, balance, multilocation and assetId
+				self.0 = Some(AssetTraderRefunder {
+					weight,
+					balance: asset_balance,
+					location: location.clone(),
+					asset: local_asset_id,
+				});
 				Ok(unused)
 			},
 			_ => return Err(XcmError::TooExpensive),
@@ -139,19 +161,22 @@ impl<
 
 	fn refund_weight(&mut self, weight: Weight) -> Option<MultiAsset> {
 		log::trace!(target: "xcm::weight", "TakeFirstAssetTrader::refund_weight weight: {:?}", weight);
-		let weight = weight.min(self.0);
-		if let Some((asset_location, local_asset_id)) = self.2.clone() {
+		if let Some(mut asset_trader) = self.0.clone() {
+			let weight = weight.min(asset_trader.weight);
+
 			// Calculate asset_balance
 			// This read should have already be cached in buy_weight
 			let asset_balance =
-				FeeCharger::charge_weight_in_fungibles(local_asset_id, weight).ok()?;
+				FeeCharger::charge_weight_in_fungibles(asset_trader.asset, weight).ok()?;
 
-			self.0 = self.0.saturating_sub(weight);
-			self.1 = self.1.saturating_sub(asset_balance);
+			// Substract from existing weight and balance
+			asset_trader.weight = asset_trader.weight.saturating_sub(weight);
+			asset_trader.balance = asset_trader.balance.saturating_sub(asset_balance);
+			self.0 = Some(asset_trader.clone());
 
 			let asset_balance: u128 = asset_balance.saturated_into();
 			if asset_balance > 0 {
-				Some((asset_location, asset_balance).into())
+				Some((asset_trader.location, asset_balance).into())
 			} else {
 				None
 			}
@@ -172,10 +197,11 @@ impl<
 	> Drop for TakeFirstAssetTrader<AccountId, FeeCharger, Matcher, Assets, HandleRefund>
 {
 	fn drop(&mut self) {
-		if let Some((location, _)) = &self.2 {
-			let u128_amount: u128 = self.1.try_into().map_err(|_| XcmError::Overflow).unwrap();
+		if let Some(asset_trader) = self.0.clone() {
+			let u128_amount: u128 =
+				asset_trader.balance.try_into().map_err(|_| XcmError::Overflow).unwrap();
 			HandleRefund::take_revenue(MultiAsset {
-				id: location.clone().into(),
+				id: asset_trader.location.clone().into(),
 				fun: Fungibility::Fungible(u128_amount),
 			});
 		}
