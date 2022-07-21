4ea499216318405ccb99c5814080e77784188bc2
Muharem Ismailov <ismailov.m.h@gmail.com>
2022-07-21 19:43:51 +0200

    Move alliance proposal provider to impls.rs (#1464)
    * Move to impls alliance proposal provider
    
    * rustfmt

--- parachains/runtimes/collectives/collectives-polkadot/src/impls.rs
+++ parachains/runtimes/collectives/collectives-polkadot/src/impls.rs
@@ -14,13 +14,20 @@
 // limitations under the License.
 
 use frame_support::{
+	dispatch::{DispatchError, DispatchResultWithPostInfo},
 	log,
 	traits::{Currency, Get, Imbalance, OnUnbalanced, OriginTrait},
+	weights::Weight,
 };
+use pallet_alliance::{ProposalIndex, ProposalProvider};
 use sp_std::{boxed::Box, marker::PhantomData};
 use xcm::latest::{Fungibility, Junction, NetworkId, Parent};
 
 type AccountIdOf<T> = <T as frame_system::Config>::AccountId;
+
+type ProposalOf<T, I> = <T as pallet_collective::Config<I>>::Proposal;
+
+type HashOf<T> = <T as frame_system::Config>::Hash;
 
 type NegativeImbalanceOf<T, I> = <<T as pallet_alliance::Config<I>>::Currency as Currency<
 	<T as frame_system::Config>::AccountId,
@@ -32,6 +39,7 @@ type BalanceOf<T, I> = <<T as pallet_alliance::Config<I>>::Currency as Currency<
 	<T as frame_system::Config>::AccountId,
 >>::Balance;
 
+/// Implements `OnUnbalanced::on_unbalanced` to teleport slashed assets to relay chain treasury account.
 pub struct ToParentTreasury<TreasuryAcc, TempAcc, T, I = ()>(
 	PhantomData<(TreasuryAcc, TempAcc, T, I)>,
 );
@@ -69,5 +77,61 @@ where
 			Err(err) => log::warn!("Failed to teleport slashed assets: {:?}", err),
 			_ => (),
 		};
+	}
+}
+
+/// Proposal provider for alliance pallet.
+/// Adapter from collective pallet to alliance proposal provider trait.
+pub struct AllianceProposalProvider<T, I = ()>(PhantomData<(T, I)>);
+
+impl<T, I> ProposalProvider<AccountIdOf<T>, HashOf<T>, ProposalOf<T, I>>
+	for AllianceProposalProvider<T, I>
+where
+	T: pallet_collective::Config<I> + frame_system::Config,
+	I: 'static,
+{
+	fn propose_proposal(
+		who: AccountIdOf<T>,
+		threshold: u32,
+		proposal: Box<ProposalOf<T, I>>,
+		length_bound: u32,
+	) -> Result<(u32, u32), DispatchError> {
+		pallet_collective::Pallet::<T, I>::do_propose_proposed(
+			who,
+			threshold,
+			proposal,
+			length_bound,
+		)
+	}
+
+	fn vote_proposal(
+		who: AccountIdOf<T>,
+		proposal: HashOf<T>,
+		index: ProposalIndex,
+		approve: bool,
+	) -> Result<bool, DispatchError> {
+		pallet_collective::Pallet::<T, I>::do_vote(who, proposal, index, approve)
+	}
+
+	fn veto_proposal(proposal_hash: HashOf<T>) -> u32 {
+		pallet_collective::Pallet::<T, I>::do_disapprove_proposal(proposal_hash)
+	}
+
+	fn close_proposal(
+		proposal_hash: HashOf<T>,
+		proposal_index: ProposalIndex,
+		proposal_weight_bound: Weight,
+		length_bound: u32,
+	) -> DispatchResultWithPostInfo {
+		pallet_collective::Pallet::<T, I>::do_close(
+			proposal_hash,
+			proposal_index,
+			proposal_weight_bound,
+			length_bound,
+		)
+	}
+
+	fn proposal_of(proposal_hash: HashOf<T>) -> Option<ProposalOf<T, I>> {
+		pallet_collective::Pallet::<T, I>::proposal_of(proposal_hash)
 	}
 }
