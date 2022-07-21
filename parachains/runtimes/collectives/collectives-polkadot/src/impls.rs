98ee2e3738e86d94a57340f1dcd47ffe6b492924
muharem <ismailov.m.h@gmail.com>
2022-07-21 15:04:33 +0200

    fix typo

--- parachains/runtimes/collectives/collectives-polkadot/src/impls.rs
+++ parachains/runtimes/collectives/collectives-polkadot/src/impls.rs
@@ -1,7 +1,7 @@
 // Copyright (C) 2021 Parity Technologies (UK) Ltd.
 // SPDX-License-Identifier: Apache-2.0
 
-// &Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
