a04a38e7b0d02c6a2fe186d5b22b81481aed8652
Szegoo <sakacszergej@gmail.com>
2022-07-21 08:35:51 +0300

    fix

--- parachains/runtimes/assets/westmint/src/weights/pallet_uniques.rs
+++ parachains/runtimes/assets/westmint/src/weights/pallet_uniques.rs
@@ -254,7 +254,7 @@ impl<T: frame_system::Config> pallet_uniques::WeightInfo for WeightInfo<T> {
 	// Storage: Uniques Class (r:1 w:0)
 	fn try_increment_id() -> Weight {
 		(19_233_000 as Weight)
-			.saturating_add(RocksDbWeight::get().reads(2 as Weight))
-			.saturating_add(RocksDbWeight::get().writes(1 as Weight))
+			.saturating_add(T::DbWeight::get().reads(2 as Weight))
+			.saturating_add(T::DbWeight::get().writes(1 as Weight))
 	}
 }
