820ad4a1242bf8e3b444a17941baeb4f725fd264
Huang ZY <zhiyong0705@hotmail.com>
2022-07-30 01:41:44 +0800

    Fix

--- parachains/runtimes/testing/penpal/src/lib.rs
+++ parachains/runtimes/testing/penpal/src/lib.rs
@@ -535,7 +535,7 @@ construct_runtime!(
 		// Monetary stuff.
 		Balances: pallet_balances::{Pallet, Call, Storage, Config<T>, Event<T>} = 10,
 		TransactionPayment: pallet_transaction_payment::{Pallet, Storage, Event<T>} = 11,
-		AssetTxPayment: pallet_asset_tx_payment::{Pallet} = 12,
+		AssetTxPayment: pallet_asset_tx_payment::{Pallet, Event<T>} = 12,
 
 		// Collator support. The order of these 4 are important and shall not change.
 		Authorship: pallet_authorship::{Pallet, Call, Storage} = 20,
