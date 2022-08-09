01107b20796b7145e4835119034b378af600b132
joe petrowski <25483142+joepetrowski@users.noreply.github.com>
2022-08-09 18:15:11 +0200

    Update parachains/runtimes/collectives/collectives-polkadot/src/xcm_config.rs
    Co-authored-by: Keith Yeung <kungfukeith11@gmail.com>

--- parachains/runtimes/collectives/collectives-polkadot/src/xcm_config.rs
+++ parachains/runtimes/collectives/collectives-polkadot/src/xcm_config.rs
@@ -172,7 +172,7 @@ impl pallet_xcm::Config for Runtime {
 	type XcmRouter = XcmRouter;
 	// We support local origins dispatching XCM executions in principle...
 	type ExecuteXcmOrigin = EnsureXcmOrigin<Origin, LocalOriginToLocation>;
-	// ... but disallow generic XCM execution. As a result only teleports and reserve transfers are allowed.
+	// ... but disallow generic XCM execution. As a result only teleports are allowed.
 	type XcmExecuteFilter = Nothing;
 	type XcmExecutor = XcmExecutor<XcmConfig>;
 	type XcmTeleportFilter = Everything;
