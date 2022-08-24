dae095734e6afa63508890de50e5f0434780b5a2
Thomas Niederberger <781000+Niederb@users.noreply.github.com>
2022-08-24 11:49:37 +0200

    Update polkadot to v0.9.27 (#153)
    * Update polkadot version (Auto generated)
    
    * Update versions for internal dependencies (Auto generated)
    
    * Update toolchain to newest nightly
    
    * Modify open-runtime-module-library dependency to use polkadot-v0.9.27 branch
    
    * Run cargo update
    
    * Trait has a new parameter is_dev
    
    * Role::Light has been removed, so we can also remove the check
    
    * Add new Enum item
    
    * Modify enum handling to be closer to cumulus implementation
    
    * Update json files to polkadot v0.9.27
    
    * Increase version numbers
    
    * Update Cargo.lock
    
    * Bum diff libs according to cumulus changes in branch polkadot-v0.9.27
    
    * Switch to official polkadot-v0.9.27 branch
    
    * Update Cargo.lock
    
    Co-authored-by: echevrier <edith.chevrier@scs.ch>

--- polkadot-parachains/integritee-runtime/src/lib.rs
+++ polkadot-parachains/integritee-runtime/src/lib.rs
@@ -107,7 +107,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("integritee-parachain"),
 	impl_name: create_runtime_str!("integritee-full"),
 	authoring_version: 2,
-	spec_version: 24,
+	spec_version: 25,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 3,
