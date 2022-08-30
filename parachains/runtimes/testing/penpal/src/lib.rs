b5a952ba520aff237b2c580c83675191755ee6d1
Shawn Tabrizi <shawntabrizi@gmail.com>
2022-08-30 23:02:05 +0100

    cargo test -p collectives-polkadot-runtime

--- parachains/runtimes/testing/penpal/src/lib.rs
+++ parachains/runtimes/testing/penpal/src/lib.rs
@@ -148,7 +148,7 @@ impl WeightToFeePolynomial for WeightToFee {
 		// in Rococo, extrinsic base weight (smallest non-zero weight) is mapped to 1 MILLIUNIT:
 		// in our template, we map to 1/10 of that, or 1/10 MILLIUNIT
 		let p = MILLIUNIT / 10;
-		let q = 100 * Balance::from(ExtrinsicBaseWeight::get());
+		let q = 100 * Balance::from(ExtrinsicBaseWeight::get().ref_time());
 		smallvec![WeightToFeeCoefficient {
 			degree: 1,
 			negative: false,
