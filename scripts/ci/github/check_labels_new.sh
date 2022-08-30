1b9bd116fa3fd90c553f2ad83f67016a0367f071
alvicsam <alvicsam@gmail.com>
2022-08-30 18:03:53 +0200

    add labels

--- scripts/ci/github/check_labels_new.sh
+++ scripts/ci/github/check_labels_new.sh
@@ -3,13 +3,14 @@
 DIR=$(dirname -- "$0")
 
 # This is the new version using `ruled-labels`
+api_base="https://api.github.com/repos"
 repo="$GITHUB_REPOSITORY"
-pr="$GITHUB_PR"
+pr_id="$GITHUB_PR"
 
 pushd "$DIR/../ruled_labels" > /dev/null
 
-# TODO: Fetch the labels for the PR under test
-labels=("B0-Silent" "X1-Runtime" "P1" "D1-audited 👍")
+# Fetch the labels for the PR under test
+labels=$(curl -H "Authorization: token $GITHUB_PR_TOKEN" -s "$api_base/$repo/pulls/$pr_id" | jq ".labels | .[] " | grep '"name":' | tr -d ',' | sed 's/  "name": //g' | tr -d '"' | awk '{ORS=" "} {print$NF}')
 
 ruled-labels --version
-ruled-labels check --labels "${labels[@]}"
+ruled-labels check --labels "${labels}"
