07d4973a8de7e847349935f1399a8b4e6c060fe8
Shawn Tabrizi <shawntabrizi@gmail.com>
2022-08-30 22:11:07 +0100

    cargo test -p cumulus-pallet-xcmp-queue

--- parachains/runtimes/assets/statemint/src/xcm_config.rs
+++ parachains/runtimes/assets/statemint/src/xcm_config.rs
@@ -127,7 +127,7 @@ pub type XcmOriginToTransactDispatchOrigin = (
 
 parameter_types! {
 	// One XCM operation is 1_000_000_000 weight - almost certainly a conservative estimate.
-	pub UnitWeightCost: Weight = 1_000_000_000;
+	pub UnitWeightCost: u64 = 1_000_000_000;
 	pub const MaxInstructions: u32 = 100;
 	pub XcmAssetFeesReceiver: Option<AccountId> = Authorship::author();
 }
