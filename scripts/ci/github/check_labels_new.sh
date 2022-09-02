93432e29c403e651f7bb70d58c089b3e7441a862
Wilfried Kopp <wilfried@parity.io>
2022-09-02 17:18:05 +0200

    Fix new script

--- scripts/ci/github/check_labels_new.sh
+++ scripts/ci/github/check_labels_new.sh
@@ -7,10 +7,18 @@ api_base="https://api.github.com/repos"
 repo="$GITHUB_REPOSITORY"
 pr_id="$GITHUB_PR"
 
+echo "repo: $repo"
+echo "pr_ir: $pr_id"
+
 pushd "$DIR/../ruled_labels" > /dev/null
 
 # Fetch the labels for the PR under test
-labels=$(curl -H "Authorization: token $GITHUB_PR_TOKEN" -s "$api_base/$repo/pulls/$pr_id" | jq ".labels | .[] " | grep '"name":' | tr -d ',' | sed 's/  "name": //g' | tr -d '"' | awk '{ORS=" "} {print$NF}')
+labels="$(curl -H "Authorization: token $GITHUB_PR_TOKEN" -s "$api_base/$repo/pulls/$pr_id" | jq ".labels | .[] | .name" | tr '\n' ' ')"
+
+echo "labels: $labels"
 
 ruled-labels --version
-ruled-labels check --labels "${labels}"
+cmd='ruled-labels check --labels '${labels[@]}
+echo "cmd: $cmd"
+bash -c "$cmd"
+# ruled-labels check --labels ${labels[@]}
