4984ef30bf1eac29bd2c885756cf15979534ff61
Keith Yeung <kungfukeith11@gmail.com>
2022-09-27 16:53:10 +0800

    Remove unused import

--- pallets/dmp-queue/src/lib.rs
+++ pallets/dmp-queue/src/lib.rs
@@ -31,10 +31,7 @@ pub use pallet::*;
 use scale_info::TypeInfo;
 use sp_runtime::RuntimeDebug;
 use sp_std::{convert::TryFrom, prelude::*};
-use xcm::{
-	latest::{prelude::*, Weight as XCMWeight},
-	VersionedXcm, MAX_XCM_DECODE_DEPTH,
-};
+use xcm::{latest::prelude::*, VersionedXcm, MAX_XCM_DECODE_DEPTH};
 
 #[derive(Copy, Clone, Eq, PartialEq, Encode, Decode, RuntimeDebug, TypeInfo)]
 pub struct ConfigData {
