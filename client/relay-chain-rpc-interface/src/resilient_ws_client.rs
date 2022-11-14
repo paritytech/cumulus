1821ae17a50b3923d201206d85dc1f6a5a76a90d
Sebastian Kunert <skunert49@gmail.com>
2022-11-14 15:28:05 +0100

    Improve subscription handling

--- client/relay-chain-rpc-interface/src/resilient_ws_client.rs
+++ client/relay-chain-rpc-interface/src/resilient_ws_client.rs
@@ -1,4 +1,4 @@
-use std::sync::Arc;
+use std::{sync::Arc, time::Instant};
 
 use cumulus_primitives_core::relay_chain::Header as PHeader;
 use cumulus_relay_chain_interface::{RelayChainError, RelayChainResult};
@@ -10,7 +10,7 @@ use futures::{
 };
 use jsonrpsee::{
 	core::{
-		client::{Client as JsonRpcClient, ClientT, SubscriptionClientT},
+		client::{Client as JsonRpcClient, ClientT, Subscription, SubscriptionClientT},
 		Error as JsonRpseeError, JsonValue,
 	},
 	ws_client::WsClientBuilder,
@@ -121,6 +121,12 @@ fn handle_event_distribution(
 ) -> Result<(), String> {
 	match event {
 		Some(Ok(header)) => {
+			tracing::info!(
+				target: LOG_TARGET,
+				"Received block via RPC: #{} ({})",
+				header.number,
+				header.hash()
+			);
 			senders.retain_mut(|e| {
 				match e.try_send(header.clone()) {
 					// Receiver has been dropped, remove Sender from list.
@@ -152,6 +158,12 @@ struct ClientManager {
 	active_client: (usize, Arc<JsonRpcClient>),
 }
 
+struct RelayChainSubscriptions {
+	import_subscription: Subscription<PHeader>,
+	finalized_subscription: Subscription<PHeader>,
+	best_subscription: Subscription<PHeader>,
+}
+
 async fn find_next_client(
 	urls: &Vec<Url>,
 	starting_position: usize,
@@ -167,16 +179,38 @@ async fn find_next_client(
 	Err(())
 }
 
+async fn create_request(
+	client: Arc<JsonRpcClient>,
+	method: String,
+	params: Option<Vec<JsonValue>>,
+	response_sender: OneshotSender<Result<JsonValue, JsonRpseeError>>,
+) -> Result<(), RpcDispatcherMessage> {
+	let resp = client.request(&method, params.clone().map(|p| p.into())).await;
+
+	if let Err(JsonRpseeError::RestartNeeded(_)) = resp {
+		return Err(RpcDispatcherMessage::Request(method, params, response_sender));
+	}
+
+	if let Err(err) = response_sender.send(resp) {
+		tracing::error!(
+			target: LOG_TARGET,
+			?err,
+			"Recipient no longer interested in request result"
+		);
+	}
+	return Ok(());
+}
+
 impl ClientManager {
 	pub async fn new(urls: Vec<Url>) -> Result<Self, ()> {
 		if urls.is_empty() {
 			return Err(());
 		}
-		let active_client = find_next_client(&urls, 1).await?;
+		let active_client = find_next_client(&urls, 0).await?;
 		Ok(Self { urls, active_client })
 	}
 
-	pub async fn connect_to_new_rpc_server(&mut self) -> Result<(), ()> {
+	pub async fn connect_to_new_rpc_server(&mut self) -> Result<Arc<JsonRpcClient>, ()> {
 		tracing::info!(
 			target: LOG_TARGET,
 			is_connected = self.active_client.1.is_connected(),
@@ -184,7 +218,7 @@ impl ClientManager {
 		);
 		let new_active = find_next_client(&self.urls, self.active_client.0 + 1).await?;
 		self.active_client = new_active;
-		Ok(())
+		Ok(self.active_client.1.clone())
 	}
 
 	pub fn get_client(&mut self) -> Arc<JsonRpcClient> {
@@ -213,76 +247,38 @@ impl RpcStreamWorker {
 	/// 2. Distribute incoming import, best head and finalization notifications to registered listeners.
 	///    If an error occurs during sending, the receiver has been closed and we remove the sender from the list.
 	pub async fn run(mut self) {
-		let mut client_manager = ClientManager::new(self.ws_urls.clone()).await.expect("jojo");
+		let Ok(mut client_manager) = ClientManager::new(self.ws_urls.clone()).await else {
+			tracing::error!(target: LOG_TARGET, "No valid RPC url found. Stopping RPC worker.");
+			return;
+		};
 
-		let mut subscription_client = client_manager.get_client();
+		let active_client = client_manager.get_client();
 		let mut pending_requests = FuturesUnordered::new();
 
-		let mut import_sub = subscription_client
-			.subscribe::<PHeader>("chain_subscribeAllHeads", None, "chain_unsubscribeAllHeads")
-			.await
-			.expect("should not fail")
-			.fuse();
-
-		let mut best_head_sub = subscription_client
-			.subscribe::<PHeader>(
-				"chain_subscribeNewHeads",
-				None,
-				"chain_unsubscribeFinalizedHeads",
-			)
-			.await
-			.expect("should not fail");
-
-		let mut finalized_sub = subscription_client
-			.subscribe::<PHeader>(
-				"chain_subscribeFinalizedHeads",
-				None,
-				"chain_unsubscribeFinalizedHeads",
-			)
-			.await
-			.expect("should not fail")
-			.fuse();
+		let Ok(mut subscriptions) = get_subscriptions(&active_client).await else {
+			return;
+		};
 
 		let mut reset_streams = false;
 		loop {
 			if reset_streams {
-				if let Err(()) = client_manager.connect_to_new_rpc_server().await {
+				let Ok(active_client) = client_manager.connect_to_new_rpc_server().await else {
 					tracing::error!(
 						target: LOG_TARGET,
 						"Unable to find valid external RPC server, shutting down."
 					);
+					return;
 				};
-				subscription_client = client_manager.get_client();
-				import_sub = subscription_client
-					.subscribe::<PHeader>(
-						"chain_subscribeAllHeads",
-						None,
-						"chain_unsubscribeAllHeads",
-					)
-					.await
-					.expect("should not fail")
-					.fuse();
 
-				best_head_sub = subscription_client
-					.subscribe::<PHeader>(
-						"chain_subscribeNewHeads",
-						None,
-						"chain_unsubscribeFinalizedHeads",
-					)
-					.await
-					.expect("should not fail");
+				if let Ok(new_subscriptions) = get_subscriptions(&active_client).await {
+					subscriptions = new_subscriptions;
+				} else {
+					return;
+				};
 
-				finalized_sub = subscription_client
-					.subscribe::<PHeader>(
-						"chain_subscribeFinalizedHeads",
-						None,
-						"chain_unsubscribeFinalizedHeads",
-					)
-					.await
-					.expect("should not fail")
-					.fuse();
 				reset_streams = false;
 			};
+
 			tokio::select! {
 				evt = self.client_receiver.recv() => match evt {
 					Some(RpcDispatcherMessage::RegisterBestHeadListener(tx)) => {
@@ -295,19 +291,8 @@ impl RpcStreamWorker {
 						self.finalized_header_listeners.push(tx)
 					},
 					Some(RpcDispatcherMessage::Request(method, params, response_sender)) => {
-						let closure_client = client_manager.get_client();
-						pending_requests.push(async move {
-							let resp = closure_client.request(&method, params.clone().map(|p| p.into())).await;
-
-							if let Err(JsonRpseeError::RestartNeeded(_)) = resp {
-								return Err(RpcDispatcherMessage::Request(method, params, response_sender));
-							}
-
-							if let Err(err) = response_sender.send(resp) {
-								tracing::error!(target: LOG_TARGET, ?err, "Recipient no longer interested in request result");
-							}
-							return Ok(());
-						});
+						let client = client_manager.get_client();
+						pending_requests.push(create_request(client, method, params, response_sender));
 					},
 					None => {
 						tracing::error!(target: LOG_TARGET, "RPC client receiver closed. Stopping RPC Worker.");
@@ -321,19 +306,19 @@ impl RpcStreamWorker {
 						self.self_sender.send(req).await.expect("This should work");
 					}
 				},
-				import_event = import_sub.next(), if !reset_streams => {
+				import_event = subscriptions.import_subscription.next() => {
 					if let Err(err) = handle_event_distribution(import_event, &mut self.imported_header_listeners) {
 						tracing::error!(target: LOG_TARGET, err, "Encountered error while processing imported header notification.");
 						reset_streams = true;
 					}
 				},
-				best_header_event = best_head_sub.next(), if !reset_streams => {
+				best_header_event = subscriptions.best_subscription.next() => {
 					if let Err(err) = handle_event_distribution(best_header_event, &mut self.best_header_listeners) {
 						tracing::error!(target: LOG_TARGET, err, "Encountered error while processing best header notification.");
 						reset_streams = true;
 					}
 				}
-				finalized_event = finalized_sub.next(), if !reset_streams => {
+				finalized_event = subscriptions.finalized_subscription.next() => {
 					if let Err(err) = handle_event_distribution(finalized_event, &mut self.finalized_header_listeners) {
 						tracing::error!(target: LOG_TARGET, err, "Encountered error while processing finalized header notification.");
 						reset_streams = true;
@@ -342,4 +327,53 @@ impl RpcStreamWorker {
 			}
 		}
 	}
+}
+
+async fn get_imported_header_subscription(
+	subscription_client: &Arc<JsonRpcClient>,
+) -> Result<Subscription<PHeader>, JsonRpseeError> {
+	subscription_client
+		.subscribe::<PHeader>("chain_subscribeAllHeads", None, "chain_unsubscribeAllHeads")
+		.await
+		.map_err(|e| {
+			tracing::error!(target: LOG_TARGET, ?e, "Unable to open subscription.");
+			e
+		})
+}
+
+async fn get_best_head_subscription(
+	subscription_client: &Arc<JsonRpcClient>,
+) -> Result<Subscription<PHeader>, JsonRpseeError> {
+	subscription_client
+		.subscribe::<PHeader>("chain_subscribeNewHeads", None, "chain_unsubscribeNewHeads")
+		.await
+		.map_err(|e| {
+			tracing::error!(target: LOG_TARGET, ?e, "Unable to open subscription.");
+			e
+		})
+}
+
+async fn get_finalized_head_subscription(
+	subscription_client: &Arc<JsonRpcClient>,
+) -> Result<Subscription<PHeader>, JsonRpseeError> {
+	subscription_client
+		.subscribe::<PHeader>(
+			"chain_subscribeFinalizedHeads",
+			None,
+			"chain_unsubscribeFinalizedHeads",
+		)
+		.await
+		.map_err(|e| {
+			tracing::error!(target: LOG_TARGET, ?e, "Unable to open subscription.");
+			e
+		})
+}
+
+async fn get_subscriptions(
+	active_client: &Arc<JsonRpcClient>,
+) -> Result<RelayChainSubscriptions, JsonRpseeError> {
+	let import_subscription = get_imported_header_subscription(&active_client).await?;
+	let best_subscription = get_best_head_subscription(&active_client).await?;
+	let finalized_subscription = get_finalized_head_subscription(&active_client).await?;
+	Ok(RelayChainSubscriptions { import_subscription, best_subscription, finalized_subscription })
 }
