05985d77f6c755e30d8e6e40047d1120514eccf9
Anthony Lazam <xlzm.tech@gmail.com>
2022-11-17 20:00:17 +0800

    Update BridgeHub runtime version

--- parachains/runtimes/bridge-hubs/bridge-hub-rococo/src/lib.rs
+++ parachains/runtimes/bridge-hubs/bridge-hub-rococo/src/lib.rs
@@ -168,7 +168,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("bridge-hub-rococo"),
 	impl_name: create_runtime_str!("bridge-hub-rococo"),
 	authoring_version: 1,
-	spec_version: 1,
+	spec_version: 9300,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
