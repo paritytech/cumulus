2df1d63634504b2a2838badc1c08039dbc17f56e
Damilare <dharjeezy@gmail.com>
2022-11-24 22:01:15 +0100

    v2 migration for assets

--- parachains/runtimes/assets/statemint/src/lib.rs
+++ parachains/runtimes/assets/statemint/src/lib.rs
@@ -665,7 +665,7 @@ pub type Executive = frame_executive::Executive<
 	frame_system::ChainContext<Runtime>,
 	Runtime,
 	AllPalletsWithSystem,
-	pallet_assets::migration::v1::MigrateToV1<Runtime>,
+	pallet_assets::migration::v2::MigrateToV2<Runtime>,
 >;
 
 #[cfg(feature = "runtime-benchmarks")]
