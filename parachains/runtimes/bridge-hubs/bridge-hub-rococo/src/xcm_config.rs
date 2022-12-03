ff250267395998a7d150e5e46182bc1af9625455
Keith Yeung <kungfukeith11@gmail.com>
2022-12-04 01:44:52 +0900

    Add TODO

--- parachains/runtimes/bridge-hubs/bridge-hub-rococo/src/xcm_config.rs
+++ parachains/runtimes/bridge-hubs/bridge-hub-rococo/src/xcm_config.rs
@@ -195,7 +195,7 @@ pub type LocalOriginToLocation = SignedToAccountId32<RuntimeOrigin, AccountId, R
 /// queues.
 pub type XcmRouter = (
 	// Two routers - use UMP to communicate with the relay chain:
-	cumulus_primitives_utility::ParentAsUmp<ParachainSystem, PolkadotXcm>,
+	cumulus_primitives_utility::ParentAsUmp<ParachainSystem, PolkadotXcm>, // TODO: Figure out 3rd type arg
 	// ..and XCMP to communicate with the sibling chains.
 	XcmpQueue,
 );
