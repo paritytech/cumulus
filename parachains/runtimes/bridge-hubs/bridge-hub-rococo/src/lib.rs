0d69b256ad8bae8c335ea14fa9b80bde35b969b1
Branislav Kontur <bkontur@gmail.com>
2022-12-05 12:42:54 +0100

    Change runtime version

--- parachains/runtimes/bridge-hubs/bridge-hub-rococo/src/lib.rs
+++ parachains/runtimes/bridge-hubs/bridge-hub-rococo/src/lib.rs
@@ -170,7 +170,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("bridge-hub-rococo"),
 	impl_name: create_runtime_str!("bridge-hub-rococo"),
 	authoring_version: 1,
-	spec_version: 9300,
+	spec_version: 9301,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
