f366c7f52057bc143c55ad1e9ed3ea7a3a7d8b7b
Keith Yeung <kungfukeith11@gmail.com>
2022-12-07 04:41:07 +0900

    Fixes

--- parachains/runtimes/testing/rococo-parachain/src/lib.rs
+++ parachains/runtimes/testing/rococo-parachain/src/lib.rs
@@ -419,6 +419,7 @@ impl xcm_executor::Config for XcmConfig {
 	type MessageExporter = ();
 	type UniversalAliases = Nothing;
 	type CallDispatcher = RuntimeCall;
+	type SafeCallFilter = Everything;
 }
 
 /// Local origins on this chain are allowed to dispatch XCM sends/executions.
