75c14a5bc8636d0e33624903d7c3ffce2fd23092
Keith Yeung <kungfukeith11@gmail.com>
2022-12-08 03:37:18 +0900

    Fixes

--- parachains/runtimes/starters/seedling/src/lib.rs
+++ parachains/runtimes/starters/seedling/src/lib.rs
@@ -87,7 +87,7 @@ const AVERAGE_ON_INITIALIZE_RATIO: Perbill = Perbill::from_percent(10);
 /// by  Operational  extrinsics.
 const NORMAL_DISPATCH_RATIO: Perbill = Perbill::from_percent(75);
 /// We allow for .5 seconds of compute with a 12 second average block time.
-const MAXIMUM_BLOCK_WEIGHT: Weight = Weight::from_ref_time(
+const MAXIMUM_BLOCK_WEIGHT: Weight = Weight::from_parts(
 	WEIGHT_REF_TIME_PER_SECOND.saturating_div(2),
 	cumulus_primitives_core::relay_chain::v2::MAX_POV_SIZE as u64,
 );
