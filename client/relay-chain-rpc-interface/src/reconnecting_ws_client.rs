3b27fd83977a758ff670de3265343a3305b116a0
Sebastian Kunert <skunert49@gmail.com>
2022-12-13 17:08:02 +0100

    fmt

--- client/relay-chain-rpc-interface/src/reconnecting_ws_client.rs
+++ client/relay-chain-rpc-interface/src/reconnecting_ws_client.rs
@@ -182,7 +182,7 @@ async fn connect_next_available_rpc_server(
 		let index = (starting_position + counter) % urls.len();
 		tracing::info!(target: LOG_TARGET, index, ?url, "Connecting to RPC node",);
 		if let Ok(ws_client) = WsClientBuilder::default().build(url).await {
-			return Ok((index, Arc::new(ws_client)));
+			return Ok((index, Arc::new(ws_client)))
 		};
 	}
 	Err(())
@@ -191,7 +191,7 @@ async fn connect_next_available_rpc_server(
 impl ClientManager {
 	pub async fn new(urls: Vec<Url>) -> Result<Self, ()> {
 		if urls.is_empty() {
-			return Err(());
+			return Err(())
 		}
 		let active_client = connect_next_available_rpc_server(&urls, 0).await?;
 		Ok(Self { urls, active_client: active_client.1, active_index: active_client.0 })
@@ -267,7 +267,7 @@ impl ClientManager {
 			// the websocket connection is dead and requires a restart.
 			// Other errors should be forwarded to the request caller.
 			if let Err(JsonRpseeError::RestartNeeded(_)) = resp {
-				return Err(RpcDispatcherMessage::Request(method, params, response_sender));
+				return Err(RpcDispatcherMessage::Request(method, params, response_sender))
 			}
 
 			if let Err(err) = response_sender.send(resp) {
@@ -313,9 +313,8 @@ impl ReconnectingWebsocketWorker {
 			match self.client_receiver.try_recv() {
 				Ok(val) => tmp_request_storage.push(val),
 				Err(TryRecvError::Empty) => break,
-				Err(_) => {
-					return Err("Can not fetch values from client receiver channel.".to_string())
-				},
+				Err(_) =>
+					return Err("Can not fetch values from client receiver channel.".to_string()),
 			}
 		}
 
@@ -329,7 +328,7 @@ impl ReconnectingWebsocketWorker {
 					return Err(format!(
 						"Unable to retry requests, queue is unexpectedly full. err: {:?}",
 						err
-					));
+					))
 				};
 			}
 		}
@@ -339,12 +338,12 @@ impl ReconnectingWebsocketWorker {
 				return Err(format!(
 					"Unable to retry requests, queue is unexpectedly full. err: {:?}",
 					err
-				));
+				))
 			};
 		}
 
 		if client_manager.connect_to_new_rpc_server().await.is_err() {
-			return Err(format!("Unable to find valid external RPC server, shutting down."));
+			return Err(format!("Unable to find valid external RPC server, shutting down."))
 		};
 
 		client_manager.get_subscriptions().await.map_err(|e| {
@@ -389,7 +388,7 @@ impl ReconnectingWebsocketWorker {
 							message,
 							"Unable to reconnect, stopping worker."
 						);
-						return;
+						return
 					},
 				}
 				should_reconnect = false;
