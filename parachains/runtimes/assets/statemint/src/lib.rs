52417e76cae100f2549bf44c8bb0b246e8692177
Egor_P <egor@parity.io>
2022-12-21 17:42:43 +0100

    bump txn versions (#2018) (#2020)

--- parachains/runtimes/assets/statemint/src/lib.rs
+++ parachains/runtimes/assets/statemint/src/lib.rs
@@ -120,7 +120,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_version: 9360,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
-	transaction_version: 10,
+	transaction_version: 11,
 	state_version: 0,
 };
 
