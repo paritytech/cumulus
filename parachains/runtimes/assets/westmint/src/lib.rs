97684f2340fb4a493beaf02b15d8406f89b1effb
Gav <gavin@parity.io>
2022-12-23 16:22:29 +0100

    Remove old migrations

--- parachains/runtimes/assets/westmint/src/lib.rs
+++ parachains/runtimes/assets/westmint/src/lib.rs
@@ -611,10 +611,7 @@ pub type UncheckedExtrinsic =
 /// Extrinsic type that has already been checked.
 pub type CheckedExtrinsic = generic::CheckedExtrinsic<AccountId, RuntimeCall, SignedExtra>;
 
-pub type Migrations = (
-	pallet_balances::migration::MigrateToTrackInactive<Runtime, xcm_config::CheckingAccount>,
-	pallet_assets::migration::v1::MigrateToV1<Runtime>,
-);
+pub type Migrations = (pallet_assets::migration::v1::MigrateToV1<Runtime>,);
 
 /// Executive: handles dispatch to the various modules.
 pub type Executive = frame_executive::Executive<
