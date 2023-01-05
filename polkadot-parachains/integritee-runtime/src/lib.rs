615cef6ef055beee52a6392bb79b85e4c6e0bac8
clangenb <37865735+clangenb@users.noreply.github.com>
2023-01-05 18:51:29 +0100

    bump versions of the runtimes and the collator (#182)

--- polkadot-parachains/integritee-runtime/src/lib.rs
+++ polkadot-parachains/integritee-runtime/src/lib.rs
@@ -101,10 +101,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("integritee-parachain"),
 	impl_name: create_runtime_str!("integritee-full"),
 	authoring_version: 2,
-	spec_version: 27,
+	spec_version: 28,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
-	transaction_version: 3,
+	transaction_version: 4,
 	state_version: 0,
 };
 
