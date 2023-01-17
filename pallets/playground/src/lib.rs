1530a9bbbed73c01c8fb967cacdb3457e2cc38d1
Daniel Bigos <daniel.bigos@parity.io>
2023-01-17 12:34:15 +0100

    feat: add bridges playground pallet

---
+++ pallets/playground/src/lib.rs
@@ -0,0 +1,44 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+pub use pallet::*;
+
+#[cfg(test)]
+mod mock;
+
+#[frame_support::pallet]
+pub mod pallet {
+	use frame_support::{dispatch::DispatchResultWithPostInfo, pallet_prelude::*};
+	use frame_system::pallet_prelude::*;
+
+	#[pallet::config]
+	pub trait Config: frame_system::Config {
+		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
+	}
+
+	#[pallet::pallet]
+	#[pallet::generate_store(pub(super) trait Store)]
+	pub struct Pallet<T>(_);
+
+	#[pallet::event]
+	#[pallet::generate_deposit(pub(super) fn deposit_event)]
+	pub enum Event<T: Config> {
+		Triggered(u32),
+	}
+
+	#[pallet::error]
+	pub enum Error<T> {
+		InvalidMultiLocation,
+	}
+
+	#[pallet::hooks]
+	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {}
+
+	#[pallet::call]
+	impl<T: Config> Pallet<T> {
+		#[pallet::weight(Weight::from_ref_time(10_000))]
+		pub fn do_something(_origin: OriginFor<T>, something: u32) -> DispatchResult {
+			Self::deposit_event(Event::Triggered(something));
+			Ok(())
+		}
+	}
+}
