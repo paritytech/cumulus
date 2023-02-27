9131b556e18b14c63174ddb2492a616493747023
Egor_P <egor@parity.io>
2023-02-27 12:27:46 +0100

    Tests + backport fix for teleport_assets (ReceiveTeleportedAsset) (#2235) (#2239)
    * Wwstmint test for ReceiveTeleportedAsset
    
    * Missing fix for `weigh_multi_assets`
    
    * Added tests for statemine/statemint
    
    * [Enhancement] Use XCM V3 for initiate_teleport weight calc (#2102)
    
    * [Enhancement] Use XCM V3 for initiate_teleport weight calc
    
    * deref
    
    * replicate in all the runtimes
    
    * fmt
    
    * better handling for AllOf
    
    * fmt
    
    * small type fix
    
    * replicate the fix for all runtimes
    
    ---------
    
    Co-authored-by: parity-processbot <>
    
    * removed `frame_support::sp_tracing::try_init_simple();`
    
    * Review fixes
    
    * Removed `as u64`
    
    ---------
    
    Co-authored-by: Branislav Kontur <bkontur@gmail.com>
    Co-authored-by: Roman Useinov <roman.useinov@gmail.com>

--- parachains/runtimes/bridge-hubs/bridge-hub-polkadot/src/weights/xcm/mod.rs
+++ parachains/runtimes/bridge-hubs/bridge-hub-polkadot/src/weights/xcm/mod.rs
@@ -17,7 +17,7 @@
 mod pallet_xcm_benchmarks_fungible;
 mod pallet_xcm_benchmarks_generic;
 
-use crate::Runtime;
+use crate::{xcm_config::MaxAssetsIntoHolding, Runtime};
 use frame_support::weights::Weight;
 use pallet_xcm_benchmarks_fungible::WeightInfo as XcmFungibleWeight;
 use pallet_xcm_benchmarks_generic::WeightInfo as XcmGeneric;
@@ -28,14 +28,25 @@ trait WeighMultiAssets {
 	fn weigh_multi_assets(&self, weight: Weight) -> Weight;
 }
 
-const MAX_ASSETS: u32 = 100;
+const MAX_ASSETS: u64 = 100;
 
 impl WeighMultiAssets for MultiAssetFilter {
 	fn weigh_multi_assets(&self, weight: Weight) -> Weight {
 		match self {
 			Self::Definite(assets) =>
 				weight.saturating_mul(assets.inner().into_iter().count() as u64),
-			Self::Wild(_) => weight.saturating_mul(MAX_ASSETS as u64),
+			Self::Wild(asset) => match asset {
+				All => weight.saturating_mul(MAX_ASSETS),
+				AllOf { fun, .. } => match fun {
+					WildFungibility::Fungible => weight,
+					// Magic number 2 has to do with the fact that we could have up to 2 times
+					// MaxAssetsIntoHolding in the worst-case scenario.
+					WildFungibility::NonFungible =>
+						weight.saturating_mul((MaxAssetsIntoHolding::get() * 2) as u64),
+				},
+				AllCounted(count) => weight.saturating_mul(MAX_ASSETS.min(*count as u64)),
+				AllOfCounted { count, .. } => weight.saturating_mul(MAX_ASSETS.min(*count as u64)),
+			},
 		}
 	}
 }
