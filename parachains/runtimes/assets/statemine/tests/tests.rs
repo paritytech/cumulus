b5ca0bfeb6c381d01f4ac000dea09396d0e48b2d
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2023-02-27 17:30:29 +0100

    Migrate to from_parts
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

--- parachains/runtimes/assets/statemine/tests/tests.rs
+++ parachains/runtimes/assets/statemine/tests/tests.rs
@@ -63,7 +63,7 @@ fn test_asset_xcm_trader() {
 			RuntimeHelper::<Runtime>::run_to_block(2, Some(AccountId::from(ALICE)));
 
 			// We are going to buy 4e9 weight
-			let bought = Weight::from_ref_time(4_000_000_000u64);
+			let bought = Weight::from_parts(4_000_000_000u64, 0);
 
 			// Lets calculate amount needed
 			let asset_amount_needed =
@@ -139,7 +139,7 @@ fn test_asset_xcm_trader_with_refund() {
 			RuntimeHelper::<Runtime>::run_to_block(2, Some(AccountId::from(ALICE)));
 
 			// We are going to buy 4e9 weight
-			let bought = Weight::from_ref_time(4_000_000_000u64);
+			let bought = Weight::from_parts(4_000_000_000u64, 0);
 
 			let asset_multilocation = MultiLocation::new(
 				0,
@@ -217,7 +217,7 @@ fn test_asset_xcm_trader_refund_not_possible_since_amount_less_than_ed() {
 			RuntimeHelper::<Runtime>::run_to_block(2, Some(AccountId::from(ALICE)));
 
 			// We are going to buy small amount
-			let bought = Weight::from_ref_time(500_000_000u64);
+			let bought = Weight::from_parts(500_000_000u64, 0);
 
 			let asset_multilocation = MultiLocation::new(
 				0,
@@ -277,7 +277,7 @@ fn test_that_buying_ed_refund_does_not_refund() {
 			RuntimeHelper::<Runtime>::run_to_block(2, Some(AccountId::from(ALICE)));
 
 			// We are gonna buy ED
-			let bought = Weight::from_ref_time(ExistentialDeposit::get().try_into().unwrap());
+			let bought = Weight::from_parts(ExistentialDeposit::get().try_into().unwrap(), 0);
 
 			let asset_multilocation = MultiLocation::new(
 				0,
@@ -358,7 +358,7 @@ fn test_asset_xcm_trader_not_possible_for_non_sufficient_assets() {
 			RuntimeHelper::<Runtime>::run_to_block(2, Some(AccountId::from(ALICE)));
 
 			// We are going to buy 4e9 weight
-			let bought = Weight::from_ref_time(4_000_000_000u64);
+			let bought = Weight::from_parts(4_000_000_000u64, 0);
 
 			// lets calculate amount needed
 			let asset_amount_needed = WeightToFee::weight_to_fee(&bought);
