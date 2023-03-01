9c652c394fbc051213764489899bde7080965458
Serban Iorga <serban@parity.io>
2023-03-01 14:32:04 +0200

    Update RBH/WBH spec version

--- parachains/runtimes/bridge-hubs/bridge-hub-rococo/src/lib.rs
+++ parachains/runtimes/bridge-hubs/bridge-hub-rococo/src/lib.rs
@@ -149,7 +149,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("bridge-hub-rococo"),
 	impl_name: create_runtime_str!("bridge-hub-rococo"),
 	authoring_version: 1,
-	spec_version: 9371,
+	spec_version: 9372,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
