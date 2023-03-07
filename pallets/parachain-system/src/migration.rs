eeefeb0acf14783ed9577d37f6afc1bac2902c3e
Vivek Pandya <vivekvpandya@gmail.com>
2023-03-07 16:47:57 +0530

    Remove Store trait usage from parachain-system pallet

--- pallets/parachain-system/src/migration.rs
+++ pallets/parachain-system/src/migration.rs
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Cumulus.  If not, see <http://www.gnu.org/licenses/>.
 
-use crate::{Config, Pallet, Store};
+use crate::{Config, Pallet, ReservedDmpWeightOverride, ReservedXcmpWeightOverride };
 use frame_support::{
 	traits::{Get, StorageVersion},
 	weights::Weight,
@@ -52,7 +52,7 @@ mod v2 {
 	pub fn migrate<T: Config>() -> Weight {
 		let translate = |pre: u64| -> Weight { Weight::from_parts(pre, DEFAULT_POV_SIZE) };
 
-		if <Pallet<T> as Store>::ReservedXcmpWeightOverride::translate(|pre| pre.map(translate))
+		if ReservedXcmpWeightOverride::<T>::translate(|pre| pre.map(translate))
 			.is_err()
 		{
 			log::error!(
@@ -61,7 +61,7 @@ mod v2 {
 			);
 		}
 
-		if <Pallet<T> as Store>::ReservedDmpWeightOverride::translate(|pre| pre.map(translate))
+		if ReservedDmpWeightOverride::<T>::translate(|pre| pre.map(translate))
 			.is_err()
 		{
 			log::error!(
