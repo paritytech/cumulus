9b4e5d37fb5d33e6d539187a13ed62182afbc9b7
Sebastian Kunert <skunert49@gmail.com>
2023-03-31 16:51:37 +0200

    Remove `--tmp` flag

--- polkadot-parachain/src/cli.rs
+++ polkadot-parachain/src/cli.rs
@@ -66,9 +66,9 @@ pub enum Subcommand {
 
 const AFTER_HELP_EXAMPLE: &str = color_print::cstr!(
 	r#"<bold><underline>Examples:</></>
-   <bold>polkadot-parachain --chain statemint --tmp --sync warp -- --chain polkadot</>
+   <bold>polkadot-parachain --chain statemint --sync warp -- --chain polkadot</>
            Launch a warp-syncing full node of the <italic>statemint</> parachain on the <italic>polkadot</> relay chain.
-   <bold>polkadot-parachain --chain statemint --tmp --sync warp --relay-chain-rpc-url ws://rpc.example.com -- --chain polkadot</>
+   <bold>polkadot-parachain --chain statemint --sync warp --relay-chain-rpc-url ws://rpc.example.com -- --chain polkadot</>
            Launch a full node of the <italic>statemint</> parachain on the <italic>polkadot</> relay chain.
            Uses <italic>ws://rpc.example.com</> as remote relay chain node.
  "#
