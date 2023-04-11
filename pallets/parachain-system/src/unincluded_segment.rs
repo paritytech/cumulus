8617f8cbe133436aafcb9d36f3ea52907b6cf951
Chris Sosnin <chris125_@live.com>
2023-04-12 02:28:38 +0400

    doc comments

--- pallets/parachain-system/src/unincluded_segment.rs
+++ pallets/parachain-system/src/unincluded_segment.rs
@@ -14,42 +14,93 @@
 // You should have received a copy of the GNU General Public License
 // along with Cumulus.  If not, see <http://www.gnu.org/licenses/>.
 
+//! Primitives used for tracking message queues constraints in an unincluded block segment
+//! of the parachain.
+//!
+//! Unincluded segment describes a chain of latest included block descendants, which are not yet
+//! sent to relay chain.
+
 use codec::{Decode, Encode};
 use cumulus_primitives_core::{relay_chain, ParaId};
 use scale_info::TypeInfo;
 use sp_std::collections::btree_map::BTreeMap;
 
+/// Constraints on outbound HRMP channel.
 pub struct HrmpOutboundLimits {
+	/// The maximum bytes that can be written to the channel.
 	pub bytes_remaining: u32,
+	/// The maximum messages that can be written to the channel.
 	pub messages_remaining: u32,
 }
 
+/// Constraints imposed on the entire segment, i.e. based on the latest included parablock.
 pub struct TotalBandwidthLimits {
+	/// The amount of UMP messages remaining.
 	pub ump_messages_remaining: u32,
+	/// The amount of UMP bytes remaining.
 	pub ump_bytes_remaining: u32,
+	/// The limitations of all registered outbound HRMP channels.
 	pub hrmp_outgoing: BTreeMap<ParaId, HrmpOutboundLimits>,
+	/// Number of remaining DMP messages.
 	pub dmp_remaining_messages: u32,
 }
 
+/// The error type for updating bandwidth used by a segment.
 pub enum LimitExceededError {
-	HrmpMessagesOverflow { recipient: ParaId, messages_remaining: u32, messages_submitted: u32 },
-	HrmpBytesOverflow { recipient: ParaId, bytes_remaining: u32, bytes_submitted: u32 },
-	UmpMessagesOverflow { messages_remaining: u32, messages_submitted: u32 },
-	UmpBytesOverflow { bytes_remaining: u32, bytes_submitted: u32 },
-	DmpMessagesUnderflow { messages_remaining: u32, messages_processed: u32 },
+	/// Too many messages submitted to HRMP channel.
+	HrmpMessagesOverflow {
+		/// Parachain id of the recipient.
+		recipient: ParaId,
+		/// The amount of remaining messages in the capacity of the channel.
+		messages_remaining: u32,
+		/// The amount of messages submitted to the channel.
+		messages_submitted: u32,
+	},
+	/// Too many bytes submitted to HRMP channel.
+	HrmpBytesOverflow {
+		/// Parachain id of the recipient.
+		recipient: ParaId,
+		/// The amount of remaining bytes in the capacity of the channel.
+		bytes_remaining: u32,
+		/// The amount of bytes submitted to the channel.
+		bytes_submitted: u32,
+	},
+	UmpMessagesOverflow {
+		messages_remaining: u32,
+		messages_submitted: u32,
+	},
+	/// Too many messages submitted to UMP queue.
+	UmpBytesOverflow {
+		/// The amount of remaining messages in the capacity of UMP.
+		bytes_remaining: u32,
+		/// The amount of messages submitted to UMP.
+		bytes_submitted: u32,
+	},
+	/// Too many messages processed from DMP.
+	DmpMessagesUnderflow {
+		/// The amount of messages waiting to be processed from DMP.
+		messages_remaining: u32,
+		/// The amount of DMP messages processed.
+		messages_processed: u32,
+	},
 }
 
+/// The number of messages and size in bytes submitted to HRMP channel.
 #[derive(Default, Copy, Clone, Encode, Decode, TypeInfo)]
-pub struct HrmpChannelSize {
+pub struct HrmpChannelUpdate {
+	/// The amount of messages submitted to the channel.
 	pub msg_count: u32,
+	/// The amount of bytes submitted to the channel.
 	pub total_bytes: u32,
 }
 
-impl HrmpChannelSize {
+impl HrmpChannelUpdate {
+	/// Returns `true` if the update is empty, `false` otherwise.
 	fn is_empty(&self) -> bool {
 		self.msg_count == 0 && self.total_bytes == 0
 	}
 
+	/// Tries to append another update, respecting given bandwidth limits.
 	fn append(
 		&self,
 		other: &Self,
@@ -83,21 +134,31 @@ impl HrmpChannelSize {
 		Ok(new)
 	}
 
+	/// Subtracts previously added channel update.
 	fn subtract(&mut self, other: &Self) {
 		self.msg_count -= other.msg_count;
 		self.total_bytes -= other.total_bytes;
 	}
 }
 
+/// Bandwidth used by a parachain block(s).
+///
+/// This struct can be created with pub items, however, it should
+/// never hit the storage directly to avoid bypassing limitations checks.
 #[derive(Default, Clone, Encode, Decode, TypeInfo)]
 pub struct UsedBandwidth {
+	/// The amount of UMP messages sent.
 	pub ump_msg_count: u32,
+	/// The amount of UMP bytes sent.
 	pub ump_total_bytes: u32,
-	pub hrmp_outgoing: BTreeMap<ParaId, HrmpChannelSize>,
+	/// Outbound HRMP channels updates.
+	pub hrmp_outgoing: BTreeMap<ParaId, HrmpChannelUpdate>,
+	/// The amount of DMP messages processed.
 	pub dmp_processed_count: u32,
 }
 
 impl UsedBandwidth {
+	/// Tries to append another update, respecting given bandwidth limits.
 	fn append(
 		&self,
 		other: &Self,
@@ -135,6 +196,7 @@ impl UsedBandwidth {
 		Ok(new)
 	}
 
+	/// Subtracts previously added bandwidth update.
 	fn subtract(&mut self, other: &Self) {
 		self.ump_msg_count -= other.ump_msg_count;
 		self.ump_total_bytes -= other.ump_total_bytes;
@@ -152,29 +214,40 @@ impl UsedBandwidth {
 	}
 }
 
+/// Ancestors of the block being currently executed which are not yet included
+/// into the relay chain.
 #[derive(Encode, Decode, TypeInfo)]
 pub struct Ancestor {
+	/// Bandwidth used by this block.
 	used_bandwidth: UsedBandwidth,
+	/// Output head data of this block.
 	para_head: relay_chain::HeadData,
 }
 
 impl Ancestor {
+	/// Returns [`UsedBandwidth`] of this block.
 	pub fn used_bandwidth(&self) -> &UsedBandwidth {
 		&self.used_bandwidth
 	}
 
+	/// Returns [output head data](`relay_chain::HeadData`) of this block.
 	pub fn para_head(&self) -> &relay_chain::HeadData {
 		&self.para_head
 	}
 }
 
+/// Struct that keeps track of bandwidth used by the unincluded part of the chain
+/// along with the latest HRMP watermark.
 #[derive(Encode, Decode, TypeInfo)]
 pub struct SegmentTracker {
+	/// Bandwidth used by the segment.
 	used_bandwidth: UsedBandwidth,
+	/// The mark which specifies the block number up to which all inbound HRMP messages are processed.
 	hrmp_watermark: relay_chain::BlockNumber,
 }
 
 impl SegmentTracker {
+	/// Tries to append another block to the tracker, respecting given bandwidth limits.
 	pub fn append(
 		&mut self,
 		block: &Ancestor,
@@ -187,6 +260,7 @@ impl SegmentTracker {
 		Ok(())
 	}
 
+	/// Removes previously added block from the tracker.
 	pub fn subtract(&mut self, block: &Ancestor) {
 		self.used_bandwidth.subtract(block.used_bandwidth());
 	}
