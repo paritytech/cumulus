7be75928352565b707a0621e997a278adedd75ed
Branislav Kontur <bkontur@gmail.com>
2023-04-21 11:29:13 +0200

    Removed unused functions

--- polkadot-parachain/src/chain_spec/bridge_hubs.rs
+++ polkadot-parachain/src/chain_spec/bridge_hubs.rs
@@ -217,65 +217,6 @@ pub mod rococo {
 
 	pub type RuntimeApi = bridge_hub_rococo_runtime::RuntimeApi;
 
-	pub fn live_config<ModifyProperties: Fn(&mut sc_chain_spec::Properties)>(
-		id: &str,
-		chain_name: &str,
-		relay_chain: &str,
-		para_id: ParaId,
-		modify_props: ModifyProperties,
-	) -> BridgeHubChainSpec {
-		// Rococo defaults
-		let mut properties = sc_chain_spec::Properties::new();
-		properties.insert("ss58Format".into(), 42.into());
-		properties.insert("tokenSymbol".into(), "ROC".into());
-		properties.insert("tokenDecimals".into(), 12.into());
-		modify_props(&mut properties);
-
-		BridgeHubChainSpec::from_genesis(
-			// Name
-			chain_name,
-			// ID
-			super::ensure_id(id).expect("invalid id"),
-			ChainType::Live,
-			move || {
-				genesis(
-					// initial collators.
-					vec![
-						(
-							get_account_id_from_seed::<sr25519::Public>("Alice"),
-							get_collator_keys_from_seed::<AuraId>("Alice"),
-						),
-						(
-							get_account_id_from_seed::<sr25519::Public>("Bob"),
-							get_collator_keys_from_seed::<AuraId>("Bob"),
-						),
-					],
-					vec![
-						get_account_id_from_seed::<sr25519::Public>("Alice"),
-						get_account_id_from_seed::<sr25519::Public>("Bob"),
-						get_account_id_from_seed::<sr25519::Public>("Charlie"),
-						get_account_id_from_seed::<sr25519::Public>("Dave"),
-						get_account_id_from_seed::<sr25519::Public>("Eve"),
-						get_account_id_from_seed::<sr25519::Public>("Ferdie"),
-						get_account_id_from_seed::<sr25519::Public>("Alice//stash"),
-						get_account_id_from_seed::<sr25519::Public>("Bob//stash"),
-						get_account_id_from_seed::<sr25519::Public>("Charlie//stash"),
-						get_account_id_from_seed::<sr25519::Public>("Dave//stash"),
-						get_account_id_from_seed::<sr25519::Public>("Eve//stash"),
-						get_account_id_from_seed::<sr25519::Public>("Ferdie//stash"),
-					],
-					para_id,
-				)
-			},
-			Vec::new(),
-			None,
-			None,
-			None,
-			Some(properties),
-			Extensions { relay_chain: relay_chain.to_string(), para_id: para_id.into() },
-		)
-	}
-
 	pub fn local_config<ModifyProperties: Fn(&mut sc_chain_spec::Properties)>(
 		id: &str,
 		chain_name: &str,
@@ -395,17 +336,6 @@ pub mod wococo {
 		para_id: ParaId,
 	) -> BridgeHubChainSpec {
 		rococo::local_config(id, chain_name, relay_chain, para_id, |properties| {
-			properties.insert("tokenSymbol".into(), "WOOK".into());
-		})
-	}
-
-	pub fn live_config(
-		id: &str,
-		chain_name: &str,
-		relay_chain: &str,
-		para_id: ParaId,
-	) -> BridgeHubChainSpec {
-		rococo::live_config(id, chain_name, relay_chain, para_id, |properties| {
 			properties.insert("tokenSymbol".into(), "WOOK".into());
 		})
 	}
