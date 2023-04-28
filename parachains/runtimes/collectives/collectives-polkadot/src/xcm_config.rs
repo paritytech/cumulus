4145ee4f206dc4cb510e819176b4da2f087b42f2
muharem <ismailov.m.h@gmail.com>
2023-04-28 15:21:05 +0200

    rename

--- parachains/runtimes/collectives/collectives-polkadot/src/xcm_config.rs
+++ parachains/runtimes/collectives/collectives-polkadot/src/xcm_config.rs
@@ -294,8 +294,8 @@ impl pallet_xcm::Config for Runtime {
 	#[cfg(feature = "runtime-benchmarks")]
 	type ReachableDest = ReachableDest;
 	type AdminOrigin = EnsureRoot<AccountId>;
-	type MaxRemoteLockUsers = ConstU32<0>;
-	type RemoteLockIdentifier = ();
+	type MaxRemoteLockConsumers = ConstU32<0>;
+	type RemoteLockConsumerIdentifier = ();
 }
 
 impl cumulus_pallet_xcm::Config for Runtime {
