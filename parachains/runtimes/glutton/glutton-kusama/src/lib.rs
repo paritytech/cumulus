bb25b02533fd14076fd7f057b9550c69b210f6a1
Sergej Sakac <sakacszergej@gmail.com>
2023-05-16 10:39:15 +0200

    make clippy happy

--- parachains/runtimes/glutton/glutton-kusama/src/lib.rs
+++ parachains/runtimes/glutton/glutton-kusama/src/lib.rs
@@ -230,7 +230,7 @@ impl sp_runtime::traits::SignedExtension for DisallowSigned {
 		info: &DispatchInfoOf<Self::Call>,
 		len: usize,
 	) -> Result<Self::Pre, TransactionValidityError> {
-		Ok(self.validate(who, call, info, len).map(|_| ())?)
+		self.validate(who, call, info, len).map(|_| ())
 	}
 	fn validate(
 		&self,
