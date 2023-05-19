c9726e5ad4c9373e17b95bca0639e1ebf677e93d
Branislav Kontur <bkontur@gmail.com>
2023-05-19 15:27:31 +0200

    tmp

--- bridges/docs/dockerhub-rialto-bridge-node.README.md
+++
@@ -1,1 +0,0 @@
-# rialto-bridge-node
--- bridges/scripts/regenerate_runtimes.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd tools/runtime-codegen
-cargo run --bin runtime-codegen -- --from-node-url "wss://rococo-bridge-hub-rpc.polkadot.io:443" > ../../relays/client-bridge-hub-rococo/src/codegen_runtime.rs
-cargo run --bin runtime-codegen -- --from-node-url "http://localhost:20433" > ../../relays/client-rialto-parachain/src/codegen_runtime.rs
-cd -
-cargo fmt --all
