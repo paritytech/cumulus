d03d0674c2357a39dc56d7f4a215f31724b9c5a9
gilescope <gilescope@gmail.com>
2023-05-24 11:16:38 +0100

    cargo fmt, update readme

--- parachains/integration-tests/emulated/assets/statemine/src/lib.rs
+++ parachains/integration-tests/emulated/assets/statemine/src/lib.rs
@@ -19,10 +19,7 @@ pub use integration_tests_common::{
 pub use polkadot_core_primitives::InboundDownwardMessage;
 pub use xcm::{
 	prelude::*,
-	v3::{
-		Error,
-		NetworkId::Kusama as KusamaId,
-	},
+	v3::{Error, NetworkId::Kusama as KusamaId},
 };
 pub use xcm_emulator::{
 	assert_expected_events, bx, cumulus_pallet_dmp_queue, helpers::weight_within_threshold,
