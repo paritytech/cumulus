c779b004b765a8b6de539f38b03c249661663df1
NachoPal <ignacio.palacios.santos@gmail.com>
2023-06-02 12:31:07 +0200

    ready for cli

--- scripts/glutton/update/src/main.rs
+++ scripts/glutton/update/src/main.rs
@@ -1,4 +1,4 @@
-use subxt::{dynamic::{At, Value}, tx::{PairSigner}, utils::AccountId32, ext::sp_core::{Pair, sr25519::Pair as Sr25519Pair}, OnlineClient};
+use subxt::{dynamic::Value, tx::{PairSigner}, utils::AccountId32, ext::sp_core::{Pair, sr25519::Pair as Sr25519Pair}, OnlineClient};
 use crate::glutton_para::runtime_types::{sp_arithmetic::per_things::Perbill};
 
 mod config;
@@ -15,26 +15,21 @@ async fn get_account_nonce(
 	client: &OnlineClient<GluttonConfig>,
 	account: &AccountId32
 ) -> Result<u32, Box<dyn std::error::Error>> {
-	let mut nonce = 0;
+	let runtime_api_call = subxt::dynamic::runtime_api_call(
+		"AccountNonceApi",
+		"account_nonce",
+		vec![Value::from_bytes(account)],
+	);
 
-	let storage_query =
-			subxt::dynamic::storage("System", "Account", vec![Value::from_bytes(account)]);
-
-	let value = client
-		.storage()
+	let nonce = client
+		.runtime_api()
 		.at_latest()
 		.await?
-		.fetch(&storage_query)
-		.await?;
-
-	if let Some(decoded_value) = value {
-		nonce = decoded_value
-					.to_value()?
-					.at("nonce")
-					.unwrap()
-					.as_u128()
-					.unwrap() as u32
-	}
+		.call(runtime_api_call)
+		.await?
+		.to_value()?
+		.as_u128()
+		.unwrap() as u32;
 
 	Ok(nonce)
 }
@@ -71,27 +66,33 @@ async fn update_glutton(
 		.await
 		.unwrap();
 
+	let block_hash = in_block.block_hash();
+
+	println!("Tx included in block {:?}", block_hash);
+
 	Ok(())
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-	let para_ids = 1300..1301;
+	let from = 1300;
+	let to = 1300;
+	let para_ids = from..to+1;
 	let new_storage = 100_000_000;
 	let new_compute = 200_000_000;
 	let seed = "0xe5be9a5092b81bca64be81d212e7f2f9eba183bb7a90954f7b76361f6edb5c0a"; // Alice
 
 	// Get account and signer from secre seed
-    let seed_bytes = hex::decode(&seed[2..]).expect("Invalid seed hex value");
-	let account_pair: Sr25519Pair = Pair::from_seed_slice(&seed_bytes).expect("Failed to create key pair");
-	// let account_pair: Pair = Pair::from_string(seed, None).expect("Failed to create key pair");
+	let account_pair: Sr25519Pair = Pair::from_string(seed, None).expect("Failed to create key pair");
 	let sudo_signer = PairSigner::new(account_pair.clone());
 	let sudo_account = AccountId32::from(account_pair.public());
 
 	for id in para_ids {
-		println!("Para id: {}", id);
+		println!("\nUpdating Glutton {}", id);
 		// Create a new API client, configured to talk to Glutton nodes.
 		// let client = OnlineClient::<GluttonConfig>::from_url(format!("wss://versi-glutton-collator-{}-node-1.parity-versi.parity.io:443", id)).await?;
+
+		println!("Connecting to client...");
 		let client = OnlineClient::<GluttonConfig>::from_url("ws://127.0.0.1:9810").await?;
 
 		// Build `set_storage` call
@@ -99,6 +100,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 			storage: Perbill(new_storage)
 		});
 
+		println!("Submitting 'set_storage {{ storage: {} }}'...", new_storage);
 		// Sumbit `set_storage` call
 		update_glutton(&client, set_storage_call, &sudo_account, &sudo_signer).await?;
 
@@ -107,6 +109,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 			compute: Perbill(new_compute)
 		});
 
+		println!("Submitting 'set_compute {{ compute: {} }}'...", new_compute);
 		// Sumbit `set_compute` call
 		update_glutton(&client, set_compute_call, &sudo_account, &sudo_signer).await?;
 	}
