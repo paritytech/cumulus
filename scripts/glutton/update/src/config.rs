0750a64e8cacbbcad7f85664b859248d14d23c9f
NachoPal <ignacio.palacios.santos@gmail.com>
2023-06-02 13:34:54 +0200

    fmt

--- scripts/glutton/update/src/config.rs
+++ scripts/glutton/update/src/config.rs
@@ -1,86 +1,79 @@
-use subxt::{Config, SubstrateConfig, config::extrinsic_params::{ExtrinsicParams, Era}};
 use codec::{Compact, Encode};
 use core::fmt::Debug;
+use subxt::{
+	config::extrinsic_params::{Era, ExtrinsicParams},
+	Config, SubstrateConfig,
+};
 
 #[derive(Debug)]
 pub enum GluttonConfig {}
 
 impl Config for GluttonConfig {
-    type Index = <SubstrateConfig as Config>::Index;
-    type Hash = <SubstrateConfig as Config>::Hash;
-    type Hasher = <SubstrateConfig as Config>::Hasher;
-    type Header = <SubstrateConfig as Config>::Header;
-    type AccountId = <SubstrateConfig as Config>::AccountId;
-    type Address = <SubstrateConfig as Config>::Address;
-    type Signature = <SubstrateConfig as Config>::Signature;
+	type Index = <SubstrateConfig as Config>::Index;
+	type Hash = <SubstrateConfig as Config>::Hash;
+	type Hasher = <SubstrateConfig as Config>::Hasher;
+	type Header = <SubstrateConfig as Config>::Header;
+	type AccountId = <SubstrateConfig as Config>::AccountId;
+	type Address = <SubstrateConfig as Config>::Address;
+	type Signature = <SubstrateConfig as Config>::Signature;
 	// We need our custom ExtrinsicParams to remove the unexpected `tip` param
-    type ExtrinsicParams = GluttonExtrinsicParams<Self>;
+	type ExtrinsicParams = GluttonExtrinsicParams<Self>;
 }
 
 pub type GluttonExtrinsicParams<T> = BaseExtrinsicParams<T>;
 
 #[derive(Debug)]
 pub struct BaseExtrinsicParams<T: Config> {
-    era: Era,
-    nonce: T::Index,
-    spec_version: u32,
-    transaction_version: u32,
-    genesis_hash: T::Hash,
-    mortality_checkpoint: T::Hash,
-    marker: std::marker::PhantomData<T>,
+	era: Era,
+	nonce: T::Index,
+	spec_version: u32,
+	transaction_version: u32,
+	genesis_hash: T::Hash,
+	mortality_checkpoint: T::Hash,
+	marker: std::marker::PhantomData<T>,
 }
 
 pub struct BaseExtrinsicParamsBuilder<T: Config> {
-    era: Era,
-    mortality_checkpoint: Option<T::Hash>,
+	era: Era,
+	mortality_checkpoint: Option<T::Hash>,
 }
 
 impl<T: Config> Default for BaseExtrinsicParamsBuilder<T> {
-    fn default() -> Self {
-        Self {
-            era: Era::Immortal,
-            mortality_checkpoint: None,
-        }
-    }
+	fn default() -> Self {
+		Self { era: Era::Immortal, mortality_checkpoint: None }
+	}
 }
 
-impl<T: Config + Debug> ExtrinsicParams<T::Index, T::Hash>
-    for BaseExtrinsicParams<T>
-{
-    type OtherParams = BaseExtrinsicParamsBuilder<T>;
+impl<T: Config + Debug> ExtrinsicParams<T::Index, T::Hash> for BaseExtrinsicParams<T> {
+	type OtherParams = BaseExtrinsicParamsBuilder<T>;
 
-    fn new(
-        // Provided from subxt client:
-        spec_version: u32,
-        transaction_version: u32,
-        nonce: T::Index,
-        genesis_hash: T::Hash,
-        // Provided externally:
-        other_params: Self::OtherParams,
-    ) -> Self {
-        BaseExtrinsicParams {
-            era: other_params.era,
-            mortality_checkpoint: other_params.mortality_checkpoint.unwrap_or(genesis_hash),
-            nonce,
-            spec_version,
-            transaction_version,
-            genesis_hash,
-            marker: std::marker::PhantomData,
-        }
-    }
+	fn new(
+		// Provided from subxt client:
+		spec_version: u32,
+		transaction_version: u32,
+		nonce: T::Index,
+		genesis_hash: T::Hash,
+		// Provided externally:
+		other_params: Self::OtherParams,
+	) -> Self {
+		BaseExtrinsicParams {
+			era: other_params.era,
+			mortality_checkpoint: other_params.mortality_checkpoint.unwrap_or(genesis_hash),
+			nonce,
+			spec_version,
+			transaction_version,
+			genesis_hash,
+			marker: std::marker::PhantomData,
+		}
+	}
 
-    fn encode_extra_to(&self, v: &mut Vec<u8>) {
-        let nonce: u64 = self.nonce.into();
-        (self.era, Compact(nonce)).encode_to(v); // `tip` not included
-    }
+	fn encode_extra_to(&self, v: &mut Vec<u8>) {
+		let nonce: u64 = self.nonce.into();
+		(self.era, Compact(nonce)).encode_to(v); // `tip` not included
+	}
 
-    fn encode_additional_to(&self, v: &mut Vec<u8>) {
-        (
-            self.spec_version,
-            self.transaction_version,
-            self.genesis_hash,
-            self.mortality_checkpoint,
-        )
-            .encode_to(v);
-    }
+	fn encode_additional_to(&self, v: &mut Vec<u8>) {
+		(self.spec_version, self.transaction_version, self.genesis_hash, self.mortality_checkpoint)
+			.encode_to(v);
+	}
 }
--- scripts/glutton/update/src/main.rs
+++ scripts/glutton/update/src/main.rs
@@ -1,6 +1,12 @@
-use subxt::{dynamic::Value, tx::{PairSigner}, utils::AccountId32, ext::sp_core::{Pair, sr25519::Pair as Sr25519Pair}, OnlineClient};
-use crate::glutton_para::runtime_types::{sp_arithmetic::per_things::Perbill};
+use crate::glutton_para::runtime_types::sp_arithmetic::per_things::Perbill;
 use clap::Parser;
+use subxt::{
+	dynamic::Value,
+	ext::sp_core::{sr25519::Pair as Sr25519Pair, Pair},
+	tx::PairSigner,
+	utils::AccountId32,
+	OnlineClient,
+};
 
 mod config;
 use crate::config::GluttonConfig;
@@ -8,13 +14,13 @@ use crate::config::GluttonConfig;
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
-    /// Update Glutton Parachains from 'para_id = from' (inclusive)
-    #[arg(short, long)]
-    from: u32,
+	/// Update Glutton Parachains from 'para_id = from' (inclusive)
+	#[arg(short, long)]
+	from: u32,
 
-    /// Update Glutton Parachains to 'para_id = to' (inclusive)
-    #[arg(short, long)]
-    to: u32,
+	/// Update Glutton Parachains to 'para_id = to' (inclusive)
+	#[arg(short, long)]
+	to: u32,
 
 	/// Optional new 'storage' value to update
 	#[arg(short, long)]
@@ -38,7 +44,7 @@ type GluttonCall = glutton_para::runtime_types::pallet_glutton::pallet::Call;
 
 async fn get_account_nonce(
 	client: &OnlineClient<GluttonConfig>,
-	account: &AccountId32
+	account: &AccountId32,
 ) -> Result<u32, Box<dyn std::error::Error>> {
 	let runtime_api_call = subxt::dynamic::runtime_api_call(
 		"AccountNonceApi",
@@ -63,7 +69,7 @@ async fn update_glutton(
 	client: &OnlineClient<GluttonConfig>,
 	call: RuntimeCall,
 	account: &AccountId32,
-	signer: &PairSigner<GluttonConfig, Sr25519Pair>
+	signer: &PairSigner<GluttonConfig, Sr25519Pair>,
 ) -> Result<(), Box<dyn std::error::Error>> {
 	// Get signer's `nonce`
 	let nonce = get_account_nonce(client, account).await?;
@@ -74,22 +80,11 @@ async fn update_glutton(
 	// Build the signed tx
 	let signed_tx = client
 		.tx()
-		.create_signed_with_nonce(
-			&tx,
-			signer,
-			nonce,
-			Default::default()
-		)
+		.create_signed_with_nonce(&tx, signer, nonce, Default::default())
 		.unwrap();
 
 	// Submit and watch the tx
-	let in_block = signed_tx
-		.submit_and_watch()
-		.await
-		.unwrap()
-		.wait_for_in_block()
-		.await
-		.unwrap();
+	let in_block = signed_tx.submit_and_watch().await.unwrap().wait_for_in_block().await.unwrap();
 
 	let block_hash = in_block.block_hash();
 
@@ -103,11 +98,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 	let args = Cli::parse();
 	let from = args.from;
 	let to = args.to;
-	let para_ids = from..to+1;
+	let para_ids = from..to + 1;
 	let seed = args.private_key;
 
 	// Get account and signer from secre seed
-	let account_pair: Sr25519Pair = Pair::from_string(seed.as_str(), None).expect("Failed to create key pair");
+	let account_pair: Sr25519Pair =
+		Pair::from_string(seed.as_str(), None).expect("Failed to create key pair");
 	let sudo_signer = PairSigner::new(account_pair.clone());
 	let sudo_account = AccountId32::from(account_pair.public());
 
@@ -116,14 +112,17 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
 		// Create a new API client, configured to talk to Glutton nodes.
 		println!("\tConnecting to client...");
-		let client = OnlineClient::<GluttonConfig>::from_url(format!("wss://versi-glutton-collator-{}-node-1.parity-versi.parity.io:443", id)).await?;
+		let client = OnlineClient::<GluttonConfig>::from_url(format!(
+			"wss://versi-glutton-collator-{}-node-1.parity-versi.parity.io:443",
+			id
+		))
+		.await?;
 
 		// Set storage
 		if let Some(new_storage) = args.storage {
 			// Build `set_storage` call
-			let set_storage_call = RuntimeCall::Glutton(GluttonCall::set_storage {
-				storage: Perbill(new_storage)
-			});
+			let set_storage_call =
+				RuntimeCall::Glutton(GluttonCall::set_storage { storage: Perbill(new_storage) });
 
 			// Sumbit `set_storage` call
 			println!("\tSubmitting 'set_storage {{ storage: {} }}'...", new_storage);
@@ -133,9 +132,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 		// Set compute
 		if let Some(new_compute) = args.compute {
 			// Build `set_compute` call
-			let set_compute_call = RuntimeCall::Glutton(GluttonCall::set_compute {
-				compute: Perbill(new_compute)
-			});
+			let set_compute_call =
+				RuntimeCall::Glutton(GluttonCall::set_compute { compute: Perbill(new_compute) });
 
 			// Sumbit `set_compute` call
 			println!("\tSubmitting 'set_compute {{ compute: {} }}'...", new_compute);
@@ -143,5 +141,5 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 		}
 	}
 
-    Ok(())
+	Ok(())
 }
