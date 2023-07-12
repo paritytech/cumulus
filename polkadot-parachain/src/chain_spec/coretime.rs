d3b433c3d90d02e840cd236c3127f3484f3bf895
joepetrowski <joe@parity.io>
2023-07-12 13:08:26 +0200

    add runtimes

--- polkadot-parachain/src/chain_spec/coretime.rs
+++ polkadot-parachain/src/chain_spec/coretime.rs
@@ -14,11 +14,9 @@
 // You should have received a copy of the GNU General Public License
 // along with Cumulus.  If not, see <http://www.gnu.org/licenses/>.
 
-use crate::chain_spec::{get_account_id_from_seed, get_collator_keys_from_seed};
 use cumulus_primitives_core::ParaId;
 use parachains_common::Balance as CoretimeBalance;
 use sc_chain_spec::ChainSpec;
-use sp_core::sr25519;
 use std::{path::PathBuf, str::FromStr};
 
 /// Collects all supported Coretime configurations
--- polkadot-parachain/src/service.rs
+++ polkadot-parachain/src/service.rs
@@ -215,18 +215,6 @@ impl sc_executor::NativeExecutionDispatch for CoretimeKusamaRuntimeExecutor {
 	}
 }
 
-/// Native `CoretimeWestend` executor instance.
-pub struct CoretimeWestendRuntimeExecutor;
-impl sc_executor::NativeExecutionDispatch for CoretimeWestendRuntimeExecutor {
-	type ExtendHostFunctions = frame_benchmarking::benchmarking::HostFunctions;
-	fn dispatch(method: &str, data: &[u8]) -> Option<Vec<u8>> {
-		coretime_westend_runtime::api::dispatch(method, data)
-	}
-	fn native_version() -> sc_executor::NativeVersion {
-		coretime_westend_runtime::native_version()
-	}
-}
-
 /// Native contracts executor instance.
 pub struct ContractsRococoRuntimeExecutor;
 
