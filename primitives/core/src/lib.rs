ae978331b966ae351bfd17ea892565b5bb60c853
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2023-07-26 14:25:01 +0200

    Update primitives/core/src/lib.rs
    Co-authored-by: Svyatoslav Nikolsky <svyatonik@gmail.com>

--- primitives/core/src/lib.rs
+++ primitives/core/src/lib.rs
@@ -97,7 +97,7 @@ impl From<AggregateMessageOrigin> for xcm::v3::MultiLocation {
 		match origin {
 			AggregateMessageOrigin::Loopback => MultiLocation::here(),
 			AggregateMessageOrigin::Parent => MultiLocation::parent(),
-			AggregateMessageOrigin::Sibling(id) => Junction::Parachain(id.into()).into(),
+			AggregateMessageOrigin::Sibling(id) => MultiLocation::new(1, Junction::Parachain(id.into())),
 		}
 	}
 }
