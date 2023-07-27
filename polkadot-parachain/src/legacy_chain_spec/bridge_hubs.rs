82fb8b3110932e481379c107484a91693845a25a
Michal Kucharczyk <1728078+michalkucharczyk@users.noreply.github.com>
2023-07-27 17:02:53 +0200

    dead code allowed for legacy tests

--- polkadot-parachain/src/legacy_chain_spec/bridge_hubs.rs
+++ polkadot-parachain/src/legacy_chain_spec/bridge_hubs.rs
@@ -13,6 +13,9 @@
 
 // You should have received a copy of the GNU General Public License
 // along with Cumulus.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+#![allow(dead_code)]
 
 use crate::chain_spec::{get_account_id_from_seed, get_collator_keys_from_seed};
 use cumulus_primitives_core::ParaId;
--- polkadot-parachain/src/legacy_chain_spec/mod.rs
+++ polkadot-parachain/src/legacy_chain_spec/mod.rs
@@ -14,6 +14,8 @@
 // You should have received a copy of the GNU General Public License
 // along with Cumulus.  If not, see <http://www.gnu.org/licenses/>.
 
+#![allow(dead_code)]
+
 use parachains_common::{AccountId, Signature};
 use sc_chain_spec::{ChainSpecExtension, ChainSpecGroup};
 use serde::{Deserialize, Serialize};
