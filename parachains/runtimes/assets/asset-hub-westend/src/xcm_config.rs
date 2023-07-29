60624ea7b84ce5517775c4f0497fbe6352bc9ac0
Squirrel <gilescope@gmail.com>
2023-07-29 17:13:08 +0100

    Update parachains/runtimes/assets/asset-hub-westend/src/xcm_config.rs
    Co-authored-by: joe petrowski <25483142+joepetrowski@users.noreply.github.com>

--- parachains/runtimes/assets/asset-hub-westend/src/xcm_config.rs
+++ parachains/runtimes/assets/asset-hub-westend/src/xcm_config.rs
@@ -117,7 +117,7 @@ pub type ForeignAssetsConvertedConcreteId = assets_common::ForeignAssetsConverte
 	(
 		// Ignore `TrustBackedAssets` explicitly
 		StartsWith<TrustBackedAssetsPalletLocation>,
-		// Ignore asset which starts explicitly with our `GlobalConsensus(NetworkId)`, means:
+		// Ignore assets that start explicitly with our `GlobalConsensus(NetworkId)`; means:
 		// - foreign assets from our consensus should be: `MultiLocation {parents: 1, X*(Parachain(xyz), ..)}`
 		// - foreign assets outside our consensus with the same `GlobalConsensus(NetworkId)` won't be accepted here
 		StartsWithExplicitGlobalConsensus<UniversalLocationNetworkId>,
