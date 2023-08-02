68dd899d3a9601b17fd9484cb8ea11ccb71bfb22
Juan Girini <juangirini@gmail.com>
2023-08-02 10:26:01 +0200

    remove type Balance

--- parachains/runtimes/contracts/contracts-rococo/src/contracts.rs
+++ parachains/runtimes/contracts/contracts-rococo/src/contracts.rs
@@ -31,7 +31,6 @@ impl Config for Runtime {
 	type Time = Timestamp;
 	type Randomness = RandomnessCollectiveFlip;
 	type Currency = Balances;
-	type Balance = Balance;
 	type RuntimeEvent = RuntimeEvent;
 	type RuntimeCall = RuntimeCall;
 	/// The safest default is to allow no calls at all.
