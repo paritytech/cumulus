1bcdcfa13a5dfdea2a2c72d56ce5cde34c5a123f
Peter White <petras9789@gmail.com>
2023-08-08 11:20:10 -0600

    modify xcmp queue migration to migrate from v1 -> v2 -> v3

--- pallets/xcmp-queue/src/migration.rs
+++ pallets/xcmp-queue/src/migration.rs
@@ -31,17 +31,11 @@ pub const STORAGE_VERSION: StorageVersion = StorageVersion::new(2);
 pub fn migrate_to_latest<T: Config>() -> Weight {
 	let mut weight = T::DbWeight::get().reads(1);
 
-	if StorageVersion::get::<Pallet<T>>() == 1 {
-		weight.saturating_accrue(migrate_to_v2::<T>());
-		StorageVersion::new(2).put::<Pallet<T>>();
-		weight.saturating_accrue(T::DbWeight::get().writes(1));
-	}
+	weight.saturating_accrue(migrate_to_v2::<T>());
 
-	if StorageVersion::get::<Pallet<T>>() == 2 {
-		weight.saturating_accrue(migrate_to_v3::<T>());
-		StorageVersion::new(3).put::<Pallet<T>>();
-		weight.saturating_accrue(T::DbWeight::get().writes(1));
-	}
+	weight.saturating_accrue(migrate_to_v3::<T>());
+	StorageVersion::new(3).put::<Pallet<T>>();
+	weight.saturating_accrue(T::DbWeight::get().writes(1));
 
 	weight
 }
