d5118b0c5e0010384132893da8117201f32ef37e
muharem <ismailov.m.h@gmail.com>
2023-08-18 12:44:28 +0200

    benchmarks v2

--- parachains/pallets/collective-content/src/benchmarking.rs
+++ parachains/pallets/collective-content/src/benchmarking.rs
@@ -16,8 +16,8 @@
 //! The pallet benchmarks.
 
 use super::{Pallet as CollectiveContent, *};
-use frame_benchmarking::v1::{benchmarks_instance_pallet, BenchmarkError};
-use frame_support::traits::{EnsureOrigin, UnfilteredDispatchable};
+use frame_benchmarking::{impl_benchmark_test_suite, v2::*};
+use frame_support::traits::EnsureOrigin;
 
 fn assert_last_event<T: Config<I>, I: 'static>(generic_event: <T as Config<I>>::RuntimeEvent) {
 	frame_system::Pallet::<T>::assert_last_event(generic_event.into());
@@ -29,60 +29,74 @@ fn create_cid(i: u8) -> OpaqueCid {
 	cid
 }
 
-benchmarks_instance_pallet! {
-	set_charter {
+//
+
+// #[instance_benchmarks(where T: Config<I>, I: 'static)]
+#[instance_benchmarks]
+mod benchmarks {
+	use super::*;
+
+	#[benchmark]
+	fn set_charter() -> Result<(), BenchmarkError> {
 		let cid: OpaqueCid = create_cid(1);
-		let call = Call::<T, I>::set_charter { cid: cid.clone() };
-		let origin = T::CharterOrigin::try_successful_origin().map_err(|_| BenchmarkError::Weightless)?;
-	}: { call.dispatch_bypass_filter(origin)? }
-	verify {
+		let origin =
+			T::CharterOrigin::try_successful_origin().map_err(|_| BenchmarkError::Weightless)?;
+
+		#[extrinsic_call]
+		_(origin as T::RuntimeOrigin, cid.clone());
+
 		assert_eq!(CollectiveContent::<T, I>::charter(), Some(cid.clone()));
 		assert_last_event::<T, I>(Event::NewCharterSet { cid }.into());
+		Ok(())
 	}
 
-	announce {
-		let x in 0 .. 1;
-
+	#[benchmark]
+	fn announce(x: Linear<0, 1>) -> Result<(), BenchmarkError> {
 		let mut maybe_expire = None;
 		if x == 1 {
 			maybe_expire = Some(DispatchTime::<_>::At(10u32.into()));
 		}
 		let now = frame_system::Pallet::<T>::block_number();
 		let cid: OpaqueCid = create_cid(1);
-		let call = Call::<T, I>::announce {
-			cid: cid.clone(),
-			maybe_expire: maybe_expire.clone(),
-		};
-		let origin = T::AnnouncementOrigin::try_successful_origin().map_err(|_| BenchmarkError::Weightless)?;
-	}: { call.dispatch_bypass_filter(origin)? }
-	verify {
+		let origin = T::AnnouncementOrigin::try_successful_origin()
+			.map_err(|_| BenchmarkError::Weightless)?;
+
+		#[extrinsic_call]
+		_(origin as T::RuntimeOrigin, cid.clone(), maybe_expire.clone());
+
 		assert_eq!(CollectiveContent::<T, I>::announcements_count(), 1);
 		assert_eq!(NextAnnouncementExpireAt::<T, I>::get().map_or(0, |_| 1), x);
-		assert_last_event::<T, I>(Event::AnnouncementAnnounced {
-			cid,
-			maybe_expire_at: maybe_expire.map_or(None, |e| Some(e.evaluate(now))),
-		}.into());
+		assert_last_event::<T, I>(
+			Event::AnnouncementAnnounced {
+				cid,
+				maybe_expire_at: maybe_expire.map_or(None, |e| Some(e.evaluate(now))),
+			}
+			.into(),
+		);
+
+		Ok(())
 	}
 
-	remove_announcement {
+	#[benchmark]
+	fn remove_announcement() -> Result<(), BenchmarkError> {
 		let cid: OpaqueCid = create_cid(1);
-		let origin = T::AnnouncementOrigin::try_successful_origin().map_err(|_| BenchmarkError::Weightless)?;
-		CollectiveContent::<T, I>::announce(
-			origin.clone(),
-			cid.clone(),
-			None,
-		).expect("could not publish an announcement");
+		let origin = T::AnnouncementOrigin::try_successful_origin()
+			.map_err(|_| BenchmarkError::Weightless)?;
+		CollectiveContent::<T, I>::announce(origin.clone(), cid.clone(), None)
+			.expect("could not publish an announcement");
 		assert_eq!(CollectiveContent::<T, I>::announcements_count(), 1);
 
-		let call = Call::<T, I>::remove_announcement { cid: cid.clone() };
-	}: { call.dispatch_bypass_filter(origin)? }
-	verify {
+		#[extrinsic_call]
+		_(origin as T::RuntimeOrigin, cid.clone());
+
 		assert_eq!(CollectiveContent::<T, I>::announcements_count(), 0);
 		assert_last_event::<T, I>(Event::AnnouncementRemoved { cid }.into());
+
+		Ok(())
 	}
 
-	cleanup_announcements {
-		let x in 0 .. 100;
+	#[benchmark]
+	fn cleanup_announcements(x: Linear<0, 100>) -> Result<(), BenchmarkError> {
 		let origin = T::AnnouncementOrigin::try_successful_origin().unwrap();
 
 		let max_count = x;
@@ -92,15 +106,21 @@ benchmarks_instance_pallet! {
 				origin.clone(),
 				cid,
 				Some(DispatchTime::<_>::At(5u32.into())),
-			).expect("could not publish an announcement");
+			)
+			.expect("could not publish an announcement");
 		}
 		assert_eq!(CollectiveContent::<T, I>::announcements_count(), max_count);
 		frame_system::Pallet::<T>::set_block_number(10u32.into());
-	}: {
-		CollectiveContent::<T, I>::cleanup_announcements(10u32.into());
-	} verify {
+
+		#[block]
+		{
+			CollectiveContent::<T, I>::cleanup_announcements(10u32.into());
+		}
+
 		assert_eq!(CollectiveContent::<T, I>::announcements_count(), 0);
-		assert_eq!(frame_system::Pallet::<T>::events().len() as u32, max_count)
+		assert_eq!(frame_system::Pallet::<T>::events().len() as u32, max_count);
+
+		Ok(())
 	}
 
 	impl_benchmark_test_suite!(CollectiveContent, super::mock::new_bench_ext(), super::mock::Test);
