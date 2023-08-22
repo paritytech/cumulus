0bfd7a1afb8c55bbcaf8263a7042b4e1ce8339b6
EgorPopelyaev <egor@parity.io>
2023-08-22 09:59:23 +0200

    tag iamge after it was built

--- docker/scripts/build-injected-image.sh
+++ docker/scripts/build-injected-image.sh
@@ -2,19 +2,9 @@
 
 OWNER=${OWNER:-parity}
 IMAGE_NAME=${IMAGE_NAME:-polkadot-parachain}
-TAGS=${TAGS[@]:-latest}
-IFS=',' read -r -a TAG_ARRAY <<< "$TAGS"
-TAG_ARGS=" "
-
-echo "The image ${IMAGE_NAME} will be tagged with ${TAG_ARRAY[*]}"
-for tag in "${TAG_ARRAY[@]}"; do
-  TAG_ARGS+="--tag ${OWNER}/${IMAGE_NAME}:${tag} "
-done
-
-echo "$TAG_ARGS"
 
 docker build --no-cache \
     --build-arg IMAGE_NAME=$IMAGE_NAME \
-    ${TAG_ARGS} \
+    -t $OWNER/$IMAGE_NAME \
     -f ./docker/injected.Dockerfile \
     . && docker images
