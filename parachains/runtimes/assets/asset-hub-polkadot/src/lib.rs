77ca9ff652d03065e90b189db4cfdd3d865fe09c
EgorPopelyaev <egor@parity.io>
2023-08-24 11:29:09 +0200

    Update spec_version

--- parachains/runtimes/assets/asset-hub-polkadot/src/lib.rs
+++ parachains/runtimes/assets/asset-hub-polkadot/src/lib.rs
@@ -138,7 +138,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("statemint"),
 	impl_name: create_runtime_str!("statemint"),
 	authoring_version: 1,
-	spec_version: 10000,
+	spec_version: 10005,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 13,
